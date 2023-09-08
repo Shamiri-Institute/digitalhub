71327dbcf8043932e71d62f980098dd0f2df0172
hinn254 <bennyhinnotieno@gmail.com>
2023-09-08 12:32:20 +0300

    sentry

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-"use client"
-import * as Sentry from "@sentry/nextjs";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <h1>Shamiri Frontend</h1>
-
       </div>
     </main>
   )
