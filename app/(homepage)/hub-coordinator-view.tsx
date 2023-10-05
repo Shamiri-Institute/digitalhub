4a9cf74336f787adab02213e7d61a911c27ed1db
Edmund Korley <13422991+EdmundKorley@users.noreply.github.com>
2023-10-05 16:59:40 -0400

    wip

---
+++ app/(homepage)/hub-coordinator-view.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import * as React from "react";
+
+import { Button } from "#/components/ui/button";
+import { Input } from "#/components/ui/input";
+import { batchUploadFellows } from "#/app/actions";
+
+export function HubCoordinatorView() {
+  return (
+    <div>
+      <div>Welcome hub coordinators!</div>
+      <div className="max-w-fit my-5 xl:mt-10">
+        <FellowsBatchUploader />
+      </div>
+    </div>
+  );
+}
+
+function FellowsBatchUploader() {
+  return (
+    <form action={batchUploadFellows}>
+      <label className="border border-border rounded-xl px-6 py-6 flex flex-col gap-4">
+        <div className="text-muted-foreground">
+          The file you upload must be a CSV file with exactly the following
+          columns:
+          <ul className="list-disc list-inside">
+            <li>
+              <code>Name</code>
+            </li>
+            <li>
+              <code>Phone</code>
+            </li>
+            <li>
+              <code>Email</code>
+            </li>
+            <li>
+              <code>National ID</code>
+            </li>
+            <li>
+              <code>MPESA Name</code>
+            </li>
+            <li>
+              <code>MPESA Number</code>
+            </li>
+            <li>
+              <code>County</code>
+            </li>
+            <li>
+              <code>Sub-county</code>
+            </li>
+            <li>
+              <code>Date of birth</code>
+            </li>
+            <li>
+              <code>Gender</code>
+            </li>
+          </ul>
+        </div>
+        <Input id="csv-file" name="csv-file" type="file" accept="text/csv" />
+        <Button variant="brand">Upload</Button>
+      </label>
+    </form>
+  );
+}
