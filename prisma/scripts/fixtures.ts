e1ebaceef29188c93ccaa4903b52ec7784aa812c
Edmund Korley <13422991+EdmundKorley@users.noreply.github.com>
2023-10-09 13:14:48 -0400

    feat(app.ts): add session validation and redirection logic to improve user experience and security feat(schema.prisma): add visibleId field to Hub model to uniquely identify hubs feat(fixtures.ts): add new hub data and update organizationRole for better data representation feat(seed.ts): add createHubs function to seed hubs data into the database for testing and initial setup

--- prisma/scripts/fixtures.ts
+++ prisma/scripts/fixtures.ts
@@ -1,4 +1,56 @@
 const fixtures = {
+  hubs: [
+    {
+      visibleId: "23_HUB_01",
+      hubName: "Kariobangi",
+      hubCoordinatorByEmail: "jackline@shamiri.institute",
+    },
+    {
+      visibleId: "23_HUB_02",
+      hubName: "Kasarani",
+      hubCoordinatorByEmail: "dennis@shamiri.institute",
+    },
+    {
+      visibleId: "23_HUB_03",
+      hubName: "Kiambaa",
+      hubCoordinatorByEmail: "mcgovarn@shamiri.institute",
+    },
+    {
+      visibleId: "23_HUB_04",
+      hubName: "Machakos and Makueni",
+      hubCoordinatorByEmail: "amanda@shamiri.institute",
+    },
+    {
+      visibleId: "23_HUB_05",
+      hubName: "Ruiru",
+      hubCoordinatorByEmail: "chrispinus@shamiri.institute",
+    },
+    {
+      visibleId: "Partner",
+      hubName: "Partner",
+      hubCoordinatorByEmail: "paul@shamiri.institute",
+    },
+    {
+      visibleId: "HQ_2022",
+      hubName: "All",
+      hubCoordinatorByEmail: "ruiru@shamiri.institute",
+    },
+    {
+      visibleId: "HQ_2021",
+      hubName: "All",
+      hubCoordinatorByEmail: "ruiru@shamiri.institute",
+    },
+    {
+      visibleId: "HQ_2019",
+      hubName: "All",
+      hubCoordinatorByEmail: "ruiru@shamiri.institute",
+    },
+    {
+      visibleId: "HQ_2018",
+      hubName: "All",
+      hubCoordinatorByEmail: "ruiru@shamiri.institute",
+    },
+  ],
   organizations: [
     {
       name: "Team Shamiri",
@@ -106,7 +158,7 @@ const fixtures = {
       email: "edmund@shamiri.institute",
       name: "Edmund Korley",
       organizationByEmail: "team@shamiri.institute",
-      organizationRole: "hub-coordinator",
+      organizationRole: "admin",
     },
     {
       email: "dmndetei@amhf.or.ke",
@@ -115,6 +167,42 @@ const fixtures = {
         "https://shamiridigitalhub-public.s3.af-south-1.amazonaws.com/david-ndetei-headshot.jpg",
       organizationByEmail: "info@amhf.or.ke",
       organizationRole: "admin",
+    },
+    {
+      email: "jackline@shamiri.institute",
+      name: "Jackline",
+      organizationByEmail: "team@shamiri.institute",
+      organizationRole: "hub-coordinator",
+    },
+    {
+      email: "dennis@shamiri.institute",
+      name: "Dennis",
+      organizationByEmail: "team@shamiri.institute",
+      organizationRole: "hub-coordinator",
+    },
+    {
+      email: "mcgovarn@shamiri.institute",
+      name: "McGovarn",
+      organizationByEmail: "team@shamiri.institute",
+      organizationRole: "hub-coordinator",
+    },
+    {
+      email: "amanda@shamiri.institute",
+      name: "Amanda",
+      organizationByEmail: "team@shamiri.institute",
+      organizationRole: "hub-coordinator",
+    },
+    {
+      email: "chrispinus@shamiri.institute",
+      name: "Chrispinus Misati",
+      organizationByEmail: "team@shamiri.institute",
+      organizationRole: "hub-coordinator",
+    },
+    {
+      email: "paul@shamiri.institute",
+      name: "Paul Okoth",
+      organizationByEmail: "team@shamiri.institute",
+      organizationRole: "hub-coordinator",
     },
   ],
 };
