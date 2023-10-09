10e58a39e39d243e4b60ff52174c6168e441a9ca
Edmund Korley <13422991+EdmundKorley@users.noreply.github.com>
2023-10-09 13:14:48 -0400

    feat(app.ts): add session validation and redirection logic to improve user experience and security feat(schema.prisma): add visibleId field to Hub model to uniquely identify hubs feat(fixtures.ts): add new hub data and update organizationRole for better data representation feat(seed.ts): add createHubs function to seed hubs data into the database for testing and initial setup

--- lib/middleware/app.ts
+++ lib/middleware/app.ts
@@ -4,6 +4,16 @@ import { getToken } from "next-auth/jwt";
 import { parse } from "#/lib/middleware/utils";
 
 export default async function AppMiddleware(req: NextRequest) {
-  const base = parse(req);
-  console.log({ base });
+  const { path } = parse(req);
+  const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  if (!session?.email && path !== "/login" && path !== "/register") {
+    return NextResponse.redirect(
+      new URL(
+        `/login${path !== "/" ? `?next=${encodeURIComponent(path)}` : ""}`,
+        req.url
+      )
+    );
+  } else if (session?.email) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
 }
