7c70f4b0eaa27a9e8a55b592cdb085e26125fd17
Edmund Korley <13422991+EdmundKorley@users.noreply.github.com>
2023-10-31 07:26:18 -0400

    flatten migrations

--- prisma/migrations/20231031040953_gn/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `group` on the `students` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "students" DROP COLUMN "group",
-ADD COLUMN     "group_name" VARCHAR(255);
--- prisma/migrations/20231031041620_isst/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `session_type` to the `intervention_sessions` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "intervention_sessions" ADD COLUMN     "session_type" VARCHAR(255) NOT NULL;
