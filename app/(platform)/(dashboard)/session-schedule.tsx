64b30e111d8b4e5fc9cd674a916bb1148122c316
Edmund Korley <13422991+EdmundKorley@users.noreply.github.com>
2023-11-07 16:53:43 -0800

    wip

--- app/(platform)/(dashboard)/session-schedule.tsx
+++ app/(platform)/(dashboard)/session-schedule.tsx
@@ -5,9 +5,11 @@ import * as React from "react";
 import { SchedulingDialog } from "#/app/(platform)/(dashboard)/scheduling-dialog";
 import { cn } from "#/lib/utils";
 import {
+  addHours,
   eachDayOfInterval,
   endOfWeek,
   format,
+  getHours,
   isToday,
   isWithinInterval,
   startOfWeek,
@@ -16,8 +18,7 @@ import {
 interface SessionEvent {
   title: string;
   date: Date;
-  startTime: string;
-  endTime: string;
+  duration: number;
 }
 
 export function SessionSchedule({ sessions }: { sessions: SessionEvent[] }) {
@@ -26,12 +27,41 @@ export function SessionSchedule({ sessions }: { sessions: SessionEvent[] }) {
   const currentWeekStart = startOfWeek(currentDate);
   const currentWeekEnd = endOfWeek(currentDate);
 
+  const firstSessionStartDate = sessions[0]?.date!;
+  const firstSessionStartHour = getHours(firstSessionStartDate);
+  const threeHoursBeforeStartHour = new Date(currentDate);
+  threeHoursBeforeStartHour.setHours(firstSessionStartHour - 3);
+  const lastSessionEndDate = new Date(sessions[sessions.length - 1]?.date!);
+  const lastSessionEndHour = getHours(lastSessionEndDate);
+  const threeHoursAfterEndHour = new Date(currentDate);
+  threeHoursAfterEndHour.setHours(lastSessionEndHour + 3);
+
+  let scheduleHoursRange: number[] = [];
+  for (
+    let i = firstSessionStartHour - 3;
+    i <= lastSessionEndHour + 3 + 1;
+    i++
+  ) {
+    scheduleHoursRange.push(i);
+  }
+  console.debug({
+    firstSessionStartHour,
+    lastSessionEndHour,
+    scheduleHoursRange,
+  });
+
   const thisWeekSessions = sessions.filter((session) =>
     isWithinInterval(session.date, {
       start: currentWeekStart,
       end: currentWeekEnd,
     }),
   );
+  console.log({
+    date: sessions[0]!.date,
+    currentWeekStart,
+    currentWeekEnd,
+    thisWeekSessions,
+  });
 
   const daysOfWeek = eachDayOfInterval({
     start: currentWeekStart,
@@ -62,6 +92,7 @@ export function SessionSchedule({ sessions }: { sessions: SessionEvent[] }) {
         <div className="mt-2 grid grid-cols-7 gap-1 px-1 text-center">
           {daysOfWeek.map(({ dayOfMonth, dayName, isToday }) => (
             <div
+              key={dayOfMonth}
               className={cn(
                 "flex flex-col items-center gap-px rounded-t-md pb-2.5 pt-1 text-left",
                 {
@@ -81,10 +112,11 @@ export function SessionSchedule({ sessions }: { sessions: SessionEvent[] }) {
           ))}
         </div>
       </div>
-      <div className="mt-4 grid max-h-96 grid-rows-5 gap-2 overflow-scroll">
-        {[6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18].map((hour) =>
-          sessions.map((session, index) => {
+      <div className="mt-4 grid grid-rows-5 gap-2 overflow-scroll">
+        {scheduleHoursRange.map((hour) =>
+          thisWeekSessions.map((session, index) => {
             const isSessionThisHour = session.date.getHours() === hour;
+            console.debug({ isSessionThisHour, hour });
             return (
               <div key={index} className="flex h-20 justify-between gap-2">
                 <span className="w-20 px-4 text-right font-medium">
@@ -100,7 +132,11 @@ export function SessionSchedule({ sessions }: { sessions: SessionEvent[] }) {
                       >
                         <h2 className="font-semibold">{session.title}</h2>
                         <span className="text-sm">
-                          {session.startTime} - {session.endTime}
+                          {format(session.date, "h:mm a")} -{" "}
+                          {format(
+                            addHours(session.date, session.duration),
+                            "h:mm a",
+                          )}
                         </span>
                       </button>
                     </SchedulingDialog>
