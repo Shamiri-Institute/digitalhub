805fba6ad852adc345301a4b891cdb4b06fdaac3
hinn254 <bennyhinnotieno@gmail.com>
2023-11-13 23:26:15 +0300

    wip profile page

--- prisma/migrations/20231030091812_/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "supervisors" ADD COLUMN     "bank_account_name" VARCHAR(255),
-ADD COLUMN     "bank_account_number" VARCHAR(255),
-ADD COLUMN     "bank_branch" VARCHAR(255),
-ADD COLUMN     "bank_name" VARCHAR(255),
-ADD COLUMN     "county" VARCHAR(255),
-ADD COLUMN     "sub_county" VARCHAR(255);
