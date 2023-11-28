290e56f35b79ad567902d34f3b7d320fe84e7379
hinn254 <bennyhinnotieno@gmail.com>
2023-11-28 09:46:09 +0300

    Add new components for general issues, student sessions, and case notes plan

---
+++ app/(platform)/screenings/[name]/case-header.tsx
@@ -0,0 +1,69 @@
+"use client"
+import { useState } from "react";
+import { cn } from "#/lib/utils";
+import { Icons } from "#/components/icons";
+import { sampleReferredCasses, colors } from "./page";
+import Link from "next/link";
+
+export default function CaseHeader({ name }: { name: string }) {
+    const [selected, setSelected] = useState<string>("")
+    const [color, setColor] = useState<string | undefined>("")
+    const [flagged, setFlagged] = useState<boolean>(false)
+
+    const handleOption = (status: string) => {
+        setSelected(status)
+        setColor(colors[status])
+    }
+
+    const handleFlagged = () => {
+        setFlagged(!flagged)
+    }
+
+
+    return (
+        <>
+            <div className="flex justify-between my-4">
+                <Link href="/screenings">
+                    <Icons.chevronLeft className="w-6 h-6 text-brand" />
+                </Link>
+
+                <button onClick={handleFlagged}>
+                    <Icons.flagcase className={cn("w-6 h-6 text-muted-foreground",
+                        flagged && "text-shamiri-red")}
+                    />
+                </button>
+            </div>
+
+            <div className="flex flex-1">
+                <div className={cn("flex rounded-full h-24 w-24 justify-center items-center bg-muted-green",
+                    color && color
+                )}>
+                    <div className="rounded-full h-20 w-20 bg-muted-foreground" />
+                </div>
+                <div className="flex flex-col flex-1 ml-6 justify-center ">
+                    <div className="flex flex-col">
+                        <p className="text-base font-bold text-brand">{name}</p>
+                        <p className="text-sm font-medium text-muted-foreground">Shamiri ID</p>
+                    </div>
+                    <div className="flex mt-1 justify-between">
+                        {sampleReferredCasses.map((stud) => (
+                            <button
+                                key={stud.id}
+                                className={cn(
+                                    "flex rounded-md h-7 w-12 justify-center items-center bg-[#bbb]",
+                                    // colors[stud.status]
+                                    selected === stud.status && colors[stud.status]
+                                )}
+                                onClick={() => handleOption(stud.status)}
+                            >
+                                <p className="text-sm font-medium text-white ">
+                                    {stud.status.charAt(0).toUpperCase()}
+                                </p>
+                            </button>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
---
+++ app/(platform)/screenings/[name]/general-issues.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { cn } from "#/lib/utils";
+import { useState } from "react";
+import { Input } from "#/components/ui/input";
+
+export default function GeneralIssues() {
+    const [selected, setSelected] = useState<string>("");
+    const [other, setOther] = useState<string>("");
+
+    const handleOther = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setOther(e.target.value)
+
+    }
+    return (
+        <div>
+            <h3 className="my-4 text-muted-foreground text-sm font-medium ">
+                General
+            </h3>
+            <div className="flex flex-col">
+                <div className="flex gap-2 my-1">
+                    <GeneralOption option="Academic challenges" selected={selected} setSelected={setSelected} />
+                    <GeneralOption option="Family issues" selected={selected} setSelected={setSelected} />
+                    <GeneralOption option="Peer pressure" selected={selected} setSelected={setSelected} />
+                </div>
+                <div className="flex gap-2 my-1">
+                    <GeneralOption option="Romantic r/ship issues" selected={selected} setSelected={setSelected} />
+                    <GeneralOption option="Self-esteem issues" selected={selected} setSelected={setSelected} />
+                    <GeneralOption option="Other" selected={selected} setSelected={setSelected} />
+                </div>
+            </div>
+
+            {selected === "Other" && <div className="mt-2 px-[1px]">
+                <Input
+                    id="other"
+                    name="other"
+                    type="text"
+                    value={other}
+                    onChange={handleOther}
+                    placeholder="Other? Type here"
+                    className="resize-none bg-card"
+                />
+            </div>}
+
+        </div>
+    );
+}
+
+function GeneralOption({ option, selected, setSelected }: { option: string; selected: string; setSelected: (option: string) => void }) {
+    return (
+        <button className={cn("bg-blue-400 py-4 px-3 rounded-sm flex-1",
+            selected === option && "bg-shamiri-light-blue")}
+            onClick={() => setSelected(option)}
+        >
+            <p className="text-xs text-brand font-medium">
+                {option}
+            </p>
+        </button>
+    )
+}
