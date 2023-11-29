f62044a9b52b0d96d0711e518f0443bfc6b3f51e
hinn254 <bennyhinnotieno@gmail.com>
2023-11-29 13:27:55 +0300

    formated files w prisma

--- app/(platform)/screenings/[name]/presenting-issues.tsx
+++ app/(platform)/screenings/[name]/presenting-issues.tsx
@@ -1,107 +1,109 @@
-"use client"
+"use client";
+import { Card } from "#/components/ui/card";
 import { Separator } from "#/components/ui/separator";
 import { cn } from "#/lib/utils";
 import { useState } from "react";
 import GeneralIssues from "./general-issues";
-import { Card } from "#/components/ui/card";
 
 export function PresentingIssues() {
-    return (
-        <div className="mt-4">
-            <h3 className="mb-2 text-muted-foreground text-sm font-medium">
-                Emergency
-            </h3>
-            <Separator />
-            <DiagnosingBoard />
-            <GeneralIssues />
-        </div>
-    );
+  return (
+    <div className="mt-4">
+      <h3 className="mb-2 text-sm font-medium text-muted-foreground">
+        Emergency
+      </h3>
+      <Separator />
+      <DiagnosingBoard />
+      <GeneralIssues />
+    </div>
+  );
 }
 
 const emergency_options = [
-    { id: 1, name: "Anxiety" },
-    { id: 2, name: "Self-harm" },
-    { id: 3, name: "Stress" },
-    { id: 4, name: "Bullying" },
-    { id: 5, name: "Sexual abuse" },
-    { id: 5, name: "Suicidality" },
-]
+  { id: 1, name: "Anxiety" },
+  { id: 2, name: "Self-harm" },
+  { id: 3, name: "Stress" },
+  { id: 4, name: "Bullying" },
+  { id: 5, name: "Sexual abuse" },
+  { id: 5, name: "Suicidality" },
+];
 
 function DiagnosingBoard() {
-    return (
-        <div>
-            <div className="flex justify-between mt-4">
-                <div className="flex-1" />
-                <div className="flex flex-1 justify-between ml-2">
-                    <div>
-                        <p className="text-xs text-brand font-medium">
-                            No
-                        </p>
-                    </div>
-                    <div>
-                        <p className="text-xs text-brand font-medium">
-                            Low
-                        </p>
-                    </div>
-                    <div className="">
-                        <p className="text-xs text-brand font-medium">
-                            Med
-                        </p>
-                    </div>
-                    <div>
-                        <p className="text-xs text-brand font-medium">
-                            High
-                        </p>
-                    </div>
-                </div>
-            </div>
-            {
-                emergency_options.map((option) => (
-                    <IssueOptions key={option.id} name={option.name} />
-                ))
-            }
+  return (
+    <div>
+      <div className="mt-4 flex justify-between">
+        <div className="flex-1" />
+        <div className="ml-2 flex flex-1 justify-between">
+          <div>
+            <p className="text-xs font-medium text-brand">No</p>
+          </div>
+          <div>
+            <p className="text-xs font-medium text-brand">Low</p>
+          </div>
+          <div className="">
+            <p className="text-xs font-medium text-brand">Med</p>
+          </div>
+          <div>
+            <p className="text-xs font-medium text-brand">High</p>
+          </div>
         </div>
-    )
-
+      </div>
+      {emergency_options.map((option) => (
+        <IssueOptions key={option.id} name={option.name} />
+      ))}
+    </div>
+  );
 }
 
-function SingleIssueOption({ onSelect = f => f, selected, option }: { selected: string; onSelect: (option: string) => void; option: string }) {
-    return (
-        <div className="flex">
-            <button className={cn("h-6 w-6 rounded-full bg-[#d9d9d9] mr-1",
-                selected === option && "bg-brand")}
-                onClick={() => onSelect(option)}
-            />
-        </div>
-    )
+function SingleIssueOption({
+  onSelect = (f) => f,
+  selected,
+  option,
+}: {
+  selected: string;
+  onSelect: (option: string) => void;
+  option: string;
+}) {
+  return (
+    <div className="flex">
+      <button
+        className={cn(
+          "mr-1 h-6 w-6 rounded-full bg-[#d9d9d9]",
+          selected === option && "bg-brand",
+        )}
+        onClick={() => onSelect(option)}
+      />
+    </div>
+  );
 }
 
 function IssueOptions({ name }: { name: string }) {
-    const [selected, setSelected] = useState<string>("")
+  const [selected, setSelected] = useState<string>("");
 
-    const handlePresentingIssue = (option: string) => {
-        // TODO: API CALL - name is the presenting issue
-        let valueSelected = { name, option }
-        setSelected(option)
-    }
+  const handlePresentingIssue = (option: string) => {
+    // TODO: API CALL - name is the presenting issue
+    let valueSelected = { name, option };
+    setSelected(option);
+  };
 
-    return (
-        <div className="flex justify-between mt-2">
-            <div className="flex-1">
-                <Card className="shadow-none px-4 py-2 text-center rounded-sm w-fit" >
-                    <p className="text-xs text-brand font-medium min-w-[8.5rem]">
-                        {name}
-                    </p>
-                </Card>
-            </div>
-            <div className="flex flex-1 justify-between">
-                {
-                    ["No", "Low", "Med", "High"].map((option, i) => (
-                        <SingleIssueOption key={i} selected={selected} onSelect={handlePresentingIssue} option={option} />
-                    ))
-                }
-            </div>
-        </div>
-    )
+  return (
+    <div className="mt-2 flex justify-between">
+      <div className="flex-1">
+        <Card className="w-fit rounded-sm px-4 py-2 text-center shadow-none">
+          <p className="min-w-[8.5rem] text-xs font-medium text-brand">
+            {name}
+          </p>
+        </Card>
+      </div>
+      <div className="flex flex-1 justify-between">
+        {["No", "Low", "Med", "High"].map((option, i) => (
+          <SingleIssueOption
+            key={i}
+            selected={selected}
+            onSelect={handlePresentingIssue}
+            option={option}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
-
