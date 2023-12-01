8b6a538f4e28a66b9db963fc79e13471a1b54ec4
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2023-12-01 12:48:24 +0300

    use correct import for useFormState and fix type errors (#29)
    * fix import
    
    * use correct import for useFormState and fix type errors
    
    * fix add members page

--- app/(platform)/schools/[visibleId]/reschedule-dialog.tsx
+++ app/(platform)/schools/[visibleId]/reschedule-dialog.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useFormState } from "react-dom";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-// @ts-expect-error
-import { experimental_useFormState as useFormState } from "react-dom";
 
 import { inviteUserToImplementer } from "#/app/actions";
 import { Button } from "#/components/ui/button";
@@ -37,7 +36,7 @@ const FormSchema = z.object({
 });
 
 const initialState = {
-  message: null,
+  message: "",
 };
 
 export function RescheduleDialog({
