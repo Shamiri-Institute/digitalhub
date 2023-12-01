2857d9cdb1f5f445d8dba27dfedccf19cf1bad34
Shadrack Lilan <slilan96@gmail.com>
2023-12-01 12:31:52 +0300

    fix import

--- app/(platform)/schools/[visibleId]/reschedule-dialog.tsx
+++ app/(platform)/schools/[visibleId]/reschedule-dialog.tsx
@@ -3,8 +3,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-// @ts-expect-error
-import { experimental_useFormState as useFormState } from "react-dom";
+import { useFormState } from "react-dom";
 
 import { inviteUserToImplementer } from "#/app/actions";
 import { Button } from "#/components/ui/button";
