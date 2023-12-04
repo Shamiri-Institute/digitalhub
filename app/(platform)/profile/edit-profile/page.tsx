579d1d89ed3cab0c12c2caf9fdd2946b7950c04e
hinn254 <bennyhinnotieno@gmail.com>
2023-12-04 17:37:35 +0300

    Fix formatting in edit-profile page

--- app/(platform)/profile/edit-profile/page.tsx
+++ app/(platform)/profile/edit-profile/page.tsx
@@ -1,9 +1,7 @@
-
 import EditProfileBio from "#/app/(platform)/profile/edit-profile/components/edit-bio";
 import { currentSupervisor } from "#/app/auth";
 
 export default async function Page() {
-
   let supervisor = await currentSupervisor();
 
   return (
