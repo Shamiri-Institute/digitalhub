0051bdd5a762312b2ef778e4d0672c98092727f9
Shadrack Lilan <slilan96@gmail.com>
2023-12-07 10:43:58 +0300

    reintroduce styles

--- components/header.tsx
+++ components/header.tsx
@@ -21,7 +21,7 @@ export const Header = forwardRef<
     <motion.div
       ref={ref}
       className={cn(
-        "fixed inset-x-0 top-0 z-50 flex h-14 items-center justify-between gap-12 px-4 transition sm:px-6 lg:hidden",
+        "fixed inset-x-0 top-0 z-50 flex h-14 items-center justify-between gap-12 px-4 transition sm:px-6 lg:hidden lg:left-72 lg:z-30 lg:px-8 xl:left-80",
         "bg-background",
         className,
       )}
