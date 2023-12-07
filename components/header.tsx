904f718ddf6e06a2e5e18b4230d71b49b56f4491
Shadrack Lilan <slilan96@gmail.com>
2023-12-07 10:38:30 +0300

    SDH-50: fix issue where navigation bar would disappear between md and lg breakpoints

--- components/header.tsx
+++ components/header.tsx
@@ -21,7 +21,7 @@ export const Header = forwardRef<
     <motion.div
       ref={ref}
       className={cn(
-        "fixed inset-x-0 top-0 z-50 flex h-14 items-center justify-between gap-12 px-4 transition sm:px-6 md:hidden lg:left-72 lg:z-30 lg:px-8 xl:left-80",
+        "fixed inset-x-0 top-0 z-50 flex h-14 items-center justify-between gap-12 px-4 transition sm:px-6 lg:hidden",
         "bg-background",
         className,
       )}
