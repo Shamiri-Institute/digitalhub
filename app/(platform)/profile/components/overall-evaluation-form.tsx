295343e43437bc62bc233517cbb14af775e7c191
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2023-12-13 16:12:15 +0300

    add skeleton of the form

---
+++ app/(platform)/profile/components/overall-evaluation-form.tsx
@@ -0,0 +1,128 @@
+'use client';
+import React from 'react'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "#/components/ui/form";
+import { Textarea } from "#/components/ui/textarea";
+import { LineChart } from "recharts";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTrigger,
+} from "#/components/ui/dialog";
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { Separator } from '#/components/ui/separator';
+
+type Props = {
+  children: React.ReactNode;
+  fellowName: string;
+  fellowId: string;
+  supervisorId: string;
+  pastEvaluations: any[];
+}
+
+export default function FellowEvaluationForm(props: Props) {
+  const [open, setDialogOpen] = React.useState<boolean>(false)
+
+  const form = useForm();
+
+  return (
+    <Dialog open={open} onOpenChange={setDialogOpen}>
+      <DialogTrigger asChild>{props.children}</DialogTrigger>
+      <DialogContent>
+        <Form {...form}>
+          <form>
+            <DialogHeader>
+              <h2>{props.fellowName} Overall Report</h2>
+            </DialogHeader>
+            <Separator />
+            <div>
+              <div>Chart goes here</div>
+              <div>{props.pastEvaluations.length} Reporting Note{props.pastEvaluations.length === 1 ? '' : 's'}</div>
+              <div>
+                <p>Fellow Behaviour</p>
+                <FormField
+                  control={form.control}
+                  name="fellowBehaviourNotes"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Textarea
+                          className="resize-none"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div>
+                <p>Program delivery</p>
+                <FormField
+                  control={form.control}
+                  name="programDeliveryNotes"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Textarea
+                          className="resize-none"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div>
+                <p>Dressing and grooming</p>
+                <FormField
+                  control={form.control}
+                  name="dressingAndGroomingNotes"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Textarea
+                          className="resize-none"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div>
+                <p>Attendance</p>
+                <FormField
+                  control={form.control}
+                  name="attendanceNotes"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Textarea
+                          className="resize-none"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
