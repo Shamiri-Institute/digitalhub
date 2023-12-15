c4e2045927f3667bee8219f48884020dcbf7b3d0
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2023-12-15 17:06:32 +0300

    ensure that an empty string is not valid

--- app/(platform)/profile/schema.ts
+++ app/(platform)/profile/schema.ts
@@ -3,9 +3,12 @@ import * as z from "zod";
 export const ReportingNotesSchema = z.object({
   fellowId: z.string(),
   supervisorId: z.string(),
-  notes: z.string({
-    required_error: "Please input content in the notes field",
-  }),
+  notes: z
+    .string({
+      required_error: "Please input content in the notes field",
+    })
+    .trim()
+    .min(1, { message: "Please input content in the notes field" }),
 });
 
 export type ReportingNotesSchema = z.infer<typeof ReportingNotesSchema>;
