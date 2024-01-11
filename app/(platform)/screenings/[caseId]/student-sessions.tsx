9e508ef5715cdacf94bf5478586503a865f1700a
hinn254 <bennyhinnotieno@gmail.com>
2024-01-11 19:05:32 +0300

    Add Sessions component and SessionsCard component

---
+++ app/(platform)/screenings/[caseId]/student-sessions.tsx
@@ -0,0 +1,100 @@
+import { AddClinicalSessionDialog } from "#/app/(platform)/screenings/[caseId]/components/add-clinical-session";
+import { Button } from "#/components/ui/button";
+import { Card } from "#/components/ui/card";
+import { Separator } from "#/components/ui/separator";
+import { cn } from "#/lib/utils";
+import { ClinicalSessionAttendance } from "@prisma/client";
+
+// const sample_sessions_attended = [
+//   {
+//     name: "Nishan",
+//     session: "1",
+//     date: "20 Aug 2021",
+//   },
+//   {
+//     name: "Sweet",
+//     session: "4",
+//     date: "19 Jan 2021",
+//   },
+//   {
+//     name: "Orion",
+//     session: "3",
+//     date: "03 Aug 2021",
+//   },
+//   {
+//     name: "Esther",
+//     session: "2",
+//     date: "20 Feb 2021",
+//   },
+// ];
+
+type SessionsType = ClinicalSessionAttendance[]
+
+export function Sessions({ caseId, sessions, supervisorId }: { caseId: string, sessions: SessionsType[], supervisorId: string }) {
+  return (
+    <div className="flex flex-col">
+      <Card className={cn("pr-3.5bg-white  my-2 gap-5 p-4")}>
+        <div className="mb-2 flex justify-between">
+          <p className="text-base font-medium text-muted-foreground">No.</p>
+          <p className="text-base font-medium text-muted-foreground">
+            Session Attended
+          </p>
+          <p className="text-base font-medium text-muted-foreground">Date</p>
+        </div>
+        <Separator />
+        {/* {sessions.map((stud: SessionsType, index) => (
+          <SessionsCard
+            key={stud.id}
+            id={index}
+            session={stud.session}
+            date={stud.date}
+          />
+        ))} */}
+        <>
+          {
+            sessions.map((session, index) => (
+              <SessionsCard
+                key={session.id}
+                id={index}
+                session={session.session}
+                date={session.date}
+              />
+
+            ))
+          }
+        </>
+
+        {
+          sessions.length === 0 && <p className="text-center text-sm text-brand mt-2">No sessions attended</p>
+        }
+      </Card>
+      <div className="flex justify-end">
+        <AddClinicalSessionDialog caseId={caseId} supervisorId={supervisorId}>
+          <Button variant="brand" className="w-fit">
+            Add
+          </Button>
+        </AddClinicalSessionDialog>
+      </div>
+    </div>
+  );
+}
+
+function SessionsCard({
+  id,
+  session,
+  date,
+}: {
+  id: number;
+  session: string;
+  date: Date;
+}) {
+  return (
+    <div className="mt-2 flex  items-center justify-between border-b last:border-none">
+      <p className="flex-1 text-left text-sm text-brand ">{id + 1}</p>
+      <p className="flex-1 text-center text-sm  text-brand">{session}</p>
+      <p className="flex-1 text-right text-sm  text-brand">{
+        new Date(date).toLocaleDateString()
+      }</p>
+    </div>
+  );
+}
