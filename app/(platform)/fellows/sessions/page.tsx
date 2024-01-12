820583f2caaff91e598deb4d41233b1ab5fe3c93
EdmundKorley <13422991+EdmundKorley@users.noreply.github.com>
2024-01-12 11:57:29 -0500

    SDH-135: supervisor should be able to request repayments (#47)
    * repayment requests table
    
    * feat(profile): add RequestRepaymentDialog component to fellow-card.tsx
    
    * add to seed script
    
    * feat(fellow-card.tsx): pass fellow data to RequestRepaymentDialog for detailed view
    refactor(request-repayment-dialog.tsx): enhance dialog with fellow's repayment history and MPESA number validation warning for better user experience and error prevention
    
    * Add school select combobox
    
    * add session selector
    
    * Add button for requesting repayment
    
    * fix build
    
    * wip
    
    * Add global error handler
    
    https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#react-render-errors-in-app-router
    
    * Add repayment request functionality

--- app/(platform)/fellows/sessions/page.tsx
+++ app/(platform)/fellows/sessions/page.tsx
@@ -56,6 +56,28 @@ export default async function FellowSessionsPage({
           supervisor: true,
         },
       },
+      repaymentRequests: {
+        include: {
+          groupSession: {
+            include: {
+              session: {
+                include: {
+                  school: true,
+                },
+              },
+            },
+          },
+        },
+      },
+      groupSessions: {
+        include: {
+          session: {
+            include: {
+              school: true,
+            },
+          },
+        },
+      },
     },
   });
 
