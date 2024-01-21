715abd740e8fcb7cda905482d134cc1d05eda76a
Benny H. Otieno <bennyhinnotieno@gmail.com>
2024-01-21 16:24:05 +0300

    Benny/sdh-206-on-homepage-redirect-fellows-card-to-profile-page (#63)
    * Refactor: changefellow card to point to profile
    
    * Benny/sdh 207 school cards on school page not showing all the data and (#59)
    
    * Add session existence check function
    
    * Add session types and update school details on SchoolCard component
    
    * Refactor SchoolsList function to include additional school data
    
    * Add intervention session and student count to assigned school
    
    * Add doesSessionExist function import to school-card

--- app/(platform)/schools/page.tsx
+++ app/(platform)/schools/page.tsx
@@ -26,19 +26,6 @@ function Header() {
 }
 
 async function SchoolsList() {
-  const sessionTypes = ["Pre", "S1", "S2", "S3", "S4"];
-  // const assignedSchool = {
-  //   name: "Maranda Sec School",
-  //   population: 1400,
-  //   sessions: ["Pre", "S1"],
-  //   fellowsCount: 15,
-  //   type: "Public",
-  //   county: "Nairobi",
-  //   pointPerson: "Benny Otieno",
-  //   contactNo: "+254712342314780",
-  //   demographics: "Mixed",
-  // };
-  const assignedSchoolId = "ANS23_School_17";
   const supervisor = await currentSupervisor();
   if (!supervisor) {
     return <InvalidPersonnelRole role="supervisor" />;
@@ -47,8 +34,16 @@ async function SchoolsList() {
 
   const otherSchools = await db.school.findMany({
     where: {
-      visibleId: { not: assignedSchoolId },
+      visibleId: { not: assignedSchool.visibleId },
       hubId: assignedSchool.hubId,
+    },
+    include: {
+      interventionSessions: true,
+      _count: {
+        select: {
+          students: true,
+        },
+      },
     },
   });
 
@@ -58,9 +53,11 @@ async function SchoolsList() {
         <h2 className="py-3 text-xl font-semibold">My School</h2>
         <div className="grid grid-cols-1 gap-4 sm:gap-6 md:grid-cols-2 xl:grid-cols-3">
           <SchoolCard
-            key={assignedSchool.schoolName}
+            key={assignedSchool.id}
             school={assignedSchool}
-            sessionTypes={sessionTypes}
+            fellowsCount={supervisor.fellows.length}
+            sessionTypes={assignedSchool.interventionSessions}
+            studentCount={supervisor.assignedSchool._count.students}
             assigned
           />
           <div />
@@ -74,7 +71,8 @@ async function SchoolsList() {
             <SchoolCard
               key={school.schoolName}
               school={school}
-              sessionTypes={sessionTypes}
+              studentCount={school._count?.students}
+              sessionTypes={school.interventionSessions}
             />
           ))}
         </div>
