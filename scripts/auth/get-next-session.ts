e26a3ead4532787173ba94d142362fd623dbee59
EdmundKorley <13422991+EdmundKorley@users.noreply.github.com>
2024-01-24 10:39:09 +0300

    Session scheduling (#70)
    * tool for simulating another users session in development
    
    * Update supervisor view and session schedule
    
    * session schedule layout
    
    * Make schedule day of week clickable
    
    * add chevrons for paging through weeks
    
    * go to school attendance view
    
    * Refactor session schedule layout
    
    * fix(session-schedule.tsx): set anchorDate to current date instead of hardcoded date for better user experience
    feat(session-schedule.tsx): add visual indicator for days with sessions to improve user navigation
    
    * refactor(dashboard): remove hardcoded date from SessionSchedule component in page.tsx
    feat(dashboard): add sorting of sessions by date in session-schedule.tsx to ensure chronological order
    
    * fix bad import for test
    
    * rm console.log

---
+++ scripts/auth/get-next-session.ts
@@ -0,0 +1,41 @@
+import { db } from "#/lib/db";
+import { generateSessionToken } from "#/tests/helpers";
+
+async function main() {
+  const email = process.argv[2];
+  if (!email) {
+    console.error("Usage: npm run next-auth:session -- <email>");
+    process.exit(1);
+  }
+
+  const token = await generateSessionToken(email);
+
+  let futureDate = new Date();
+  futureDate.setFullYear(futureDate.getFullYear() + 1);
+  const futureTimestamp = Math.floor(futureDate.getTime() / 1000);
+
+  const cookie = {
+    name: "next-auth.session-token",
+    value: token,
+    domain: "localhost",
+    path: "/",
+    httpOnly: true,
+    sameSite: "Lax",
+    expires: futureTimestamp,
+  };
+
+  console.log(
+    `Add the following cookie to your browser to simulate being logged in as ${email}`,
+  );
+  console.log(JSON.stringify(cookie, null, 2));
+}
+
+main()
+  .then(async () => {
+    await db.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await db.$disconnect();
+    process.exit(1);
+  });
