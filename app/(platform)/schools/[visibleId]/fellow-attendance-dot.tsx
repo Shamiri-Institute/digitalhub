3e11671db35394a51f717df9248d3cc67ddfeb49
EdmundKorley <13422991+EdmundKorley@users.noreply.github.com>
2024-01-25 19:31:59 +0300

    Delayed payments (#74)
    * Delayed payment confirmation flow
    
    * test of edge cases
    
    * basic test
    
    * rm intervention_session.occuring_at
    
    * deplayed_payment_requests
    
    * Add supervisor to FellowCard component
    
    * add vitest workflow
    
    * add tests for other states
    
    * move unit test to specific folder
    
    e2e tests: *.spec.ts
    unit tests: *.test.{ts,tsx}
    
    * update playwright test dir

--- app/(platform)/schools/[visibleId]/fellow-attendance-dot.tsx
+++ app/(platform)/schools/[visibleId]/fellow-attendance-dot.tsx
@@ -1,15 +1,16 @@
 "use client";
 
+import { Prisma } from "@prisma/client";
+import { addDays, isBefore, setHours, setMinutes, startOfWeek } from "date-fns";
 import * as React from "react";
 
+import { submitDelayedPaymentRequest } from "#/app/(platform)/schools/[visibleId]/actions";
+import { AttendanceConfirmationDialog } from "#/app/(platform)/schools/[visibleId]/attendance-confirmation-dialog";
 import { markFellowAttendance } from "#/app/actions";
 import { useToast } from "#/components/ui/use-toast";
 import { cn } from "#/lib/utils";
 import type { AttendanceStatus, SessionLabel } from "#/types/app";
-import type {
-  FellowWithAttendance,
-  SchoolFindUniqueOutput,
-} from "#/types/prisma";
+import type { FellowWithAttendance } from "#/types/prisma";
 
 const dotColor = (status: AttendanceStatus) => ({
   "bg-[#85A070]": status === "present",
@@ -18,81 +19,217 @@ const dotColor = (status: AttendanceStatus) => ({
 });
 
 export function FellowAttendanceDot({
-  session,
+  sessionItem,
   fellow,
   school,
+  supervisor,
+  recordTime,
 }: {
-  session: { status: AttendanceStatus; label: SessionLabel };
+  sessionItem: {
+    status: AttendanceStatus;
+    label: SessionLabel;
+    session: Prisma.InterventionSessionGetPayload<{}> | null;
+  };
   fellow: FellowWithAttendance;
-  school: SchoolFindUniqueOutput;
+  school: Prisma.SchoolGetPayload<{}>;
+  supervisor: Prisma.SupervisorGetPayload<{}>;
+  recordTime: Date;
 }) {
   const { toast } = useToast();
-  const [status, setStatus] = React.useState(session.status);
+  const [status, setStatus] = React.useState(sessionItem.status);
 
-  const onDotClick = async () => {
+  const markAttendance = React.useCallback(
+    async (
+      nextStatus: AttendanceStatus,
+      sessionLabel: SessionLabel,
+      fellowVisibleId: string,
+      schoolVisibleId: string,
+    ): Promise<{ fellowAttendanceId: number } | undefined> => {
+      const response = await markFellowAttendance(
+        nextStatus,
+        sessionLabel,
+        fellowVisibleId,
+        schoolVisibleId,
+      );
+      if (response?.error) {
+        toast({
+          variant: "destructive",
+          title: response?.error,
+        });
+        return;
+      } else if (response.attendance) {
+        toast({
+          description: (
+            <div className="flex gap-1">
+              <span
+                className={cn(
+                  "mx-1 h-5 w-5 rounded-full transition-all active:scale-90",
+                  dotColor(nextStatus),
+                )}
+              ></span>
+              <span>
+                {nextStatus === "not-marked" ? (
+                  <span>
+                    <span className="font-bold">Unmarked</span>{" "}
+                    {sessionItem.label} for {fellow.fellowName}
+                  </span>
+                ) : (
+                  <span>
+                    Marked {sessionItem.label} as{" "}
+                    <span className="font-bold">{nextStatus}</span> for{" "}
+                    {fellow.fellowName}
+                  </span>
+                )}
+              </span>
+            </div>
+          ),
+        });
+
+        setStatus(nextStatus);
+
+        return { fellowAttendanceId: response.attendance.id };
+      } else {
+        toast({
+          variant: "destructive",
+          title: "Something went wrong",
+        });
+      }
+    },
+    [toast, dotColor, sessionItem.label, fellow.fellowName],
+  );
+
+  const [dialogOpen, setDialogOpen] = React.useState(false);
+
+  const onDialogSubmit = async () => {
     const nextStatus = nextAttendanceStatus(status);
 
-    const response = await markFellowAttendance(
+    const response = await markAttendance(
       nextStatus,
-      session.label,
+      sessionItem.label,
       fellow.visibleId,
       school.visibleId,
     );
-    if (response?.error) {
-      toast({
-        variant: "destructive",
-        title: response?.error,
-      });
-      return;
+
+    if (!response || !response?.fellowAttendanceId) {
+      throw Error(`No fellowAttendanceId returned from markAttendance`);
     }
 
-    if (response) {
-      toast({
-        description: (
-          <div className="flex gap-1">
-            <span
-              className={cn(
-                "mx-1 h-5 w-5 rounded-full transition-all active:scale-90",
-                dotColor(nextStatus),
-              )}
-            ></span>
-            <span>
-              {nextStatus === "not-marked" ? (
-                <span>
-                  <span className="font-bold">Unmarked</span> {session.label}{" "}
-                  for {fellow.fellowName}
-                </span>
-              ) : (
-                <span>
-                  Marked {session.label} as{" "}
-                  <span className="font-bold">{nextStatus}</span> for{" "}
-                  {fellow.fellowName}
-                </span>
-              )}
-            </span>
-          </div>
-        ),
-      });
-
-      setStatus(nextStatus);
-    } else {
-      toast({
-        variant: "destructive",
-        title: "Something went wrong",
-      });
+    if (!sessionItem.session) {
+      throw Error(`No session found for sessionItem`);
     }
+
+    await submitDelayedPaymentRequest({
+      fellowId: fellow.visibleId,
+      supervisorId: supervisor.id,
+      interventionSessionId: sessionItem.session?.id,
+      attendanceId: response.fellowAttendanceId,
+    });
+
+    setDialogOpen(false);
   };
+
+  function getNextCutoffDate(sessionDate: Date): Date {
+    const monday = 1;
+    const thursday = 4;
+    const cutoffHour = 9;
+
+    // Start of the current week (Monday)
+    let thisMonday = startOfWeek(sessionDate, { weekStartsOn: monday });
+    thisMonday = setHours(thisMonday, cutoffHour);
+    thisMonday = setMinutes(thisMonday, 0);
+
+    // Calculate this week's Thursday
+    let thisThursday = addDays(thisMonday, thursday - monday);
+    thisThursday = setHours(thisThursday, cutoffHour);
+    thisThursday = setMinutes(thisThursday, 0);
+
+    // If the session date is after this week's Thursday, calculate next week's Monday
+    if (sessionDate > thisThursday) {
+      let nextMonday = addDays(thisMonday, 7);
+      return nextMonday;
+    }
+
+    // If the session date is after this week's Monday but before or equal to this week's Thursday, return this week's Thursday
+    if (sessionDate > thisMonday && sessionDate <= thisThursday) {
+      return thisThursday;
+    }
+
+    // Otherwise, return this week's Monday
+    return thisMonday;
+  }
+
+  const isBeforePayoutCutoff = React.useCallback(() => {
+    if (!sessionItem.session?.sessionDate) {
+      return false;
+    }
+
+    const { sessionDate } = sessionItem.session;
+
+    const cutoffDate = getNextCutoffDate(sessionDate);
+
+    return isBefore(recordTime, cutoffDate);
+  }, [sessionItem.session, recordTime]);
+
+  const onDotClick = React.useCallback(async () => {
+    const nextStatus = nextAttendanceStatus(status);
+
+    if (status === "present") {
+      toast({
+        variant: "destructive",
+        title: "You cannot change attendance status after the cutoff",
+      });
+    } else if (nextStatus === "present" && !isBeforePayoutCutoff()) {
+      setDialogOpen(true);
+    } else {
+      await markAttendance(
+        nextStatus,
+        sessionItem.label,
+        fellow.visibleId,
+        school.visibleId,
+      );
+    }
+  }, [
+    status,
+    isBeforePayoutCutoff,
+    toast,
+    markAttendance,
+    sessionItem.label,
+    fellow.visibleId,
+    school.visibleId,
+  ]);
 
   return (
     <div className="flex flex-col items-center gap-1.5">
-      <div className="text-sm text-muted-foreground">{session.label}</div>
-      <button
-        onClick={onDotClick}
-        className={cn(
-          "mx-1 h-5 w-5 rounded-full transition-all active:scale-90",
-          dotColor(status),
-        )}
-      ></button>
+      <div className="text-sm text-muted-foreground">{sessionItem.label}</div>
+      <AttendanceConfirmationDialog
+        fellow={fellow}
+        attendanceInfo={{
+          sessionStatus: nextAttendanceStatus(status),
+          sessionLabel: sessionItem.label,
+          sessionDate: sessionItem.session?.sessionDate ?? null,
+          schoolName: school.schoolName,
+        }}
+        open={dialogOpen}
+        onOpenChange={(open: boolean) => {
+          if (open && !dialogOpen) {
+            return;
+          } else {
+            setDialogOpen(open);
+          }
+        }}
+        onSubmit={onDialogSubmit}
+      >
+        <button
+          onClick={onDotClick}
+          className={cn(
+            "mx-1 h-5 w-5 rounded-full transition-all active:scale-95",
+            dotColor(status),
+          )}
+          data-testid="attendance-dot"
+        >
+          <span className="hidden">{status}</span>
+        </button>
+      </AttendanceConfirmationDialog>
     </div>
   );
 }
