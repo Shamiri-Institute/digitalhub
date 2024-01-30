b1ce7e030865c727b2595b3694986faefb0e9cab
Edmund Korley <13422991+EdmundKorley@users.noreply.github.com>
2024-01-30 11:41:57 +0300

    only look at schools in current project

--- lib/actions/get-schools-for-supervisor.ts
+++ lib/actions/get-schools-for-supervisor.ts
@@ -1,15 +1,24 @@
 "use server";
 
 import { CurrentSupervisor } from "#/app/auth";
+import { CURRENT_PROJECT_ID } from "#/lib/constants";
 import { db } from "#/lib/db";
 
 export async function getSchoolsForSupervisor(
   supervisor: NonNullable<CurrentSupervisor>,
 ) {
+  const assignedSchools = await db.school.findMany({
+    where: {
+      assignedSupervisorId: supervisor.id,
+      hub: { projectId: CURRENT_PROJECT_ID },
+    },
+  });
   const fetchedSchools = await Promise.all(
-    supervisor.assignedSchools.map(async (school) => {
+    assignedSchools.map(async (school) => {
       const interventionSessions = await db.interventionSession.findMany({
-        where: { schoolId: school.id },
+        where: {
+          schoolId: school.id,
+        },
       });
       return { school, interventionSessions };
     }),
