e4d549b66b98876a26b1cb4b8bcdd10a3027ba0e
Edmund Korley <13422991+EdmundKorley@users.noreply.github.com>
2024-02-02 15:02:37 +0300

    Refactor payout calculation and generate route
    This commit refactors the `calculatePayouts` function and the `GET` route in the `payouts/generate` module. The changes include:
    - Renaming the `currentDate` parameter to `effectiveDate` for clarity.
    - Updating the start and end cutoff range calculations based on the `day` and `effectiveDate` parameters.
    - Improving variable names and code readability.
    - Updating the email content and attachment based on the new payout report structure.

--- app/api/payouts/generate/route.ts
+++ app/api/payouts/generate/route.ts
@@ -11,24 +11,29 @@ import { calculatePayouts } from "./calculate-payouts";
 export const revalidate = 0;
 
 export async function GET(request: NextRequest) {
-  const currentDate = new Date();
-
   const { searchParams } = request.nextUrl;
-  const dayParam = z
-    .object({ day: z.enum(["M", "R"]) })
-    .safeParse({ day: searchParams.get("day") });
-  if (!dayParam.success) {
+  const params = z
+    .object({
+      day: z.enum(["M", "R"]),
+      effectiveDate: z.date().optional().default(new Date()),
+    })
+    .safeParse({
+      day: searchParams.get("day"),
+      effectiveDate: searchParams.get("effectiveDate"),
+    });
+  if (!params.success) {
     return NextResponse.json({ error: "Invalid day" }, { status: 400 });
   }
+  const { day, effectiveDate } = params.data;
 
   try {
-    const payoutSummary = await calculatePayouts({
-      day: dayParam.data.day,
-      currentDate,
+    const payoutReport = await calculatePayouts({
+      day,
+      effectiveDate,
     });
 
     const csvStream = fastcsv.format({ headers: true });
-    payoutSummary.rows.forEach((row) => csvStream.write(row));
+    payoutReport.payoutDetails.forEach((row) => csvStream.write(row));
     csvStream.end();
 
     const chunks: Buffer[] = [];
@@ -47,17 +52,17 @@ export async function GET(request: NextRequest) {
           `From: "Shamiri Institute" <tech@shamiri.institute>\n` +
             `To: tech@shamiri.institute\n` +
             `Subject: Payouts for sessions ${format(
-              payoutSummary.cuttoffStartTime,
+              payoutReport.payoutPeriod.startDate,
               "yyyy-MM-dd",
-            )} to ${format(payoutSummary.cuttoffEndTime, "yyyy-MM-dd")}\n` +
+            )} to ${format(payoutReport.payoutPeriod.endDate, "yyyy-MM-dd")}\n` +
             `MIME-Version: 1.0\n` +
             `Content-Type: multipart/mixed; boundary="NextPart"\n\n` +
             `--NextPart\n` +
             `Content-Type: text/plain\n\n` +
             `Please find the attached payouts CSV.\n\n` +
-            `There were ${payoutsWithoutMpesaName} payouts without Mpesa names and ${payoutsWithoutMpesaNumber} payouts without Mpesa numbers.\n\n` +
-            `The total payout amount is KES ${totalPayoutAmount} and the total payout amount with Mpesa info present is KES ${totalPayoutAmountWithMpesaInfoPresent}.\n\n` +
-            `--NextPart\n` +
+            `There were ${payoutReport.incompleteRecords.countMissingMpesaName} payouts without Mpesa names and ${payoutReport.incompleteRecords.countMissingMpesaNumber} payouts without Mpesa numbers.\n\n` +
+            `The total payout amount is KES ${payoutReport.totalPayoutAmount} and the total payout amount with Mpesa info present is KES ${payoutReport.totalPayoutAmountWithMpesaInfo}.\n\n` +
+            `--NextPart--` +
             `Content-Type: text/csv; name="payouts.csv"\n` +
             `Content-Disposition: attachment; filename="payouts.csv"\n\n` +
             csvBuffer.toString() +
@@ -70,14 +75,17 @@ export async function GET(request: NextRequest) {
     await sendEmail({ emailInput, overrideEnv });
 
     return NextResponse.json({
-      message: `Tabulated ${payoutRows.length} payouts`,
-      currentTime: new Date(),
-      cuttoffStartTime,
-      cuttoffEndTime,
-      payoutsWithoutMpesaName,
-      payoutsWithoutMpesaNumber,
-      totalPayoutAmount,
-      totalPayoutAmountWithMpesaInfoPresent,
+      message: `Tabulated ${payoutReport.payoutDetails.length} payouts`,
+      effectiveDate,
+      payoutPeriodStartDate: payoutReport.payoutPeriod.startDate,
+      payoutPeriodEndDate: payoutReport.payoutPeriod.endDate,
+      payoutsWithoutMpesaName:
+        payoutReport.incompleteRecords.countMissingMpesaName,
+      payoutsWithoutMpesaNumber:
+        payoutReport.incompleteRecords.countMissingMpesaNumber,
+      totalPayoutAmount: payoutReport.totalPayoutAmount,
+      totalPayoutAmountWithMpesaInfoPresent:
+        payoutReport.totalPayoutAmountWithMpesaInfo,
     });
   } catch (error: unknown) {
     if (error instanceof Error) {
