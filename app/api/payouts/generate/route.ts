50cee495cdeb96107b57a17a47b559fd22395c07
Edmund Korley <13422991+EdmundKorley@users.noreply.github.com>
2024-02-02 14:37:48 +0300

    factor out calculate payouts

--- app/api/payouts/generate/route.ts
+++ app/api/payouts/generate/route.ts
@@ -1,123 +1,34 @@
 import { SendRawEmailCommandInput } from "@aws-sdk/client-ses";
-import {
-  addDays,
-  format,
-  setHours,
-  setMinutes,
-  startOfWeek,
-  subDays,
-} from "date-fns";
+import { format } from "date-fns";
 import * as fastcsv from "fast-csv";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
 import { constants } from "#/lib/constants";
-import { db } from "#/lib/db";
 import { sendEmailWithAttachment } from "#/lib/ses";
+import { calculatePayouts } from "./calculate-payouts";
 
 export const revalidate = 0;
 
 export async function GET(request: NextRequest) {
+  const currentDate = new Date();
+
+  const { searchParams } = request.nextUrl;
   const dayParam = z
     .object({ day: z.enum(["M", "R"]) })
-    .safeParse({ day: request.nextUrl.searchParams.get("day") });
+    .safeParse({ day: searchParams.get("day") });
   if (!dayParam.success) {
     return NextResponse.json({ error: "Invalid day" }, { status: 400 });
   }
 
   try {
-    const { day } = dayParam.data;
-    const isFebruary1st =
-      new Date().getMonth() === 1 && new Date().getDate() === 1;
-    const specialStartTimeForFeb1st = new Date(2024, 0, 23);
-    const cuttoffStartTime = isFebruary1st
-      ? specialStartTimeForFeb1st
-      : getStartCuttoffRange(day);
-    const cuttoffEndTime = getEndCuttoffRange(day);
-
-    const attendances = await db.fellowAttendance.findMany({
-      where: {
-        session: {
-          sessionDate: {
-            gte: cuttoffStartTime,
-            lt: cuttoffEndTime,
-          },
-          occurred: true,
-        },
-        attended: true,
-      },
-      include: {
-        fellow: true,
-        session: true,
-      },
+    const payoutSummary = await calculatePayouts({
+      day: dayParam.data.day,
+      currentDate,
     });
 
-    const payouts: {
-      [fellowVisibleId: string]: {
-        fellowVisibleId: string;
-        fellowName: string;
-        mpesaName: string;
-        mpesaNumber: string;
-        kesPayoutAmount: number;
-        presessionCount: number;
-        sessionCount: number;
-      };
-    } = {};
-
-    const filterSet = new Set();
-
-    for (const attendance of attendances) {
-      if (!payouts[attendance.fellow.visibleId]) {
-        payouts[attendance.fellow.visibleId] = {
-          fellowVisibleId: attendance.fellow.visibleId,
-          fellowName: attendance.fellow.fellowName ?? "N/A",
-          mpesaName: attendance.fellow.mpesaName ?? "N/A",
-          mpesaNumber: attendance.fellow.mpesaNumber ?? "N/A",
-          kesPayoutAmount: 0,
-          presessionCount: 0,
-          sessionCount: 0,
-        };
-      }
-      const sessionType = attendance.session?.sessionType;
-      const payout = payouts[attendance.fellow.visibleId];
-      const filterKey = `${attendance.fellow.visibleId}-${attendance.schoolId}-${attendance.session?.sessionType}`;
-
-      if (sessionType && payout) {
-        if (sessionType === "s0") {
-          if (!filterSet.has(filterKey)) {
-            payout.kesPayoutAmount += 500;
-            payout.presessionCount += 1;
-            filterSet.add(filterKey);
-          }
-        } else if (["s1", "s2", "s3", "s4"].includes(sessionType)) {
-          if (!filterSet.has(filterKey)) {
-            payout.kesPayoutAmount += 1500;
-            payout.sessionCount += 1;
-            filterSet.add(filterKey);
-          }
-        } else {
-          throw new Error("Invalid session type");
-        }
-      }
-    }
-    const payoutRows = Object.values(payouts);
-
-    const payoutsWithoutMpesaNumber = payoutRows.filter(
-      (payout) => !payout.mpesaNumber,
-    ).length;
-    const payoutsWithoutMpesaName = payoutRows.filter(
-      (payout) => !payout.mpesaName,
-    ).length;
-    const totalPayoutAmount = payoutRows.reduce(
-      (acc, payout) => acc + payout.kesPayoutAmount,
-      0,
-    );
-    const totalPayoutAmountWithMpesaInfoPresent = payoutRows
-      .filter((payout) => payout.mpesaNumber && payout.mpesaName)
-      .reduce((acc, payout) => acc + payout.kesPayoutAmount, 0);
-
     const csvStream = fastcsv.format({ headers: true });
-    payoutRows.forEach((row) => csvStream.write(row));
+    payoutSummary.rows.forEach((row) => csvStream.write(row));
     csvStream.end();
 
     const chunks: Buffer[] = [];
@@ -136,9 +47,9 @@ export async function GET(request: NextRequest) {
           `From: "Shamiri Institute" <tech@shamiri.institute>\n` +
             `To: tech@shamiri.institute\n` +
             `Subject: Payouts for sessions ${format(
-              cuttoffStartTime,
+              payoutSummary.cuttoffStartTime,
               "yyyy-MM-dd",
-            )} to ${format(cuttoffEndTime, "yyyy-MM-dd")}\n` +
+            )} to ${format(payoutSummary.cuttoffEndTime, "yyyy-MM-dd")}\n` +
             `MIME-Version: 1.0\n` +
             `Content-Type: multipart/mixed; boundary="NextPart"\n\n` +
             `--NextPart\n` +
@@ -155,7 +66,8 @@ export async function GET(request: NextRequest) {
       },
     };
 
-    await sendEmail(request, emailInput);
+    const overrideEnv = searchParams.get("sendEmail") === "true";
+    await sendEmail({ emailInput, overrideEnv });
 
     return NextResponse.json({
       message: `Tabulated ${payoutRows.length} payouts`,
@@ -179,60 +91,16 @@ export async function GET(request: NextRequest) {
   }
 }
 
-const cutoffHour = 6; // 6am UTC / 9am EAT
-const cutoffMinute = 0;
-
-async function sendEmail(
-  request: NextRequest,
-  emailInput: SendRawEmailCommandInput,
-) {
-  if (
-    constants.NEXT_PUBLIC_ENV === "production" ||
-    request.nextUrl.searchParams.get("sendEmail") === "true"
-  ) {
+async function sendEmail({
+  emailInput,
+  overrideEnv,
+}: {
+  emailInput: SendRawEmailCommandInput;
+  overrideEnv: boolean;
+}) {
+  if (constants.NEXT_PUBLIC_ENV === "production" || overrideEnv) {
     await sendEmailWithAttachment(emailInput);
   } else {
     console.warn("EMAIL NOT SENT OUTSIDE OF PRODUCTION:", emailInput);
-  }
-}
-
-function getStartCuttoffRange(day: "R" | "M"): Date {
-  switch (day) {
-    case "R":
-      let thisMonday = startOfWeek(new Date(), { weekStartsOn: 1 });
-
-      thisMonday = setHours(thisMonday, cutoffHour);
-      thisMonday = setMinutes(thisMonday, cutoffMinute);
-      return thisMonday;
-    case "M":
-      let lastMonday = startOfWeek(subDays(new Date(), 7), {
-        weekStartsOn: 1,
-      });
-
-      let lastThursday = setHours(lastMonday, cutoffHour);
-      lastThursday = setMinutes(lastThursday, cutoffMinute);
-      lastThursday = addDays(lastThursday, 3); // Move to Thursday
-      return lastThursday;
-    default:
-      throw new Error("Invalid day");
-  }
-}
-
-function getEndCuttoffRange(day: "R" | "M"): Date {
-  switch (day) {
-    case "R":
-      let thisThursday = startOfWeek(new Date(), { weekStartsOn: 1 });
-      thisThursday = setHours(thisThursday, cutoffHour);
-      thisThursday = setMinutes(thisThursday, cutoffMinute);
-      thisThursday = addDays(thisThursday, 3); // Move to Thursday
-
-      return thisThursday;
-    case "M":
-      let thisMonday = startOfWeek(new Date(), { weekStartsOn: 1 });
-      thisMonday = setHours(thisMonday, cutoffHour);
-      thisMonday = setMinutes(thisMonday, cutoffMinute);
-      return thisMonday;
-    default:
-      throw new Error("Invalid day");
   }
 }
