5d25db276830fb4bdaef2ae16ade2a8081ac5d88
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2024-03-01 09:48:35 +0300

    add new layout based on new designs for hub-coordinator flows (#89)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { getServerSession } from "next-auth";
-import { Inter } from "next/font/google";
+import { Figtree, Inter } from "next/font/google";
 
 import { Providers } from "#/components/providers";
 import { cn } from "#/lib/utils";
@@ -8,6 +8,8 @@ import { cn } from "#/lib/utils";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const figtree = Figtree({ subsets: ["latin"], weight: ["600"] });
 
 export const metadata: Metadata = {
   title: {
@@ -26,7 +28,13 @@ export default async function RootLayout({
 
   return (
     <html lang="en" className="h-full">
-      <body className={cn(inter.className, "flex min-h-full antialiased")}>
+      <body
+        className={cn(
+          inter.className,
+          figtree.className,
+          "flex min-h-full antialiased",
+        )}
+      >
         <Providers session={session}>
           <div className="w-full">{children}</div>
         </Providers>
