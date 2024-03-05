d229717ac0c03f5c43d86b5dd0e8e9b8773a03fe
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2024-03-05 11:07:48 +0300

    add role switcher logic to allow switching between the new and old design

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,11 +3,17 @@
 import { motion } from "framer-motion";
 import Link from "next/link";
 
+import { PersonnelTool } from "#/app/dev-personnel-switcher";
 import { Footer } from "#/components/footer";
 import { Header } from "#/components/header";
 import { Icons } from "#/components/icons";
 import { Navigation } from "#/components/navigation";
-import { AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "#/components/ui/popover";
+import { constants } from "#/lib/constants";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import ArrowDropdown from "../public/icons/arrow-drop-down.svg";
@@ -20,138 +26,12 @@ import NotificationIcon from "../public/icons/notification-icon.svg";
 import PeopleIconAlternate from "../public/icons/people-icon-alternate.svg";
 import PeopleIcon from "../public/icons/people-icon.svg";
 import SchoolIcon from "../public/icons/school-icon.svg";
-import { Avatar } from "./ui/avatar";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
 
-  // TODO: need to ensure that this screens for other prefix routes e.g. /op /sc etc
   if (pathname.startsWith("/hc")) {
-    return (
-      <>
-        <header className="bg-background-secondary">
-          <div className="flex items-center justify-between px-4">
-            <div className="px-3">
-              <Link href="#" className="text-xl text-shamiri-new-blue">
-                Shamiri Hub
-              </Link>
-            </div>
-            <div className="flex">
-              <div className="mr-8 flex flex-none items-center gap-2 py-2">
-                <Image
-                  unoptimized
-                  priority
-                  src={FeedbackIcon}
-                  alt="Feedback Icon"
-                  width={24}
-                  height={24}
-                />
-                <Link href="#">Feedback</Link>
-              </div>
-              <div className="mr-8 flex flex-none items-center gap-2 py-2">
-                <Image
-                  unoptimized
-                  priority
-                  src={HelpIcon}
-                  alt="Help Icon"
-                  width={24}
-                  height={24}
-                />
-                <Link href="#">Help</Link>
-              </div>
-              <div className="flex flex-none items-center gap-6">
-                <div className="relative">
-                  <Image
-                    unoptimized
-                    priority
-                    src={NotificationIcon}
-                    alt="Notification Icon"
-                    width={24}
-                    height={24}
-                  />
-                  <span className="absolute -end-[6px] -top-[1px] rounded-[96px] bg-shamiri-light-red px-1 text-[10px] text-white">
-                    12
-                  </span>
-                </div>
-                <div className="flex flex-none items-center gap-2">
-                  {/* TODO: figure out which parts of the component should be clickable */}
-                  <Avatar className="h-8 w-8">
-                    <AvatarImage src="https://avatars.githubusercontent.com/u/140525342?s=200&v=4" />
-                    <AvatarFallback>SI</AvatarFallback>
-                  </Avatar>
-                  <p>Leroy Jenkins</p>
-                  <Image
-                    unoptimized
-                    priority
-                    src={ArrowDropdown}
-                    alt="Profile/Setting arrow drop down icon"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="no-scrollbar flex gap-8 overflow-x-auto px-6">
-            {/*TODO: the items in this list should depend on the user's role */}
-            <div className="flex flex-none items-center gap-2 py-2">
-              <Image
-                unoptimized
-                priority
-                src={CalendarIcon}
-                alt="Schedule Icon"
-              />
-              <Link href="/hc/schedule">Schedule</Link>
-            </div>
-            <div className="flex flex-none items-center gap-2 py-2">
-              <Image unoptimized priority src={SchoolIcon} alt="School Icon" />
-              <Link href="/hc/schools">Schools</Link>
-            </div>
-            <div className="flex flex-none items-center gap-2 py-2">
-              <Image
-                unoptimized
-                priority
-                src={PeopleIcon}
-                alt="Supervisors Icon"
-              />
-              <Link href="/hc/supervisors">Supervisors</Link>
-            </div>
-            <div className="flex flex-none items-center gap-2 py-2">
-              <Image
-                unoptimized
-                priority
-                src={PeopleIconAlternate}
-                alt="Fellow Icon"
-              />
-              <Link href="/hc/fellows">Fellows</Link>
-            </div>
-            <div className="flex flex-none items-center gap-2 py-2">
-              <Image
-                unoptimized
-                priority
-                src={GraduationCapIcon}
-                alt="Students icon"
-              />
-              <Link href="/hc/students">Students</Link>
-            </div>
-            <div className="flex flex-none items-center gap-2 py-2">
-              <Image
-                unoptimized
-                priority
-                src={BarchartIcon}
-                alt="Reporting icon"
-              />
-              <Link href="/hc/reporting">Reporting</Link>
-              <Image
-                unoptimized
-                priority
-                src={ArrowDropdown}
-                alt="Drop down Icon"
-              />
-            </div>
-          </div>
-        </header>
-        {children}
-      </>
-    );
+    return <LayoutV2>{children}</LayoutV2>;
   }
 
   return (
@@ -175,5 +55,144 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Footer />
       </div>
     </div>
+  );
+}
+
+function LayoutV2({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <header className="bg-background-secondary">
+        <div className="flex items-center justify-between px-4">
+          <div className="px-3">
+            <Link href="#" className="text-xl text-shamiri-new-blue">
+              Shamiri Hub
+            </Link>
+          </div>
+          <div className="flex items-center">
+            <PersonnelToolPopover>
+              <div className="mr-8 text-shamiri-new-blue">Switch Role</div>
+            </PersonnelToolPopover>
+            <div className="mr-8 flex items-center gap-2 py-2">
+              <Image
+                unoptimized
+                priority
+                src={FeedbackIcon}
+                alt="Feedback Icon"
+                width={24}
+                height={24}
+              />
+              <Link href="#">Feedback</Link>
+            </div>
+            <div className="mr-8 flex items-center gap-2 py-2">
+              <Image
+                unoptimized
+                priority
+                src={HelpIcon}
+                alt="Help Icon"
+                width={24}
+                height={24}
+              />
+              <Link href="#">Help</Link>
+            </div>
+            <div className="flex items-center gap-6">
+              <Image
+                unoptimized
+                priority
+                src={NotificationIcon}
+                alt="Notification Icon"
+                width={24}
+                height={24}
+              />
+              {/*TODO: notification counter */}
+              <div className="flex space-x-2">
+                {/* TODO: figure out which parts of the component should be clickable */}
+                <div>{/* TODO: avatar icon */}A</div>
+                <p>Leroy Jenkins</p>
+                <Image
+                  unoptimized
+                  priority
+                  src={ArrowDropdown}
+                  alt="Profile/Setting arrow drop down icon"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="no-scrollbar flex gap-8 overflow-x-auto px-6">
+          {/*TODO: the items in this list should depend on the user's role */}
+          <div className="flex gap-2 py-2">
+            <Image
+              unoptimized
+              priority
+              src={CalendarIcon}
+              alt="Schedule Icon"
+            />
+            <Link href="/hc/schedule">Schedule</Link>
+          </div>
+          <div className="flex gap-2 py-2">
+            <Image unoptimized priority src={SchoolIcon} alt="School Icon" />
+            <Link href="/hc/schools">Schools</Link>
+          </div>
+          <div className="flex gap-2 py-2">
+            <Image
+              unoptimized
+              priority
+              src={PeopleIcon}
+              alt="Supervisors Icon"
+            />
+            <Link href="/hc/supervisors">Supervisors</Link>
+          </div>
+          <div className="flex gap-2 py-2">
+            <Image
+              unoptimized
+              priority
+              src={PeopleIconAlternate}
+              alt="Fellow Icon"
+            />
+            <Link href="/hc/fellows">Fellows</Link>
+          </div>
+          <div className="flex gap-2 py-2">
+            <Image
+              unoptimized
+              priority
+              src={GraduationCapIcon}
+              alt="Students icon"
+            />
+            <Link href="/hc/students">Students</Link>
+          </div>
+          <div className="flex gap-2 py-2">
+            <Image
+              unoptimized
+              priority
+              src={BarchartIcon}
+              alt="Reporting icon"
+            />
+            <Link href="/hc/reporting">Reporting</Link>
+            <Image
+              unoptimized
+              priority
+              src={ArrowDropdown}
+              alt="Drop down Icon"
+            />
+          </div>
+        </div>
+      </header>
+      {children}
+    </>
+  );
+}
+
+function PersonnelToolPopover({ children }: { children: React.ReactNode }) {
+  if (constants.NEXT_PUBLIC_ENV === "production") {
+    return null;
+  }
+
+  return (
+    <Popover>
+      <PopoverTrigger>{children}</PopoverTrigger>
+      <PopoverContent className="-m-px p-0 shadow-none">
+        <PersonnelTool />
+      </PopoverContent>
+    </Popover>
   );
 }
--- lib/middleware/app.ts
+++ lib/middleware/app.ts
@@ -1,4 +1,5 @@
-import { getToken } from "next-auth/jwt";
+import { ImplementerRole } from "@prisma/client";
+import { JWT, getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 import { parse } from "#/lib/middleware/utils";
@@ -6,6 +7,8 @@ import { parse } from "#/lib/middleware/utils";
 export default async function AppMiddleware(req: NextRequest) {
   const { path } = parse(req);
   const session = await getToken({ req });
+
+  // Check if authenticated and accessing authorized route
   if (!session?.email && path !== "/login" && path !== "/register") {
     console.warn(`No email in session`, {
       session,
@@ -18,8 +21,31 @@ export default async function AppMiddleware(req: NextRequest) {
       ),
     );
   } else if (session?.email) {
-    if (path === "/login" || path === "/register") {
+    if (path === "/login") {
+      if (ifHcUserAndUnprefixedPath(session, path)) {
+        return NextResponse.redirect(new URL("/hc", req.url));
+      }
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+
+    if (ifHcUserAndUnprefixedPath(session, path)) {
+      return NextResponse.redirect(new URL("/hc", req.url));
+    } else if (ifSupervisorAndHcRoute(session, path)) {
       return NextResponse.redirect(new URL("/", req.url));
     }
   }
+}
+
+function ifHcUserAndUnprefixedPath(session: JWT | null, path: string) {
+  return (
+    session?.activeMembership?.role === 'HUB_COORDINATOR' &&
+    !path.startsWith("/hc")
+  );
+}
+
+function ifSupervisorAndHcRoute(session: JWT | null, path: string) {
+  return (
+    session?.activeMembership?.role === 'SUPERVISOR' &&
+    path.startsWith("/hc")
+  );
 }
