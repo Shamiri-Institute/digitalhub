f23fbd7601d10df3f80e02505fb536473b86b4c2
hinn254 <bennyhinnotieno@gmail.com>
2024-03-12 17:59:30 +0300

    Refactor case notes plan component

--- app/(platform)/screenings/[caseId]/case-notes-plan.tsx
+++ app/(platform)/screenings/[caseId]/case-notes-plan.tsx
@@ -1,14 +1,11 @@
 "use client";
+import { CaseReports } from "#/app/(platform)/screenings/[caseId]/components/case-reports-upload";
+import { TreatmentPlan } from "#/app/(platform)/screenings/[caseId]/components/treatment-plan";
 import { GenerateProgressNotes } from "#/app/(platform)/screenings/[caseId]/generate_progress_notes";
 import { CurrentCase } from "#/app/(platform)/screenings/screen";
-import { Icons } from "#/components/icons";
-import { Button } from "#/components/ui/button";
 import { Card } from "#/components/ui/card";
-import { Input } from "#/components/ui/input";
 import { cn } from "#/lib/utils";
 import { useState } from "react";
-import { TreatmentPlan } from "#/app/(platform)/screenings/[caseId]/components/treatment-plan";
-
 
 export function CaseNotePlan({
   currentSupId,
@@ -78,18 +75,5 @@ function CaseNotePlanOption({
     >
       <p className="text-xs font-medium text-brand">{option}</p>
     </button>
-  );
-}
-
-
-function CaseReports({ currentcase }: { currentcase: CurrentCase }) {
-  return (
-    <>
-      <Input id="csv-file" name="csv-file" type="file" accept="text/csv" />
-      <Button className="hover:bg-shamiri-brand mt-2 w-full rounded-sm bg-shamiri-blue px-3 py-2 text-white">
-        <Icons.upload className="mr-2 h-4 w-4" />
-        Upload File
-      </Button>
-    </>
   );
 }
