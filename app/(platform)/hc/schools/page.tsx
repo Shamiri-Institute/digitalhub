af0379789783940834a27bad16c619d0d2082f12
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2024-03-19 14:48:59 +0300

    add schools section layout and initial functionality to the the hub coordinator workflows (#93)
    - Flesh out layout
    - Add data table support with ability to adjust columns
    - Add placeholders for graphs
    - Modify seed data to have > 1 school

---
+++ app/(platform)/hc/schools/page.tsx
@@ -0,0 +1,93 @@
+import { currentHubCoordinator } from "#/app/auth";
+import { Button } from "#/components/ui/button";
+import { Command, CommandInput } from "#/components/ui/command";
+import PageHeading from "#/components/ui/page-heading";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "#/components/ui/select";
+import { Separator } from "#/components/ui/separator";
+import Image from "next/image";
+import AddCircleOutlined from "../../../../public/icons/add-circle-outline.svg";
+import {
+  fetchDropoutReasons,
+  fetchSchoolData,
+  fetchSessionAttendanceData,
+} from "./actions";
+import ChartArea from "./components/chart-area";
+import { columns } from "./components/columns";
+import SchoolsDataTable from "./components/data-table";
+
+export default async function SchoolsPage() {
+  const hubCoordinator = await currentHubCoordinator();
+  // TODO: convert this to Promise.all for concurrent fetch
+  const data = await fetchSchoolData(hubCoordinator?.assignedHubId as string);
+  const dropoutData = await fetchDropoutReasons(
+    hubCoordinator?.assignedHubId as string,
+  );
+  const sessionAttendanceData = await fetchSessionAttendanceData(
+    hubCoordinator?.assignedHubId as string,
+  );
+
+  return (
+    <>
+      <PageHeading title="Schools" />
+      <Separator className="my-5" />
+      <div className="flex items-center justify-between">
+        <div className="flex gap-3">
+          <Command className="max-w-64 rounded border">
+            <CommandInput placeholder="Search Schools" />
+            {/*
+            <CommandList>
+              <CommandEmpty>No schools found</CommandEmpty>
+              {dummySchools.map(sch => (
+                <CommandItem>
+                  {sch}
+                </CommandItem>
+              ))}
+            </CommandList>
+            */}
+          </Command>
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder="Filter by" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="hub">Hub</SelectItem>
+                <SelectItem value="supervisor">Supervisor</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="flex items-center gap-3">
+          <Button className="flex items-center gap-2 bg-shamiri-new-blue text-base font-semibold leading-6 text-white">
+            <Image
+              unoptimized
+              priority
+              src={AddCircleOutlined}
+              alt="Add icon circle outlined"
+              width={24}
+              height={24}
+            />
+            Weekly Hub Report
+          </Button>
+          {/* TODO: dispaly options button */}
+        </div>
+      </div>
+      {/* TODO: better to use grid here for responsive views */}
+      <ChartArea
+        dropoutData={dropoutData}
+        sessionAttendanceData={sessionAttendanceData}
+      />
+      <Separator />
+      <div className="pt-5">
+        <SchoolsDataTable data={data} columns={columns} />
+      </div>
+    </>
+  );
+}
