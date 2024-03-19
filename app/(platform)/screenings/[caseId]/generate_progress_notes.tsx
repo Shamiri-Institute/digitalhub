db3a56de6db0e45211fff089a9aecd716f7b330e
Benny H. Otieno <bennyhinnotieno@gmail.com>
2024-03-19 12:58:40 +0300

    Updated progress notes from generate to upload (#104)
    * Updated from generate file to upload file
    
    * Fix disabled state in file upload button - prevent rage clicks
    
    * Add route for uploading progress notes to Google Drive

--- app/(platform)/screenings/[caseId]/generate_progress_notes.tsx
+++ app/(platform)/screenings/[caseId]/generate_progress_notes.tsx
@@ -1,57 +1,72 @@
+import { readFileContent } from "#/app/(platform)/screenings/[caseId]/components/treatment-plan";
 import { CurrentCase } from "#/app/(platform)/screenings/screen";
-import { createSupProgressNoteToGDriveAndSaveOnDb } from "#/commands/google-drive-actions";
 import { Icons } from "#/components/icons";
 import { Button } from "#/components/ui/button";
+import { Input } from "#/components/ui/input";
 import { useToast } from "#/components/ui/use-toast";
 import { Loader2 } from "lucide-react";
 import Link from "next/link";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 export function GenerateProgressNotes({
   currentcase,
 }: {
   currentcase: CurrentCase;
 }) {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [uploading, setUploading] = useState(false);
   const { toast } = useToast();
-  const [loading, setLoading] = useState(false);
 
-  const handleFileGeneration = async () => {
-    if (
-      !currentcase.currentSupervisor ||
-      !currentcase.currentSupervisor.supervisorEmail ||
-      !currentcase.currentSupervisor.supervisorName
-    ) {
-      toast({
-        variant: "destructive",
-        title: "Error creating case. No supervisor found.",
-      });
+  function handleFileUpload(event: ChangeEvent<HTMLInputElement>) {
+    if (!event?.target?.files) return;
+
+    const file = event.target.files[0];
+
+    if (file) {
+      setSelectedFile(file);
+    }
+  }
+
+  const handleUpload = async () => {
+    if (!selectedFile) {
+      toast({ title: "No file selected", variant: "destructive" });
+      console.error("No file selected");
       return;
     }
 
+    const formData = new FormData();
+
+    formData.append("studentId", currentcase.student.id);
+    formData.append("caseId", currentcase.id);
+
+    const fileContent = await readFileContent(selectedFile);
+    formData.append("file", fileContent, selectedFile.name);
+
     try {
-      setLoading(true);
-      await createSupProgressNoteToGDriveAndSaveOnDb({
-        caseId: currentcase.id,
-        studentId: currentcase.student.id,
-        supervisorEmail: currentcase?.currentSupervisor?.supervisorEmail,
-        supervisorName: currentcase?.currentSupervisor?.supervisorName,
+      setUploading(true);
+
+      const resp = await fetch("/api/files/gdrive/progress-notes", {
+        method: "POST",
+        body: formData,
       });
 
-      toast({
-        variant: "default",
-        title: "Progress Notes Generated Successfully",
-      });
-
+      if (resp.ok) {
+        toast({ title: "File uploaded successfully", variant: "default" });
+      }
       window.location.href = `/screenings/${currentcase.id}`;
     } catch (error) {
-      console.log(error);
+      console.error("Failed to upload file:", error);
+      toast({
+        title: "Failed to upload file. Please try again.",
+        variant: "destructive",
+      });
     } finally {
-      setLoading(false);
+      setUploading(false);
     }
   };
 
   return (
-    <div>
+    <>
       {currentcase.progressNotes ? (
         <div>
           <p className="text-sm text-gray-500">Progress Notes Link</p>
@@ -64,21 +79,44 @@ export function GenerateProgressNotes({
           </Link>
         </div>
       ) : (
-        <Button
-          className="hover:bg-shamiri-brand w-full rounded-sm bg-shamiri-blue px-3 py-2 text-white"
-          onClick={handleFileGeneration}
-        >
-          {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-          {loading ? (
-            "Generating file..."
-          ) : (
+        <div>
+          <Input
+            className="cursor-pointer"
+            id="docx-file"
+            name="docx-file"
+            type="file"
+            accept=".doc, .docx, application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+            onChange={handleFileUpload}
+          />
+
+          {selectedFile && (
             <>
-              <Icons.upload className="mr-2 h-4 w-4" />
-              Generate File
+              <p className="mt-2 text-sm text-gray-500">
+                Selected file: {selectedFile.name}
+              </p>
+              <p className="mt-2 text-sm text-gray-500">
+                File size: {selectedFile.size} bytes
+              </p>
             </>
           )}
-        </Button>
+          <Button
+            disabled={!selectedFile || uploading}
+            onClick={handleUpload}
+            type="submit"
+            className="hover:bg-shamiri-brand mt-2 w-full rounded-sm bg-shamiri-blue px-3 py-2 text-white"
+          >
+            {uploading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+            {uploading ? (
+              "Uploading file..."
+            ) : (
+              <>
+                <Icons.upload className="mr-2 h-4 w-4" />
+                Upload File
+              </>
+            )}
+          </Button>
+        </div>
       )}
-    </div>
+    </>
   );
 }
