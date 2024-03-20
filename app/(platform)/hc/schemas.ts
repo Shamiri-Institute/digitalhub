ee15a4e64a96e171e22d3ed910fa7c859ae18303
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2024-03-20 19:18:36 +0300

    allow hub coordinator to drop schools (#107)

---
+++ app/(platform)/hc/schemas.ts
@@ -0,0 +1,14 @@
+import { SCHOOL_DROPOUT_REASONS } from "#/lib/app-constants/constants";
+import { stringValidation } from "#/lib/utils";
+import { z } from "zod";
+
+export const DropoutSchoolSchema = z.object({
+  schoolId: stringValidation("Missing school ID"),
+  // @ts-ignore
+  dropoutReason: z.enum(SCHOOL_DROPOUT_REASONS, {
+    errorMap: (_issue, _ctx) => ({
+      message:
+        "Please select one of the supplied school dropout reason options",
+    }),
+  }),
+});
