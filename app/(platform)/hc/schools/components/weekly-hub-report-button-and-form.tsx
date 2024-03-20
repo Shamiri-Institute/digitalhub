306d5d06b6fc54b5d19e56f15948519eb19b2b52
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2024-03-20 19:54:42 +0300

    flesh out add weekly hub report form

--- app/(platform)/hc/schools/components/weekly-hub-report-button-and-form.tsx
+++ app/(platform)/hc/schools/components/weekly-hub-report-button-and-form.tsx
@@ -1,21 +1,45 @@
 "use client";
 
 import { Button } from "#/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTrigger,
+} from "#/components/ui/dialog";
+import { Separator } from "#/components/ui/separator";
 import Image from "next/image";
+import { useState } from "react";
 import AddCircleOutlined from "../../../../../public/icons/add-circle-outline.svg";
 
 export default function WeeklyHubReportButtonAndForm() {
+  const [open, setDialogOpen] = useState<boolean>(false);
   return (
-    <Button className="flex items-center gap-2 bg-shamiri-new-blue text-base font-semibold leading-6 text-white">
-      <Image
-        unoptimized
-        priority
-        src={AddCircleOutlined}
-        alt="Add icon circle outlined"
-        width={24}
-        height={24}
-      />
-      Weekly Hub Report
-    </Button>
+    <Dialog open={open} onOpenChange={setDialogOpen}>
+      <DialogTrigger asChild>
+        <Button className="flex items-center gap-2 bg-shamiri-new-blue text-base font-semibold leading-6 text-white">
+          <Image
+            unoptimized
+            priority
+            src={AddCircleOutlined}
+            alt="Add icon circle outlined"
+            width={24}
+            height={24}
+          />
+          Weekly Hub Report
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <h2>Hello world</h2>
+        </DialogHeader>
+        <Separator />
+        <div>Hello world</div>
+        <DialogFooter>
+          <div>Close footer</div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
