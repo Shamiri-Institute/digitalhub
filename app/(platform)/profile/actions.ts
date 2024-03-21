f9b58b7693f1ab5d0fbc02ee1e1d22ea83573513
hinn254 <bennyhinnotieno@gmail.com>
2024-03-21 11:47:48 +0300

    Update action inputs

--- app/(platform)/profile/actions.ts
+++ app/(platform)/profile/actions.ts
@@ -1,6 +1,5 @@
 "use server";
 
-import { RatingState } from "#/app/(platform)/profile/components/weekly-fellow-evaluation-form";
 import { db } from "#/lib/db";
 import { revalidatePath } from "next/cache";
 import { z } from "zod";
@@ -65,26 +64,13 @@ export async function submitFellowComplaint(
 
 export async function submitWeeklyFellowRating(
   data: z.infer<typeof WeeklyFellowRatingSchema>,
-  rating: RatingState,
 ) {
   try {
-    const { ratings } = rating;
-
     const parsedData = WeeklyFellowRatingSchema.parse(data);
-
-    console.log({ ratings })
-    console.log({ parsedData })
-    console.log({
-      finalData: {
-        ...parsedData,
-        ...ratings
-      }
-    })
 
     await db.weeklyFellowRatings.create({
       data: {
         ...parsedData,
-        ...ratings,
       },
     });
 
