5daf86573436d9272699daf26c64b0c756da2795
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2024-04-04 14:55:35 +0300

    add child routes for the school view page

---
+++ app/(platform)/hc/schools/[visibleId]/layout.tsx
@@ -0,0 +1,58 @@
+import { Separator } from "#/components/ui/separator";
+import { db } from "#/lib/db";
+import SchoolsNav from "../components/schools-nav";
+
+export default async function SchoolViewLayout({
+  children,
+  params: { visibleId },
+}: {
+  children: React.ReactNode;
+  params: { visibleId: string };
+}) {
+  const school = await db.school.findFirst({
+    where: {
+      visibleId,
+    },
+    include: {
+      _count: {
+        select: {
+          interventionSessions: true,
+          students: true,
+          interventionGroups: {
+            where: {
+              archivedAt: null,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  const avatarContent = school?.schoolName
+    .split(" ")
+    .filter((i) => i.toLowerCase() !== "school")
+    .map((i) => i[0]?.toUpperCase())
+    .join("");
+
+  return (
+    <div className="flex">
+      <div className="space-y-6 border-r-shamiri-light-grey px-6 py-8">
+        <div className="flex items-center gap-x-4">
+          <div className="flex h-16 w-16 items-center justify-center rounded-full bg-shamiri-new-light-blue p-[18px] text-xl font-semibold text-shamiri-new-blue">
+            {avatarContent}
+          </div>
+          <h2 className="text-[28px] font-semibold text-black">
+            {school?.schoolName}
+          </h2>
+        </div>
+        <Separator />
+        <div>Session pills go here</div>
+        <div>School information goes here</div>
+      </div>
+      <div>
+        <SchoolsNav visibleId={visibleId} />
+        {children}
+      </div>
+    </div>
+  );
+}
--- app/(platform)/hc/schools/[visibleId]/page.tsx
+++ app/(platform)/hc/schools/[visibleId]/page.tsx
@@ -1,51 +1,9 @@
-import { Separator } from "#/components/ui/separator";
-import { db } from "#/lib/db";
-export default async function SchoolPage({
+import { redirect } from "next/navigation";
+
+export default function SchoolsPage({
   params: { visibleId },
 }: {
   params: { visibleId: string };
 }) {
-  const school = await db.school.findFirst({
-    where: {
-      visibleId,
-    },
-    include: {
-      _count: {
-        select: {
-          interventionSessions: true,
-          students: true,
-          interventionGroups: {
-            where: {
-              archivedAt: null,
-            },
-          },
-        },
-      },
-    },
-  });
-
-  const avatarContent = school?.schoolName
-    .split(" ")
-    .filter((i) => i.toLowerCase() !== "school")
-    .map((i) => i[0]?.toUpperCase())
-    .join("");
-
-  return (
-    <div className="flex">
-      <div className="space-y-6 border-r-shamiri-light-grey px-6 py-8">
-        <div className="flex items-center gap-x-4">
-          <div className="flex h-16 w-16 items-center justify-center rounded-full bg-shamiri-new-light-blue p-[18px] text-xl font-semibold text-shamiri-new-blue">
-            {avatarContent}
-          </div>
-          <h2 className="text-[28px] font-semibold text-black">
-            {school?.schoolName}
-          </h2>
-        </div>
-        <Separator />
-        <div>Session pills go here</div>
-        <div></div>
-      </div>
-      <div>Some data comes here</div>
-    </div>
-  );
+  redirect(`/hc/schools/${visibleId}/supervisors`);
 }
