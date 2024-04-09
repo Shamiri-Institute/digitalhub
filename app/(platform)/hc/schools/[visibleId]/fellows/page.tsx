0dd6c6cb5b1017a9d28974da2eb77e0e1b43d30b
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2024-04-09 12:39:44 +0300

    initial work on column definition and db query

--- app/(platform)/hc/schools/[visibleId]/fellows/page.tsx
+++ app/(platform)/hc/schools/[visibleId]/fellows/page.tsx
@@ -1,3 +1,29 @@
-export default function FellowsPage() {
+import { db } from "#/lib/db";
+
+export default async function FellowsPage({ params: { visibleId } }: { params: { visibleId: string } }) {
+  const fellows = await db.fellow.findMany({
+    where: {
+      groups: {
+        some: {
+          school: {
+            visibleId
+          }
+        }
+
+      }
+    },
+    include: {
+      groups: {
+        where: {
+          school: {
+            visibleId
+          }
+        }
+      },
+      supervisor: true
+    }
+  })
+
+  console.log(JSON.stringify(fellows, null, 2))
   return <div>Fellows data goes here</div>;
 }
