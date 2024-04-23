47a3484f3adf0c3b1c54a04792be3d8ed96afe98
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2024-04-23 09:53:49 +0300

    add supervisors table in the schools page (#128)

--- app/(platform)/hc/schools/[visibleId]/supervisors/page.tsx
+++ app/(platform)/hc/schools/[visibleId]/supervisors/page.tsx
@@ -1,3 +1,35 @@
-export default function SupervisorsPage() {
-  return <div>Supervisors go here</div>;
+import { db } from "#/lib/db";
+import DataTable from "../../components/data-table";
+import { columns } from "./components/columns";
+
+export default async function SupervisorsPage({
+  params: { visibleId },
+}: {
+  params: { visibleId: string };
+}) {
+  const supervisors = await db.supervisor.findMany({
+    where: {
+      fellows: {
+        some: {
+          groups: {
+            some: {
+              schoolId: visibleId,
+            },
+          },
+        },
+      },
+    },
+    include: {
+      assignedSchools: true,
+      fellows: true,
+    },
+  });
+
+  return (
+    <DataTable
+      data={supervisors}
+      columns={columns}
+      emptyStateMessage="No supervisors found for this school"
+    />
+  );
 }
