e245b0eba067e92235a29ae910641d5908b448ec
Benny H. Otieno <bennyhinnotieno@gmail.com>
2024-04-23 12:17:37 +0300

    Benny/sdh 276 update clinical case session attended date and view (#125)
    * Add new component for editing clinical session date in screenings folder
    
    * Add new component for deleting clinical session attendance in screenings folder
    
    * Add new components for deleting and editing clinical session details in student-sessions.tsx
    
    * Add new functions for editing and deleting clinical session attendance
    
    * Refactor DeleteClinicalSessionDateDialog component to remove unused code
    
    * remove href
    
    * Pushed text styles to the parent div
    
    * Refactor CSS class

--- app/(platform)/screenings/[caseId]/student-sessions.tsx
+++ app/(platform)/screenings/[caseId]/student-sessions.tsx
@@ -1,4 +1,6 @@
 import { AddClinicalSessionDialog } from "#/app/(platform)/screenings/[caseId]/components/add-clinical-session";
+import { DeleteClinicalSessionDateDialog } from "#/app/(platform)/screenings/[caseId]/components/delete-session-attendance";
+import { EditClinicalSessionDateDialog } from "#/app/(platform)/screenings/[caseId]/components/edit-clinical-session-dialogue";
 import { Button } from "#/components/ui/button";
 import { Card } from "#/components/ui/card";
 import { Separator } from "#/components/ui/separator";
@@ -17,22 +19,26 @@ export function Sessions({
 }) {
   return (
     <div className="flex flex-col">
-      <Card className="pr-3.5bg-white  my-2 gap-5 p-4">
-        <div className="mb-2 flex justify-between">
-          <p className="text-base font-medium text-muted-foreground">No.</p>
-          <p className="text-base font-medium text-muted-foreground">
-            Session Attended
-          </p>
-          <p className="text-base font-medium text-muted-foreground">Date</p>
+      <Card className="my-2 w-full gap-5 bg-white p-4 pr-3.5">
+        <div className="mb-2 flex flex-1 justify-between text-base font-medium text-muted-foreground">
+          <p className="flex-1">No.</p>
+          <p className="flex-1">Created</p>
+          <p className="flex-1">Session</p>
+          <p className="flex-1">Session Date</p>
+          <p className="flex-1 text-right">Updated</p>
         </div>
         <Separator />
         <>
           {sessions.map((session, index) => (
             <SessionsCard
               key={session.id}
+              sessionId={session.id}
               id={index}
               session={session.session}
               date={session.date}
+              createdAt={session.createdAt}
+              updatedAt={session.updatedAt}
+              caseId={caseId}
             />
           ))}
         </>
@@ -55,21 +61,51 @@ export function Sessions({
 }
 
 function SessionsCard({
+  sessionId,
   id,
   session,
   date,
+  createdAt,
+  updatedAt,
+  caseId,
 }: {
+  sessionId: string;
   id: number;
   session: string;
   date: Date;
+  createdAt: Date;
+  updatedAt: Date;
+  caseId: string;
 }) {
   return (
-    <div className="mt-2 flex  items-center justify-between border-b last:border-none">
-      <p className="flex-1 text-left text-sm text-brand ">{id + 1}</p>
-      <p className="flex-1 text-center text-sm  text-brand">{session}</p>
-      <p className="flex-1 text-right text-sm  text-brand">
-        {new Date(date).toLocaleDateString()}
-      </p>
+    <div className="border-b pb-1 last:border-none">
+      <div className="mt-2 flex flex-col text-sm text-brand sm:flex-row">
+        <p className="flex-1">{id + 1}.</p>
+        <p className="flex-1">{new Date(createdAt).toLocaleDateString()}</p>
+        <p className="flex-1">{session}</p>
+        <p className="flex-1">{new Date(date).toLocaleDateString()}</p>
+        <p className="flex-1 text-left sm:text-right">
+          {new Date(updatedAt).toLocaleDateString()}
+        </p>
+      </div>
+      <div className="flex flex-1 justify-end">
+        <DeleteClinicalSessionDateDialog sessionId={sessionId} caseId={caseId}>
+          <Button variant="destructive" className="mr-3 mt-1 w-fit">
+            Delete
+          </Button>
+        </DeleteClinicalSessionDateDialog>
+
+        <EditClinicalSessionDateDialog
+          sessionId={sessionId}
+          session={session}
+          date={date}
+          caseId={caseId}
+        >
+          <Button variant="default" className="mt-1 w-fit">
+            Edit
+          </Button>
+        </EditClinicalSessionDateDialog>
+      </div>
     </div>
   );
 }
