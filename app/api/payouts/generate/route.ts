46e45adea39905ee7598e1ea7debe8c19d7895cc
Davis Wambugu <davvey27@gmail.com>
2024-05-28 15:45:17 +0300

    - Fixes ENG-310

--- app/api/payouts/generate/route.ts
+++ app/api/payouts/generate/route.ts
@@ -19,6 +19,13 @@ export const revalidate = 0;
 export const maxDuration = 300;
 
 export async function GET(request: NextRequest) {
+  const authHeader = request.headers.get("authorization");
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response("Unauthorized", {
+      status: 401,
+    });
+  }
+
   const { searchParams } = request.nextUrl;
   const params = z
     .object({
