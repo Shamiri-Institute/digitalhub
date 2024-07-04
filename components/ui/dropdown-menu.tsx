81b3520ddfba2424e32967e81afd7257f61f4107
Davis Wambugu <davvey27@gmail.com>
2024-07-04 17:15:43 +0300

    - schedule filter toggle

--- components/ui/dropdown-menu.tsx
+++ components/ui/dropdown-menu.tsx
@@ -104,14 +104,14 @@ const DropdownMenuCheckboxItem = React.forwardRef<
   <DropdownMenuPrimitive.CheckboxItem
     ref={ref}
     className={cn(
-      "relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      "relative flex cursor-pointer select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
       className,
     )}
     checked={checked}
     {...props}
   >
-    <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
-      <DropdownMenuPrimitive.ItemIndicator>
+    <span className="absolute left-2 flex h-4 w-4 items-center justify-center overflow-hidden rounded border border-shamiri-light-grey bg-white drop-shadow data-[state=checked]:border-shamiri-new-blue">
+      <DropdownMenuPrimitive.ItemIndicator className="data-[state=checked]:bg-shamiri-new-blue data-[state=checked]:text-white">
         <CheckIcon className="h-4 w-4" />
       </DropdownMenuPrimitive.ItemIndicator>
     </span>
--- lib/actions/fetch-sessions.ts
+++ lib/actions/fetch-sessions.ts
@@ -1,15 +1,18 @@
 "use server";
 
+import { Filters } from "#/app/(platform)/hc/schedule/context/filters-context";
 import { db } from "#/lib/db";
 
 export async function fetchInterventionSessions({
   hubId,
   start,
   end,
+  filters,
 }: {
   hubId: string;
   start?: Date;
   end?: Date;
+  filters?: Filters;
 }) {
   if (!hubId) {
     throw new Error("No assigned hub ID provided");
@@ -21,6 +24,13 @@ export async function fetchInterventionSessions({
       sessionDate: {
         gte: start,
         lte: end,
+      },
+      sessionType: {
+        in:
+          filters &&
+          Object.keys(filters.sessionTypes).filter((sessionType) => {
+            return filters.sessionTypes[sessionType];
+          }),
       },
     },
     include: {
