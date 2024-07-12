49b538db7c165660d1512228cca75b871b39015f
Davis Wambugu <davvey27@gmail.com>
2024-07-12 13:16:18 +0300

    - styling changes on schools page; setup school -nfo context for modals

---
+++ app/(platform)/hc/schools/components/school-table-dropdown.tsx
@@ -0,0 +1,73 @@
+import { DropoutSchool } from "#/app/(platform)/hc/components/dropout-school-form";
+import { SchoolsTableData } from "#/app/(platform)/hc/schools/components/columns";
+import { SchoolInfoContext } from "#/app/(platform)/hc/schools/context/school-info-context";
+import { Icons } from "#/components/icons";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "#/components/ui/dropdown-menu";
+import Link from "next/link";
+import { useContext } from "react";
+
+export default function SchoolTableDropdown({
+  schoolRow,
+}: {
+  schoolRow: SchoolsTableData;
+}) {
+  const context = useContext(SchoolInfoContext);
+  return (
+    <DropdownMenu
+      onOpenChange={(value) => {
+        if (value) {
+          context.setSchool(schoolRow);
+        }
+      }}
+    >
+      <DropdownMenuTrigger asChild>
+        <div className="absolute inset-0 border-l bg-white">
+          <div className="flex h-full w-full items-center justify-center">
+            <Icons.moreHorizontal className="h-5 w-5 text-shamiri-text-grey" />
+          </div>
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        align="end"
+        onCloseAutoFocus={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <DropdownMenuLabel>
+          <span className="text-xs font-medium uppercase text-shamiri-text-grey">
+            Actions
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Link href={`/hc/schools/${schoolRow.visibleId}`}>View school</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={() => {
+            context.setEditDialog(true);
+          }}
+        >
+          Edit School Information
+        </DropdownMenuItem>
+        <DropdownMenuItem>Assign point supervisor</DropdownMenuItem>
+        {!schoolRow.droppedOut || !schoolRow.droppedOutAt ? (
+          <DropdownMenuItem className="text-shamiri-red">
+            <DropoutSchool
+              schoolId={schoolRow.id}
+              schoolName={schoolRow.schoolName}
+            >
+              <div>Dropout school</div>
+            </DropoutSchool>
+          </DropdownMenuItem>
+        ) : null}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
