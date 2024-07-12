62cd00ddb6f54b7e836b9e17d3bcb8af977429b7
Davis Wambugu <davvey27@gmail.com>
2024-07-12 15:29:21 +0300

    - styling changes on charts section; add seed data for charts

--- app/(platform)/hc/schools/page.tsx
+++ app/(platform)/hc/schools/page.tsx
@@ -38,7 +38,7 @@ export default async function SchoolsPage() {
   );
 
   return (
-    <div className="container w-full space-y-5 py-10">
+    <div className="container w-full space-y-3 py-10">
       <PageHeading title="Schools" />
       <Separator />
       <div className="flex items-center justify-between">
@@ -61,26 +61,24 @@ export default async function SchoolsPage() {
         schoolAttendances={schoolAttendanceData}
       />
       <Separator />
-      <div className="pt-5">
-        <SchoolInfoProvider>
-          <SchoolsDataTable
-            data={data}
-            columns={columns}
-            emptyStateMessage="No schools found for this hub"
-            className={"data-table mt-4 bg-white"}
-            columnVisibilityState={{
-              "School ID": false,
-              "Sub - county": false,
-              "Point teacher": false,
-              "Point teacher phone no.": false,
-              "Point teacher email": false,
-              "Point supervisor phone no.": false,
-              "Point supervisor email": false,
-            }}
-          />
-          <EditSchoolDetailsForm />
-        </SchoolInfoProvider>
-      </div>
+      <SchoolInfoProvider>
+        <SchoolsDataTable
+          data={data}
+          columns={columns}
+          emptyStateMessage="No schools found for this hub"
+          className={"data-table mt-4 bg-white"}
+          columnVisibilityState={{
+            "School ID": false,
+            "Sub - county": false,
+            "Point teacher": false,
+            "Point teacher phone no.": false,
+            "Point teacher email": false,
+            "Point supervisor phone no.": false,
+            "Point supervisor email": false,
+          }}
+        />
+        <EditSchoolDetailsForm />
+      </SchoolInfoProvider>
     </div>
   );
 }
