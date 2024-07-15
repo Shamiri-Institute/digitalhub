74cc5e9d0ce07bfc2764f004b58337f3de87e0f9
Davis Wambugu <davvey27@gmail.com>
2024-07-15 21:02:11 +0300

    school breadcrumb component + context; styling changes; sessions-occurred widget; left panel component

---
+++ app/(platform)/hc/components/count-widget.tsx
@@ -0,0 +1,26 @@
+export default function CountWidget({
+  sessions = 0,
+  fellows = 0,
+  cases = 0,
+}: {
+  sessions?: number;
+  fellows?: number;
+  cases?: number;
+}) {
+  return (
+    <div className="relative flex items-center justify-between divide-x divide-[#E8E8E8] self-stretch rounded-lg border border-[#E8E8E8] bg-[#F7F7F7] font-normal">
+      <div className="flex w-28 flex-col items-center justify-center py-2">
+        <div className="text-sm text-gray-500">Sessions</div>
+        <div className="text-xl text-black">{sessions}</div>
+      </div>
+      <div className="flex w-28 flex-col items-center justify-center py-2">
+        <div className="text-sm text-gray-500">Fellows</div>
+        <div className="text-xl text-black">{fellows}</div>
+      </div>
+      <div className="flex w-28 flex-col items-center justify-center py-2">
+        <div className="text-sm text-gray-500">Cases</div>
+        <div className="text-xl text-black">{cases}</div>
+      </div>
+    </div>
+  );
+}
