548d8dd170f7b5f06cd18f13e8023ed8c7d13c31
Davis Wambugu <davvey27@gmail.com>
2024-07-15 21:02:11 +0300

    school breadcrumb component + context; styling changes; sessions-occurred widget; left panel component

--- app/(platform)/hc/schools/components/chart-area.tsx
+++ app/(platform)/hc/schools/components/chart-area.tsx
@@ -41,12 +41,6 @@ export default function ChartArea({
   schoolAttendances: SchoolAttendances[];
   sessionRatingsData: SessionRatingAverages[];
 }) {
-  console.log(
-    dropoutData,
-    schoolDataCompletenessData,
-    sessionRatingsData,
-    schoolAttendances,
-  );
   return (
     <div className="grid grid-cols-2 gap-5 py-5 md:grid-cols-4">
       <ChartCard title="Session progress">
