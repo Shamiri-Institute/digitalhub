48c080d3a0fb1ea38fdcbd0fdeac2c4b424e4cbd
Davis Wambugu <davvey27@gmail.com>
2024-07-15 21:02:11 +0300

    school breadcrumb component + context; styling changes; sessions-occurred widget; left panel component

---
+++ app/(platform)/hc/schools/components/sessions-occurred-widget.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { Icons } from "#/components/icons";
+import { SESSION_TYPES } from "#/lib/app-constants/constants";
+import { cn } from "#/lib/utils";
+import { Prisma } from "@prisma/client";
+
+export default function SessionsOccurredWidget({
+  sessions,
+}: {
+  sessions: Prisma.InterventionSessionGetPayload<{}>[];
+}) {
+  return (
+    <div className="flex justify-center gap-2">
+      {SESSION_TYPES.map((sessionType) => {
+        const occurredStatus = sessions.find((session) => {
+          return session.sessionType === sessionType.name;
+        })?.occurred;
+        const cancelledStatus =
+          sessions.find((session) => {
+            return session.sessionType === sessionType.name;
+          })?.status === "Cancelled";
+        return (
+          <div
+            key={sessionType.name}
+            className={cn(
+              "select-none rounded-[0.25rem] border px-1.5 py-0.5",
+              {
+                "border-green-border": occurredStatus,
+                "border-blue-border": !occurredStatus,
+                "border-red-border": cancelledStatus,
+              },
+              {
+                "bg-green-bg": occurredStatus,
+                "bg-blue-bg": !occurredStatus,
+                "bg-red-bg": cancelledStatus,
+              },
+            )}
+          >
+            <div
+              className={cn("flex gap-1 text-[0.825rem] font-semibold", {
+                "text-green-base": occurredStatus,
+                "text-blue-base": !occurredStatus,
+                "text-red-base": cancelledStatus,
+              })}
+            >
+              <div className="flex items-center gap-1">
+                {occurredStatus && !cancelledStatus && (
+                  <Icons.checkCircle
+                    className="h-3.5 w-3.5"
+                    strokeWidth={2.5}
+                  />
+                )}
+                {!occurredStatus && !cancelledStatus && (
+                  <Icons.helpCircle className="h-3.5 w-3.5" strokeWidth={2.5} />
+                )}
+                {cancelledStatus && (
+                  <Icons.crossCircleFilled
+                    className="h-3.5 w-3.5"
+                    strokeWidth={2.5}
+                  />
+                )}
+              </div>
+              {sessionType.description}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
