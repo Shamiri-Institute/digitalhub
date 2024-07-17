6c5c1d628bc8e07034ab45997c9ef73fc34ce83f
Davis Wambugu <davvey27@gmail.com>
2024-07-17 15:18:29 +0300

    code format; update fetch for school supervisor data

--- app/(platform)/hc/schools/layout.tsx
+++ app/(platform)/hc/schools/layout.tsx
@@ -1,6 +1,7 @@
 import { fetchSchoolData } from "#/app/(platform)/hc/schools/actions";
 import SchoolsDataProvider from "#/app/(platform)/hc/schools/components/schools-data-provider";
 import { currentHubCoordinator } from "#/app/auth";
+import { InvalidPersonnelRole } from "#/components/common/invalid-personnel-role";
 import React from "react";
 
 export default async function SchoolsLayout({
@@ -8,9 +9,17 @@ export default async function SchoolsLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const hubCoordinator = await currentHubCoordinator();
-  // TODO: convert this to Promise.all for concurrent fetch
-  const data = await fetchSchoolData(hubCoordinator?.assignedHubId as string);
+  const coordinator = await currentHubCoordinator();
+
+  if (!coordinator) {
+    return <InvalidPersonnelRole role="hub-coordinator" />;
+  }
+
+  if (!coordinator.assignedHubId) {
+    return <div>Hub coordinator has no assigned hub</div>;
+  }
+
+  const data = await fetchSchoolData(coordinator?.assignedHubId as string);
   return (
     <div className="w-full self-stretch">
       <SchoolsDataProvider schools={data}>{children}</SchoolsDataProvider>
