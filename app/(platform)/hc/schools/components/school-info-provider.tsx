b3ad9e1cec1fed95b45cac99ea1da83d683a965f
Davis Wambugu <davvey27@gmail.com>
2024-07-24 15:40:40 +0300

    add school context to school page

--- app/(platform)/hc/schools/components/school-info-provider.tsx
+++ app/(platform)/hc/schools/components/school-info-provider.tsx
@@ -5,18 +5,22 @@ import { useState } from "react";
 
 export default function SchoolInfoProvider({
   children,
+  school,
 }: {
   children: React.ReactNode;
+  school?: SchoolsTableData;
 }) {
   const [editDialog, setEditDialog] = useState(false);
   const [pointSupervisorDialog, setPointSupervisorDialog] = useState(false);
   const [schoolDropOutDialog, setSchoolDropOutDialog] = useState(false);
   const [undoDropOutDialog, setUndoDropOutDialog] = useState(false);
-  const [school, setSchool] = useState<SchoolsTableData | null>(null);
+  const [_school, setSchool] = useState<SchoolsTableData | null>(
+    school ?? null,
+  );
   return (
     <SchoolInfoContext.Provider
       value={{
-        school,
+        school: _school,
         setSchool,
         editDialog,
         setEditDialog,
