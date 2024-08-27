e5af56d1bc87e4dd5cac78b70ec9a273848f15cf
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2024-08-27 16:20:59 +0300

    add skeleton of other pages

---
+++ app/(platform)/sc/schedule/page.tsx
@@ -0,0 +1,3 @@
+export default function SupervisorSchedulePage() {
+  return <div>Schedule Page coming soon!</div>
+}
---
+++ app/(platform)/sc/schools/page.tsx
@@ -0,0 +1,3 @@
+export default function SupervisorSchoolsPage() {
+  return <div>Schedule Page coming soon!</div>
+}
