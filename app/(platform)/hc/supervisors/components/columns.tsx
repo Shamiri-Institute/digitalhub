afd16f1e81c0e5bc90da979da2fd795aceb13820
wambugudavis <davvey27@gmail.com>
2024-08-28 15:26:01 +0300

    ENG-362: hc should be able to assign supervisors to fellows

--- app/(platform)/hc/supervisors/components/columns.tsx
+++ app/(platform)/hc/supervisors/components/columns.tsx
@@ -1,7 +1,7 @@
 "use client";
 
+import DataTableRatingStars from "#/app/(platform)/hc/components/datatable-rating-stars";
 import { AllSupervisorsDataTableMenu } from "#/app/(platform)/hc/supervisors/components/main-supervisors-datatable";
-import { Icons } from "#/components/icons";
 import { Badge } from "#/components/ui/badge";
 import { Checkbox } from "#/components/ui/checkbox";
 import {
@@ -111,6 +111,7 @@ export const columns: ColumnDef<SupervisorsData>[] = [
         <Badge variant="shamiri-green">Active</Badge>
       ),
   },
+  // TODO: Refactor query to calculate average server side
   {
     header: "Average Rating",
     cell: ({ row }) => {
@@ -156,42 +157,7 @@ export const columns: ColumnDef<SupervisorsData>[] = [
             );
           })
           .reduce((a, b) => a + b, 0) / evaluations.length;
-      const remainder = rating - Math.floor(rating);
-      const rounded = Number(rating).toFixed(1);
-      return (
-        <div className="flex items-center gap-2">
-          <div className="relative flex items-center gap-1">
-            {Array.from(Array(5).keys()).map((index) => {
-              return (
-                <div key={index.toString()} className="relative h-5 w-5 shrink">
-                  <Icons.starRating className="h-full w-full text-shamiri-light-grey" />
-                </div>
-              );
-            })}
-            {!isNaN(rating) && (
-              <div className="absolute inset-0 flex items-center gap-1 text-shamiri-light-orange">
-                {Array.from(Array(Math.floor(rating)).keys()).map((index) => {
-                  return <Icons.starRating key={index} className="h-5 w-5" />;
-                })}
-                {remainder > 0 ? (
-                  <div className="relative h-5 w-5 shrink">
-                    <Icons.starRating className="h-full w-full text-transparent" />
-                    <div
-                      className="absolute inset-y-0 left-0 overflow-hidden"
-                      style={{ width: remainder * 100 + "%" }}
-                    >
-                      <Icons.starRating className="h-5 w-5" />
-                    </div>
-                  </div>
-                ) : null}
-              </div>
-            )}
-          </div>
-          <div className="text-shamiri-text-grey">
-            {isNaN(rating) ? "0.0" : rounded}
-          </div>
-        </div>
-      );
+      return <DataTableRatingStars rating={rating} />;
     },
     id: "Average Rating",
   },
