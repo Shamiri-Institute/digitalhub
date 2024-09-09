f980a55dcf3b77881bda6678e731e3ae512d91cc
hinn254 <bennyhinnotieno@gmail.com>
2024-09-09 08:35:31 +0300

    feat: add InfoCard and StudentSearchCommand components

---
+++ app/(platform)/hc/students/components/charts/constants.ts
@@ -0,0 +1,25 @@
+export const possibleSessions = [
+  "Pre",
+  "S1",
+  "S2",
+  "S3",
+  "S4",
+  "F1",
+  "F2",
+  "F3",
+  "F4",
+  "F5",
+  "F6",
+  "F7",
+  "F8",
+];
+
+export const possibleInterventionSessions = ["s0", "s1", "s2", "s3", "s4"];
+
+export const clinicaldataValue = [
+  { name: "Active", value: 0 },
+  { name: "FollowUp", value: 0 },
+  { name: "Terminated", value: 0 },
+];
+
+export const clinicalCasesColors = ["#7EA16B", "#FABC2A", "#B0D5EA"];
