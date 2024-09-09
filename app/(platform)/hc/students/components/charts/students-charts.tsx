41dd2ee61d40a43fba48bd99e01f2c45064a7d81
hinn254 <bennyhinnotieno@gmail.com>
2024-09-09 16:05:28 +0300

    feat: add StudentsCharts component, students filter and charts

---
+++ app/(platform)/hc/students/components/charts/students-charts.tsx
@@ -0,0 +1,168 @@
+"use client";
+
+import { possibleInterventionSessions } from "#/app/(platform)/hc/students/components/charts/constants";
+import ChartCard from "#/components/ui/chart-card";
+import { Prisma } from "@prisma/client";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Cell,
+  Label,
+  Legend,
+  LineChart,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+export default function HubStudentsDetailsCharts({
+  studentsAttendanceGroupedBySession,
+  studentsDropOutReasonsGroupedByReason,
+}: {
+  studentsAttendanceGroupedBySession: (Prisma.PickEnumerable<
+    Prisma.InterventionSessionGroupByOutputType,
+    "sessionType"[]
+  > & {
+    _count: {
+      sessionType: number;
+    };
+  })[];
+  studentsDropOutReasonsGroupedByReason: (Prisma.PickEnumerable<
+    Prisma.StudentGroupByOutputType,
+    "dropOutReason"[]
+  > & {
+    _count: {
+      dropOutReason: number;
+    };
+  })[];
+}) {
+  const filteredFormatedSessions = possibleInterventionSessions.map(
+    (session) => {
+      const found = studentsAttendanceGroupedBySession.find(
+        (item) => item.sessionType === session,
+      );
+      return {
+        sessionType: session,
+        count: found ? found._count.sessionType : 0,
+      };
+    },
+  );
+
+  const filteredFormatedDropOutReasons =
+    studentsDropOutReasonsGroupedByReason.map((reason) => ({
+      name: reason?.dropOutReason ?? "",
+      value: reason._count.dropOutReason,
+    }));
+
+  const formatedStudentsDropOutReasons = filteredFormatedDropOutReasons.reduce(
+    (acc, val) => {
+      const existing = acc.find(
+        (item) =>
+          item.name.trim().toLowerCase() === val.name.trim().toLowerCase(),
+      );
+      if (existing) {
+        existing.value += val.value;
+      } else {
+        acc.push({ ...val });
+      }
+      return acc;
+    },
+    [] as { name: string; value: number }[],
+  );
+
+  return (
+    <div className="grid grid-cols-2 gap-5 py-5 md:grid-cols-4">
+      <ChartCard title="Attendance" showCardFooter={false}>
+        {filteredFormatedSessions?.length ? (
+          <ResponsiveContainer width="100%" height="100%">
+            <BarChart width={307} height={307} data={filteredFormatedSessions}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="sessionType" />
+              <YAxis dataKey="count" />
+              <Tooltip />
+              <Bar dataKey="count" stackId="a" fill="#0085FF" />
+              <Bar dataKey="sessionType" stackId="a" fill="#CCE7FF" />
+            </BarChart>
+          </ResponsiveContainer>
+        ) : null}
+      </ChartCard>
+      <ChartCard title="Drop-out reasons" showCardFooter={false}>
+        {formatedStudentsDropOutReasons.length ? (
+          <ResponsiveContainer width="100%" height="100%">
+            <PieChart width={307} height={307}>
+              <Pie
+                data={formatedStudentsDropOutReasons}
+                dataKey="value"
+                nameKey="name"
+                startAngle={90}
+                endAngle={450}
+                outerRadius={100}
+                innerRadius={70}
+                fill="#8884d8"
+              >
+                <Label
+                  position="center"
+                  className="text-2xl font-semibold leading-8 text-shamiri-black"
+                >
+                  {formatedStudentsDropOutReasons.reduce(
+                    (acc, val) => acc + val.value,
+                    0,
+                  )}
+                </Label>
+                {formatedStudentsDropOutReasons.map((reason) => (
+                  <Cell key={reason?.name ?? ""} fill="#8884d8" />
+                ))}
+              </Pie>
+              <Tooltip />
+            </PieChart>
+          </ResponsiveContainer>
+        ) : null}
+      </ChartCard>
+      <ChartCard title="Student information completion" showCardFooter={false}>
+        {[].length ? (
+          <ResponsiveContainer width="100%" height="100%">
+            <PieChart width={250} height={250}>
+              <Pie
+                data={[]}
+                dataKey="value"
+                nameKey="name"
+                startAngle={90}
+                endAngle={450}
+                outerRadius={100}
+                innerRadius={70}
+              >
+                <Label
+                  position="center"
+                  className="text text-2xl font-semibold leading-8"
+                  fill="#fffff"
+                >
+                  {0}
+                </Label>
+                {[].map(({ name }) => (
+                  <Cell key={name} />
+                ))}
+              </Pie>
+            </PieChart>
+          </ResponsiveContainer>
+        ) : null}
+      </ChartCard>
+      <ChartCard title="Student group ratings" showCardFooter={false}>
+        {[]?.length ? (
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart width={307} height={307} data={[]}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+            </LineChart>
+          </ResponsiveContainer>
+        ) : null}
+      </ChartCard>
+    </div>
+  );
+}
