4500c8e97cbb7494fc0a4306939f4959a11ea2eb
wambugudavis <davvey27@gmail.com>
2024-09-10 12:27:01 +0300

    feat: hc should be able to view the main fellows' data-table

---
+++ app/(platform)/hc/fellows/components/main-fellows-datatable.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import DataTable from "#/app/(platform)/hc/components/data-table";
+import {
+  columns,
+  MainFellowTableData,
+} from "#/app/(platform)/hc/fellows/components/columns";
+import { BatchUploadDownloadFellow } from "#/app/(platform)/hc/schools/[visibleId]/fellows/components/upload-csv";
+import { Prisma } from "@prisma/client";
+import { useEffect, useState } from "react";
+
+export default function MainFellowsDatatable({
+  fellows,
+  supervisors,
+}: {
+  fellows: MainFellowTableData[];
+  supervisors: Prisma.SupervisorGetPayload<{}>[];
+}) {
+  const [fellow, setFellow] = useState<MainFellowTableData | null>(null);
+
+  const renderTableActions = () => {
+    return (
+      <div className="flex items-center gap-3">
+        <BatchUploadDownloadFellow />
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    console.log(fellow);
+  }, [fellow]);
+
+  return (
+    <div>
+      <DataTable
+        columns={columns(supervisors, setFellow)}
+        data={fellows}
+        className={"data-table data-table-action mt-4 bg-white"}
+        emptyStateMessage="No fellows associated with this hub"
+        renderTableActions={renderTableActions()}
+      />
+    </div>
+  );
+}
