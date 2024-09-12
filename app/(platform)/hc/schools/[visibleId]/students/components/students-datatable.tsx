6d8c269764b3229efd04705159ded4462e13aa88
wambugudavis <davvey27@gmail.com>
2024-09-12 11:53:21 +0300

    refactor: update students datatable

---
+++ app/(platform)/hc/schools/[visibleId]/students/components/students-datatable.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import DataTable from "#/app/(platform)/hc/components/data-table";
+import {
+  columns,
+  SchoolStudentTableData,
+} from "#/app/(platform)/hc/schools/[visibleId]/students/components/columns";
+import { BatchUploadDownloadStudents } from "#/app/(platform)/hc/schools/[visibleId]/students/components/upload-csv";
+import { Prisma } from "@prisma/client";
+
+export default function StudentsDatatable({
+  data,
+  hubCoordinator,
+  visibleId,
+}: {
+  data: SchoolStudentTableData[];
+  hubCoordinator: Prisma.HubCoordinatorGetPayload<{
+    include: {
+      assignedHub: true;
+    };
+  }> | null;
+  visibleId: string;
+}) {
+  const renderTableActions = () => {
+    return (
+      hubCoordinator?.assignedHubId &&
+      hubCoordinator.implementerId &&
+      hubCoordinator.assignedHub?.projectId && (
+        <BatchUploadDownloadStudents
+          hubId={hubCoordinator?.assignedHubId}
+          implementerId={hubCoordinator?.implementerId}
+          projectId={hubCoordinator?.assignedHub?.projectId}
+          schoolVisibleId={visibleId}
+        />
+      )
+    );
+  };
+  return (
+    <div>
+      <DataTable
+        data={data}
+        columns={columns}
+        emptyStateMessage="No students found"
+        className="data-table data-table-action mt-4"
+        renderTableActions={renderTableActions()}
+        columnVisibilityState={{
+          Gender: false,
+          "Contact no.": false,
+          "Admission number": false,
+          Stream: false,
+          "Class/Form": false,
+          "Date added": false,
+        }}
+      />
+    </div>
+  );
+}
