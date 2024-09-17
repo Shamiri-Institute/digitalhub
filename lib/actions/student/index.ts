5d91b82b6c65e8dda52fdc258ae1d8c7a0edcf57
Davis Wambugu <davvey27@gmail.com>
2024-09-17 10:31:10 +0300

    feat: hc should be able to edit a students information (#193)
    * feat: edit student information
    
    * fix: type error
    
    * refactor: use 'yearOfBirth' value for students' 'age' columns
    
    * style: format students' contact number column
    
    * fix: set default value for contact number
    
    * docs: add todo
    
    * fix: remove unused variables + remove console logs + fix error message

---
+++ lib/actions/student/index.ts
@@ -0,0 +1,63 @@
+"use server";
+
+import { StudentDetailsSchema } from "#/app/(platform)/hc/schemas";
+import { currentHubCoordinator, currentSupervisor } from "#/app/auth";
+import { db } from "#/lib/db";
+import { z } from "zod";
+
+async function checkAuth() {
+  const hubCoordinator = await currentHubCoordinator();
+  const supervisor = await currentSupervisor();
+
+  if (!hubCoordinator && !supervisor) {
+    throw new Error("The session has not been authenticated");
+  }
+
+  return { hubCoordinator, supervisor };
+}
+
+export async function submitStudentDetails(
+  data: z.infer<typeof StudentDetailsSchema>,
+) {
+  try {
+    await checkAuth();
+
+    const {
+      id,
+      studentName,
+      form,
+      stream,
+      gender,
+      yearOfBirth,
+      admissionNumber,
+      phoneNumber,
+    } = StudentDetailsSchema.parse(data);
+
+    await db.student.update({
+      where: {
+        id,
+      },
+      data: {
+        studentName,
+        gender,
+        yearOfBirth,
+        admissionNumber,
+        form,
+        stream,
+        phoneNumber,
+      },
+    });
+    return {
+      success: true,
+      message: `Successfully updated details for ${studentName}`,
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      success: false,
+      message:
+        (err as Error)?.message ??
+        "Sorry, could not update student information.",
+    };
+  }
+}
