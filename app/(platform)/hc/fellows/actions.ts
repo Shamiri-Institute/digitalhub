80cc6c063943e53e9be56e49185cbaf7de982400
Benny H. Otieno <bennyhinnotieno@gmail.com>
2024-09-20 16:44:39 +0300

    feat: add fellow tab graphs on  hc (#202)
    * feat: add charts and loading indicator to FellowPage hc
    
    * feat: add functions to fetch fellow dropout reasons, fellow data completeness data, and fellow session rating averages
    
    * feat: Add charts to display fellow attendance, dropout reasons, data completeness, and session ratings

--- app/(platform)/hc/fellows/actions.ts
+++ app/(platform)/hc/fellows/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { currentHubCoordinator } from "#/app/auth";
+import { db } from "#/lib/db";
 
 async function checkAuth() {
   const hubCoordinator = await currentHubCoordinator();
@@ -8,4 +9,100 @@ async function checkAuth() {
   if (!hubCoordinator) {
     throw new Error("The session has not been authenticated");
   }
+}
+
+export type FellowDropoutReasonsGraphData = {
+  name: string;
+  value: number;
+};
+
+export async function fetchFellowDropoutReasons(hudId: string) {
+  const dropoutData = await db.$queryRaw<FellowDropoutReasonsGraphData[]>`
+    SELECT
+      COUNT(*) AS value,
+      drop_out_reason AS name
+    FROM fellows
+    WHERE
+      drop_out_reason IS NOT NULL
+      AND hub_id = ${hudId}
+    GROUP BY
+      drop_out_reason
+  `;
+
+  dropoutData.forEach((data) => {
+    data.value = Number(data.value);
+  });
+
+  return dropoutData;
+}
+
+export async function fetchFellowDataCompletenessData(hubId: string) {
+  const [fellowData] = await db.$queryRaw<{ percentage: number }[]>`
+    SELECT
+      AVG((
+        (CASE WHEN mpesa_name IS NOT NULL THEN 1 ELSE 0 END)
+        + (CASE WHEN mpesa_number IS NOT NULL THEN 1 ELSE 0 END)
+        + (CASE WHEN cell_number IS NOT NULL THEN 1 ELSE 0 END)
+        + (CASE WHEN hub_id IS NOT NULL THEN 1 ELSE 0 END)
+        + (CASE WHEN gender IS NOT NULL THEN 1 ELSE 0 END)
+        + (CASE WHEN id_number IS NOT NULL THEN 1 ELSE 0 END)
+      ) / 6.0 * 100) AS percentage
+    FROM fellows
+    WHERE hub_id = ${hubId}
+  `;
+
+  if (!fellowData) {
+    return [];
+  }
+
+  const percentage = +Number(fellowData.percentage).toFixed(2);
+
+  return [
+    { name: "actual", value: percentage },
+    { name: "difference", value: 100 - percentage },
+  ];
+}
+
+export type FellowSessionRatingAverages = {
+  session_date: string;
+  behaviour_rating: number;
+  program_delivery_rating: number;
+  dressing_and_grooming_rating: number;
+  punctuality_rating: number;
+};
+
+export async function fetchFellowSessionRatingAverages(hubId: string) {
+  const ratingAverages = await db.$queryRaw<FellowSessionRatingAverages[]>`
+    SELECT
+      CONCAT(TRIM(TO_CHAR(wfr.week, 'Month')), ' Week ', EXTRACT(WEEK FROM wfr.week)) AS session_date,
+      AVG(wfr.behaviour_rating) AS behaviour_rating,
+      AVG(wfr.program_delivery_rating) AS program_delivery_rating,
+      AVG(wfr.dressing_and_grooming_rating) AS dressing_and_grooming_rating,
+      AVG(wfr.punctuality_rating) AS punctuality_rating
+    FROM weekly_fellow_ratings wfr
+    INNER JOIN supervisors AS sup ON wfr.supervisor_id = sup.id
+    WHERE
+      sup.hub_id = ${hubId}
+    GROUP BY
+    wfr.week
+    ORDER BY
+    wfr.week
+  `;
+
+  if (!ratingAverages.length) {
+    return [];
+  }
+
+  ratingAverages.forEach((item) => {
+    item.behaviour_rating = Math.round(Number(item.behaviour_rating));
+    item.program_delivery_rating = Math.round(
+      Number(item.program_delivery_rating),
+    );
+    item.dressing_and_grooming_rating = Math.round(
+      Number(item.dressing_and_grooming_rating),
+    );
+    item.punctuality_rating = Math.round(Number(item.punctuality_rating));
+  });
+
+  return ratingAverages;
 }
