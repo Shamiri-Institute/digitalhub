3d3920b11b480e337e456e93105222e76de7e84b
hinn254 <bennyhinnotieno@gmail.com>
2024-09-24 11:13:56 +0300

    feat: add school_documents and fellow_documents tables

---
+++ prisma/migrations/20240924081256_school_fellow_documents_tables/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "school_documents" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ NOT NULL,
+    "file_name" VARCHAR(255) NOT NULL,
+    "type" VARCHAR(255) NOT NULL,
+    "school_id" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "school_documents_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "fellow_documents" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ NOT NULL,
+    "file_name" VARCHAR(255) NOT NULL,
+    "type" VARCHAR(255) NOT NULL,
+    "fellow_id" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "fellow_documents_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "school_documents" ADD CONSTRAINT "school_documents_school_id_fkey" FOREIGN KEY ("school_id") REFERENCES "schools"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "fellow_documents" ADD CONSTRAINT "fellow_documents_fellow_id_fkey" FOREIGN KEY ("fellow_id") REFERENCES "fellows"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
