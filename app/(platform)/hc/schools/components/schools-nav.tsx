8a7065af612b0246e50f48dc96651c3b983f1ce2
hinn254 <bennyhinnotieno@gmail.com>
2024-10-03 01:27:40 +0300

    feat:  datatable components

--- app/(platform)/hc/schools/components/schools-nav.tsx
+++ app/(platform)/hc/schools/components/schools-nav.tsx
@@ -16,6 +16,7 @@ export default function SchoolsNav({ visibleId }: { visibleId: string }) {
     { name: "Supervisors", href: `/hc/schools/${visibleId}/supervisors` },
     { name: "Fellows", href: `/hc/schools/${visibleId}/fellows` },
     { name: "Students", href: `/hc/schools/${visibleId}/students` },
+    { name: "Files", href: `/hc/schools/${visibleId}/files` },
   ];
   const initialTab = options.find((tab: TabType) => tab.href === pathname);
   const [activeTab, setActiveTab] = useState<TabType>(
