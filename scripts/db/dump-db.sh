cab70b944a3028ff79abffa9f11c17700890398b
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2024-10-02 13:25:35 +0300

    add timestamp and add flags for fast exits

--- scripts/db/dump-db.sh
+++ scripts/db/dump-db.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# REPLACE hostname, username, database_name  with the relevant params
+set -euxo pipefail
+
+TIMESTAMP=$(date +"%Y-%m-%d_%H%M%S")
 
 # command for creating the dump
-pg_dump -h "${PROD_DBHOST}" -U "${PROD_DBUSER}" -d "${PROD_DBNAME}" -f db-dump.sql
+pg_dump -h "${PROD_DBHOST}" -U "${PROD_DBUSER}" -d "${PROD_DBNAME}" -f db-dump-"$TIMESTAMP".sql
 
 # command for loading your local db/docker db with contents from the dump
 # you can also add flags for your user/port/host if required. Please refer to the psql command documentation
-psql -d shamiri_db -f db-dump.sql
+psql -d shamiri_db -f db-dump-"$TIMESTAMP".sql
