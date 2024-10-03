c4da090972364bc30c25edc75f501d92e1b1fa26
Benny H. Otieno <bennyhinnotieno@gmail.com>
2024-10-03 15:41:26 +0300

    Eng 397 schools files (#217)
    * feat: add files datatable and menu components for school documents
    
    * feat: add actions for removing and updating school documents
    
    * feat:  datatable components
    
    * feat: add link column to school_documents and fellow_documents tables
    
    * feat: add file preview and download functionality to files datatable menu
    
    * refactor: remove state and useEffect
    
    * Eng 399 hc fellows file upload (#218)
    
    * feat: add file upload for the 3 fellow docs
    
    * wip: add file uploader component for each fellow documents
    
    * Refactor file upload components for fellow documents
    
    * feat: file upload action
    
    * Refactor file upload logic and remove unnecessary console logs
    
    ---------
    
    Co-authored-by: wambugudavis <davvey27@gmail.com>

--- app/(platform)/hc/schools/components/schools-nav.tsx
+++ app/(platform)/hc/schools/components/schools-nav.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { ToggleGroup, ToggleGroupItem } from "#/components/ui/toggle-group";
 import { usePathname, useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
 
 type TabType = {
   name: string;
@@ -16,25 +15,20 @@ export default function SchoolsNav({ visibleId }: { visibleId: string }) {
     { name: "Supervisors", href: `/hc/schools/${visibleId}/supervisors` },
     { name: "Fellows", href: `/hc/schools/${visibleId}/fellows` },
     { name: "Students", href: `/hc/schools/${visibleId}/students` },
+    { name: "Files", href: `/hc/schools/${visibleId}/files` },
   ];
-  const initialTab = options.find((tab: TabType) => tab.href === pathname);
-  const [activeTab, setActiveTab] = useState<TabType>(
-    initialTab ?? options[1]!,
-  );
-
-  useEffect(() => {
-    router.push(activeTab.href);
-  }, [activeTab, router]);
 
   return (
     <div className="flex">
       <ToggleGroup
         type="single"
         className="form-toggle"
-        defaultValue={activeTab.name}
+        defaultValue={options.find((tab) => tab.href === pathname)?.name}
         onValueChange={(value) => {
-          const option = options.find((x) => x.name === value);
-          if (value && option) setActiveTab(option);
+          if (value) {
+            const tab = options.find((tab) => tab.name === value);
+            router.push(tab!.href);
+          }
         }}
       >
         {options.map((option) => {
