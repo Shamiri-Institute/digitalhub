c0ef2e85bf5fd31e0c08f04f355adfea35626b82
Benny H. Otieno <bennyhinnotieno@gmail.com>
2024-10-03 15:41:26 +0300

    Eng 397 schools files (#217)
    * feat: add files datatable and menu components for school documents
    
    * feat: add actions for removing and updating school documents
    
    * feat:  datatable components
    
    * feat: add link column to school_documents and fellow_documents tables
    
    * feat: add file preview and download functionality to files datatable menu
    
    * refactor: remove state and useEffect
    
    * Eng 399 hc fellows file upload (#218)
    
    * feat: add file upload for the 3 fellow docs
    
    * wip: add file uploader component for each fellow documents
    
    * Refactor file upload components for fellow documents
    
    * feat: file upload action
    
    * Refactor file upload logic and remove unnecessary console logs
    
    ---------
    
    Co-authored-by: wambugudavis <davvey27@gmail.com>

---
+++ app/(platform)/hc/schools/[visibleId]/files/components/delete-file-modal.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { removeUploadedSchoolFile } from "#/app/(platform)/hc/schools/[visibleId]/files/actions";
+import { SchoolFilesTableData } from "#/app/(platform)/hc/schools/[visibleId]/files/components/columns";
+import { revalidatePageAction } from "#/app/(platform)/hc/schools/actions";
+import DialogAlertWidget from "#/app/(platform)/hc/schools/components/dialog-alert-widget";
+import { Icons } from "#/components/icons";
+import { Button } from "#/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+} from "#/components/ui/dialog";
+import { Separator } from "#/components/ui/separator";
+import { toast } from "#/components/ui/use-toast";
+import { usePathname } from "next/navigation";
+import { Dispatch, SetStateAction } from "react";
+
+export function RemoveUploadedFile({
+  isOpen,
+  setIsOpen,
+  document,
+}: {
+  document: SchoolFilesTableData;
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+}) {
+  const pathname = usePathname();
+
+  const handleRemoveFile = async () => {
+    try {
+      await removeUploadedSchoolFile(document.id);
+      await revalidatePageAction(pathname);
+      toast({
+        variant: "default",
+        title: "Success",
+        description: `Successfully removed ${document.fileName}`,
+      });
+      setIsOpen(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogContent className="w-2/5 max-w-none">
+        <DialogHeader>
+          <h2 className="text-lg font-bold">Confirm file removal</h2>
+        </DialogHeader>
+        <DialogAlertWidget>
+          <div className="flex items-center gap-2">
+            <span>{document.fileName}</span>
+            <span className="h-1 w-1 rounded-full bg-shamiri-new-blue">
+              {""}
+            </span>
+            <span>{document.type}</span>
+          </div>
+        </DialogAlertWidget>
+
+        <p className="text-shamiri-black">Are you sure?</p>
+
+        <div className="flex items-center space-x-2 rounded-xl border-2 border-shamiri-light-red-background-base/50 bg-shamiri-light-red-background-base/15 p-2 px-4">
+          <Icons.info className="h-4 w-4 shrink-0 text-shamiri-light-red" />
+          <p className="text-shamiri-light-red">
+            Once this change has been made it is irreversible and will need you
+            to contact support in order to modify. Please be sure of your action
+            before you confirm.
+          </p>
+        </div>
+        <Separator />
+        <DialogFooter className="flex justify-end">
+          <Button
+            className="text-shamiri-light-red hover:bg-red-bg"
+            variant="ghost"
+            onClick={() => {
+              setIsOpen(false);
+            }}
+            type="button"
+          >
+            Cancel
+          </Button>
+          <Button onClick={handleRemoveFile} variant="destructive">
+            Confirm
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
