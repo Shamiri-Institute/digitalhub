e0ccea96213045503e2b700abfcfccc4d8e9fc1b
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2024-10-04 16:03:13 +0300

    complete implementation of the loading screen

--- components/charts/chart-skeleton.tsx
+++ components/charts/chart-skeleton.tsx
@@ -11,13 +11,13 @@ export default function ChartSkeleton() {
       <CardContent className="h-[275px] pt-6">
         <div className="max-w-sm animate-pulse rounded border border-gray-200 p-4 shadow dark:border-gray-700 md:p-6">
           <div className="mt-4 flex items-baseline">
-            <div className="h-72 w-full rounded-t-lg bg-gray-200 dark:bg-gray-700" />
-            <div className="ms-6 h-56 w-full rounded-t-lg bg-gray-200 dark:bg-gray-700" />
-            <div className="ms-6 h-72 w-full rounded-t-lg bg-gray-200 dark:bg-gray-700" />
-            <div className="ms-6 h-64 w-full rounded-t-lg bg-gray-200 dark:bg-gray-700" />
-            <div className="ms-6 h-80 w-full rounded-t-lg bg-gray-200 dark:bg-gray-700" />
-            <div className="ms-6 h-72 w-full rounded-t-lg bg-gray-200 dark:bg-gray-700" />
-            <div className="ms-6 h-80 w-full rounded-t-lg bg-gray-200 dark:bg-gray-700" />
+            <div className="h-[80%] w-full rounded-t-lg bg-gray-200 dark:bg-gray-700" />
+            <div className="ms-6 h-20 w-full rounded-t-lg bg-gray-200 dark:bg-gray-700" />
+            <div className="ms-6 h-12 w-full rounded-t-lg bg-gray-200 dark:bg-gray-700" />
+            <div className="h-30 ms-6 w-full rounded-t-lg bg-gray-200 dark:bg-gray-700" />
+            <div className="ms-6 h-40 w-full rounded-t-lg bg-gray-200 dark:bg-gray-700" />
+            <div className="ms-6 h-32 w-full rounded-t-lg bg-gray-200 dark:bg-gray-700" />
+            <div className="ms-6 h-20 w-full rounded-t-lg bg-gray-200 dark:bg-gray-700" />
           </div>
           <span className="sr-only">Loading...</span>
         </div>
