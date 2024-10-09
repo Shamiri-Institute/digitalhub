14d4b326a0f63aabad5d1081b12a25485a4a9992
hinn254 <bennyhinnotieno@gmail.com>
2024-10-09 16:42:56 +0300

    add basic pages for the reporting tab

---
+++ app/(platform)/hc/reporting/complaints/page.tsx
@@ -0,0 +1,7 @@
+export default async function ComplaintsPage() {
+  return (
+    <div>
+      <p>This is the complaints section.</p>
+    </div>
+  );
+}
---
+++ app/(platform)/hc/reporting/payout-history/page.tsx
@@ -0,0 +1,7 @@
+export default async function PayoutHistoryPage() {
+  return (
+    <div>
+      <p>This is the PayoutHistory page.</p>
+    </div>
+  );
+}
