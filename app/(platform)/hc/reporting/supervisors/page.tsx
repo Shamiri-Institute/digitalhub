3304c990e22bbf236958922edc55820769c38c02
hinn254 <bennyhinnotieno@gmail.com>
2024-10-15 15:27:34 +0300

    Refactor supervisor page to load and display supervisor expenses

--- app/(platform)/hc/reporting/supervisors/page.tsx
+++ app/(platform)/hc/reporting/supervisors/page.tsx
@@ -1,7 +1,8 @@
+import { loadHubSupervisorExpenses } from "#/app/(platform)/hc/reporting/supervisors/actions";
+import HCSupervisorsDataTable from "#/app/(platform)/hc/reporting/supervisors/components/supervisors-table";
+
 export default async function SupervisorsPage() {
-  return (
-    <div>
-      <p>This is the SupervisorsPage page.</p>
-    </div>
-  );
+  const expensesData = await loadHubSupervisorExpenses();
+
+  return <HCSupervisorsDataTable supervisorExpenses={expensesData} />;
 }
