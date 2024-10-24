ea797ebcb9905f021acd82864505a1dc60f1c2c0
wambugudavis <davvey27@gmail.com>
2024-10-24 12:05:06 +0300

    feat: students in group dialog

--- app/(platform)/hc/schools/[visibleId]/groups/components/groups-datatable.tsx
+++ app/(platform)/hc/schools/[visibleId]/groups/components/groups-datatable.tsx
@@ -4,6 +4,8 @@ import {
   columns,
   SchoolGroupDataTableData,
 } from "#/app/(platform)/hc/schools/[visibleId]/groups/components/columns";
+import DialogAlertWidget from "#/app/(platform)/hc/schools/components/dialog-alert-widget";
+import StudentsInGroup from "#/components/common/student/students-in-group";
 import DataTable from "#/components/data-table";
 import { useState } from "react";
 
@@ -13,13 +15,29 @@ export default function GroupsDataTable({
   data: SchoolGroupDataTableData[];
 }) {
   const [group, setGroup] = useState<SchoolGroupDataTableData>();
+  const [studentsDialog, setStudentsDialog] = useState(false);
   return (
-    <DataTable
-      columns={columns({ setGroup })}
-      data={data}
-      className="data-table data-table-action mt-4"
-      columnVisibilityState={{ "Active Status": false }}
-      emptyStateMessage="No groups associated with this school"
-    />
+    <>
+      <DataTable
+        columns={columns({ setGroup, setStudentsDialog })}
+        data={data}
+        className="data-table data-table-action mt-4"
+        columnVisibilityState={{ "Active Status": false }}
+        emptyStateMessage="No groups associated with this school"
+      />
+      {group && (
+        <StudentsInGroup
+          group={group}
+          open={studentsDialog}
+          onOpenChange={setStudentsDialog}
+        >
+          <DialogAlertWidget>
+            <div className="flex items-center gap-2">
+              <span>Group {group.groupName}</span>
+            </div>
+          </DialogAlertWidget>
+        </StudentsInGroup>
+      )}
+    </>
   );
 }
---
+++ components/common/student/students-in-group.tsx
@@ -0,0 +1,125 @@
+"use client";
+
+import { SchoolGroupDataTableData } from "#/app/(platform)/hc/schools/[visibleId]/groups/components/columns";
+import DialogAlertWidget from "#/app/(platform)/hc/schools/components/dialog-alert-widget";
+import StudentDetailsForm from "#/components/common/student/student-details-form";
+import DataTable from "#/components/data-table";
+import { Icons } from "#/components/icons";
+import { Button } from "#/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+} from "#/components/ui/dialog";
+import { Prisma } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import React, { Dispatch, SetStateAction } from "react";
+
+export default function StudentsInGroup({
+  children,
+  open,
+  onOpenChange,
+  group,
+}: {
+  children: React.ReactNode;
+  open: boolean;
+  onOpenChange: Dispatch<SetStateAction<boolean>>;
+  group: SchoolGroupDataTableData;
+}) {
+  const [addStudentDialog, setAddStudentDialog] = React.useState(false);
+  return (
+    <>
+      <Dialog open={open} onOpenChange={onOpenChange}>
+        <DialogContent className="w-2/3 max-w-none">
+          <DialogHeader>
+            <h2 className="text-xl font-bold">Students in group</h2>
+          </DialogHeader>
+          {children}
+          <DataTable
+            columns={columns}
+            editColumns={false}
+            data={group.students}
+            emptyStateMessage={"No students associated to this group"}
+            className="data-table"
+          />
+          <DialogFooter className="flex justify-end gap-2">
+            <Button
+              variant="ghost"
+              type="button"
+              className="text-base font-semibold leading-6 text-shamiri-new-blue hover:text-shamiri-new-blue"
+              onClick={() => {
+                onOpenChange(false);
+              }}
+            >
+              Cancel
+            </Button>
+            <Button
+              variant="brand"
+              onClick={() => {
+                setAddStudentDialog(true);
+              }}
+              className="flex items-center gap-2"
+            >
+              <Icons.plusCircle className="h-4 w-4" />
+              Add Student
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+      <StudentDetailsForm
+        open={addStudentDialog}
+        onOpenChange={setAddStudentDialog}
+        mode="add"
+      >
+        <DialogAlertWidget>
+          <div className="flex items-center gap-2">
+            <span>Group {group.groupName}</span>
+          </div>
+        </DialogAlertWidget>
+      </StudentDetailsForm>
+    </>
+  );
+}
+
+const columns: ColumnDef<
+  Prisma.StudentGetPayload<{
+    include: {
+      _count: {
+        select: {
+          clinicalCases: true;
+        };
+      };
+    };
+  }>
+>[] = [
+  {
+    id: "Student name",
+    header: "Student name",
+    cell: ({ row }) => {
+      return (
+        <span className="capitalize">
+          {row.original.studentName?.toLowerCase()}
+        </span>
+      );
+    },
+  },
+  {
+    header: "Shamiri ID",
+    id: "Shamiri ID",
+    accessorKey: "visibleId",
+  },
+  {
+    header: "Age",
+    id: "Age",
+    accessorFn: (row) =>
+      row.yearOfBirth && new Date().getFullYear() - row.yearOfBirth + " yrs",
+  },
+  {
+    header: "Clinical cases",
+    id: "Clinical cases",
+    accessorFn: (row) => {
+      return row._count.clinicalCases;
+    },
+  },
+];
