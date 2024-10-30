828b3055f7dc962539568cc47b5c931d7a780103
hinn254 <bennyhinnotieno@gmail.com>
2024-10-30 14:30:25 +0300

    refactor: Update payment complaints schema

--- app/(platform)/hc/reporting/complaints/schema.ts
+++ app/(platform)/hc/reporting/complaints/schema.ts
@@ -2,31 +2,32 @@ import { stringValidation } from "#/lib/utils";
 import { z } from "zod";
 
 export const ComplaintSchema = z.object({
-  fellow: z.string({
-    required_error: "Please select a fellow",
-  }),
+  fellow: stringValidation("Please select a fellow").optional(),
   mpesaNumber: stringValidation("Please confirm the Mpesa number"),
   mpesaName: stringValidation("Please enter the fellow's MPESA name."),
-  noOfTrainingSessions: z
-    .number()
-    .min(1, { message: "Please enter the no of training sessions" }),
-  noOfSupervisionSessions: z
-    .number()
-    .min(1, { message: "Please enter the no of supervision sessions" }),
-  noOfPreSessions: z
-    .number()
-    .min(1, { message: "Please enter the no of pre sessions" }),
-  noOfMainSessions: z
-    .number()
-    .min(1, { message: "Please enter the no of main sessions" }),
-  noOfSpecialSessions: z
-    .number()
-    .min(1, { message: "Please enter the no of special sessions" }),
-  paidAmount: z.number().min(1, { message: "Please enter the paid amount" }),
-  confirmedTotalReceived: z
-    .number()
-    .min(1, { message: "Please enter the confirmed total received" }),
-  complaintReason: stringValidation("Please enter the complaint reason"),
+  noOfTrainingSessions: z.coerce.number({
+    required_error: "Please enter the no of training sessions",
+  }),
+
+  noOfSupervisionSessions: z.coerce.number({
+    required_error: "Please enter the no of supervision sessions",
+  }),
+  noOfPreSessions: z.coerce.number({
+    required_error: "Please enter the no of pre sessions",
+  }),
+  noOfMainSessions: z.coerce.number({
+    required_error: "Please enter the no of main sessions",
+  }),
+  noOfSpecialSessions: z.coerce.number({
+    required_error: "Please enter the no of special sessions",
+  }),
+  paidAmount: z.coerce.number({
+    required_error: "Please enter the paid amount",
+  }),
+  confirmedAmountReceived: z.coerce.number({
+    required_error: "Please enter the confirmed amount received",
+  }),
+  reasonForComplaint: stringValidation("Please enter the complaint reason"),
   comments: stringValidation("Please enter additional comments"),
   reasonForAccepting: z.string().optional(),
   reasonForRejecting: z.string().optional(),
