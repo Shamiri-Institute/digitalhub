d8f24e0587d7516bdb4b0e77ac545d348c29d875
Davis Wambugu <davvey27@gmail.com>
2024-10-30 11:26:45 +0300

    feat: hc should be able to replace group leader (#247)

--- app/(platform)/hc/schools/[visibleId]/groups/components/groups-datatable.tsx
+++ app/(platform)/hc/schools/[visibleId]/groups/components/groups-datatable.tsx
@@ -5,21 +5,30 @@ import {
   SchoolGroupDataTableData,
 } from "#/app/(platform)/hc/schools/[visibleId]/groups/components/columns";
 import DialogAlertWidget from "#/app/(platform)/hc/schools/components/dialog-alert-widget";
+import ReplaceFellow from "#/components/common/fellow/replace-fellow";
 import StudentGroupEvaluation from "#/components/common/group/student-group-evaluation";
 import StudentsInGroup from "#/components/common/student/students-in-group";
 import DataTable from "#/components/data-table";
+import { Prisma } from "@prisma/client";
 import { useEffect, useState } from "react";
 
 export default function GroupsDataTable({
   data,
   schoolVisibleId,
+  supervisors,
 }: {
   data: SchoolGroupDataTableData[];
   schoolVisibleId: string;
+  supervisors: Prisma.SupervisorGetPayload<{
+    include: {
+      fellows: true;
+    };
+  }>[];
 }) {
   const [group, setGroup] = useState<SchoolGroupDataTableData>();
   const [studentsDialog, setStudentsDialog] = useState(false);
   const [evaluationDialog, setEvaluationDialog] = useState(false);
+  const [leaderDialog, setLeaderDialog] = useState(false);
 
   useEffect(() => {
     if (group) {
@@ -33,7 +42,12 @@ export default function GroupsDataTable({
   return (
     <>
       <DataTable
-        columns={columns({ setGroup, setStudentsDialog, setEvaluationDialog })}
+        columns={columns({
+          setGroup,
+          setStudentsDialog,
+          setEvaluationDialog,
+          setLeaderDialog,
+        })}
         data={data}
         className="data-table data-table-action mt-4"
         columnVisibilityState={{ "Active Status": false }}
@@ -68,6 +82,24 @@ export default function GroupsDataTable({
               </div>
             </DialogAlertWidget>
           </StudentGroupEvaluation>
+          <ReplaceFellow
+            open={leaderDialog}
+            onOpenChange={setLeaderDialog}
+            fellowId={group.leaderId}
+            groupId={group.id}
+            supervisors={supervisors}
+            schoolVisibleId={schoolVisibleId}
+          >
+            <DialogAlertWidget>
+              <div className="flex items-center gap-2">
+                <span>{group.fellowName}</span>
+                <span className="h-1 w-1 rounded-full bg-shamiri-new-blue">
+                  {""}
+                </span>
+                <span>{group.groupName}</span>
+              </div>
+            </DialogAlertWidget>
+          </ReplaceFellow>
         </>
       )}
     </>
