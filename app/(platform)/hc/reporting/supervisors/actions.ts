1eb290171e02f117b6fd67f2e192e21eb9bdb2db
hinn254 <bennyhinnotieno@gmail.com>
2024-11-04 20:52:57 +0300

    Refactor sign out functionality for unauthorized users and current hub supervisors

--- app/(platform)/hc/reporting/supervisors/actions.ts
+++ app/(platform)/hc/reporting/supervisors/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { currentHubCoordinator } from "#/app/auth";
+import { objectId } from "#/lib/crypto";
 import { db } from "#/lib/db";
 import { signOut } from "next-auth/react";
 
@@ -53,6 +54,8 @@ export async function loadHubSupervisorExpenses() {
       amount: expense.amount,
       status: expense.status,
       hubCoordinatorName: hubCoordinator.coordinatorName,
+      mpesaName: expense.mpesaName,
+      mpesaNumber: expense.mpesaNumber,
     };
   });
 }
@@ -95,6 +98,15 @@ export async function deleteSupervisorExpenseRequest({
   }
 }
 
+export async function getSupervisorsInHub() {
+  const hubCoordinator = await currentHubCoordinator();
+  return await db.supervisor.findMany({
+    where: {
+      hubId: hubCoordinator?.assignedHubId!,
+    },
+  });
+}
+
 export async function approveSupervisorExpense({ id }: { id: string }) {
   try {
     const hubCoordinator = await currentHubCoordinator();
@@ -120,6 +132,111 @@ export async function approveSupervisorExpense({ id }: { id: string }) {
     return {
       success: false,
       message: "Failed to approve expense ",
+    };
+  }
+}
+
+export async function addSupervisorExpense({
+  data,
+}: {
+  data: {
+    expenseType: string;
+    mpesaName: string;
+    mpesaNumber: string;
+    receiptFileKey: string;
+    session: string;
+    totalAmount: string;
+    week: string;
+    supervisor: string;
+  };
+}) {
+  try {
+    const hubCoordinator = await currentHubCoordinator();
+
+    if (!hubCoordinator) {
+      await signOut({ callbackUrl: "/login" });
+      throw new Error("Unauthorised user");
+    }
+
+    await db.reimbursementRequest.create({
+      data: {
+        id: objectId("reimb"),
+        supervisorId: data.supervisor,
+        hubId: hubCoordinator.assignedHubId!,
+        hubCoordinatorId: hubCoordinator.id,
+        incurredAt: new Date(data.week),
+        amount: parseInt(data.totalAmount),
+        kind: data.expenseType,
+        status: "PENDING",
+        details: {
+          subtype: data.expenseType,
+          receipt_link: data.receiptFileKey,
+          session: data.session,
+        },
+        mpesaName: data.mpesaName,
+        mpesaNumber: data.mpesaNumber,
+      },
+    });
+
+    return {
+      success: true,
+      message: "Successfully added expense",
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      success: false,
+      message: "Failed to add expense ",
+    };
+  }
+}
+
+export async function updateSupervisorExpense({
+  id,
+  data,
+}: {
+  id: string;
+  data: {
+    expenseType: string;
+    mpesaName: string;
+    mpesaNumber: string;
+    session: string;
+    totalAmount: string;
+    week: string;
+  };
+}) {
+  try {
+    const hubCoordinator = await currentHubCoordinator();
+
+    if (!hubCoordinator) {
+      await signOut({ callbackUrl: "/login" });
+      throw new Error("Unauthorised user");
+    }
+
+    await db.reimbursementRequest.update({
+      where: { id },
+      data: {
+        incurredAt: new Date(data.week),
+        amount: parseInt(data.totalAmount),
+        kind: data.expenseType,
+        details: {
+          subtype: data.expenseType,
+          session: data.session,
+        },
+        mpesaName: data.mpesaName,
+        mpesaNumber: data.mpesaNumber,
+      },
+    });
+
+    return {
+      success: true,
+      message: "Successfully updated expense",
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      success: false,
+      message: "Failed to update expense",
     };
   }
 }
--- app/(platform)/hc/reporting/supervisors/page.tsx
+++ app/(platform)/hc/reporting/supervisors/page.tsx
@@ -1,4 +1,7 @@
-import { loadHubSupervisorExpenses } from "#/app/(platform)/hc/reporting/supervisors/actions";
+import {
+  getSupervisorsInHub,
+  loadHubSupervisorExpenses,
+} from "#/app/(platform)/hc/reporting/supervisors/actions";
 import HCSupervisorsDataTable from "#/app/(platform)/hc/reporting/supervisors/components/supervisors-table";
 import { currentHubCoordinator } from "#/app/auth";
 import { InvalidPersonnelRole } from "#/components/common/invalid-personnel-role";
@@ -12,10 +15,13 @@ export default async function SupervisorsPage() {
     return <InvalidPersonnelRole role="hub-coordinator" />;
   }
 
+  const supervisorsInHub = await getSupervisorsInHub();
+
   return (
     <HCSupervisorsDataTable
       supervisorExpenses={expensesData}
       currentHubCoordinator={hubCoordinator}
+      supervisorsInHub={supervisorsInHub}
     />
   );
 }
