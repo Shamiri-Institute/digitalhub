19997e4af17ab6b4f675deba064a012e6a5eb1ce
Benny H. Otieno <bennyhinnotieno@gmail.com>
2024-11-12 10:51:27 +0300

    refactor: Remove search command from filter components in reporting tab (#262)

--- app/(platform)/hc/reporting/expenses/payout-history/components/payout-filter.tsx
+++ app/(platform)/hc/reporting/expenses/payout-history/components/payout-filter.tsx
@@ -1,17 +1,9 @@
 import PayoutFilterToggle from "#/app/(platform)/hc/reporting/expenses/payout-history/components/payout-filter-toggle";
-import { SearchCommand } from "#/components/search-command";
 
-export default async function PayoutFilterTab({
-  hubCoordinatorId,
-  hubId,
-}: {
-  hubCoordinatorId: string;
-  hubId: string;
-}) {
+export default async function PayoutFilterTab({ hubId }: { hubId: string }) {
   return (
     <div className="flex items-center justify-start">
       <div className="flex w-1/4 gap-3">
-        <SearchCommand data={[]} />
         <PayoutFilterToggle payout={[]} />
       </div>
     </div>
