dd5b3353631dbcea21f1bd440fe214df383e49f6
Benny H. Otieno <bennyhinnotieno@gmail.com>
2024-11-12 10:02:18 +0300

    refactor: remove search (#261)

--- components/students-filter-tab.tsx
+++ components/students-filter-tab.tsx
@@ -1,5 +1,4 @@
 import StudentsFilterToggle from "#/app/(platform)/hc/students/components/students-filter-toggle";
-import { SearchCommand } from "#/components/search-command";
 import { db } from "#/lib/db";
 
 export default async function StudentsFilterTab({
@@ -18,7 +17,6 @@ export default async function StudentsFilterTab({
   return (
     <div className="flex items-center justify-between">
       <div className="flex w-1/4 gap-3">
-        <SearchCommand data={[]} />
         <StudentsFilterToggle students={[]} />
       </div>
     </div>
