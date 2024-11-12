4802285d5dd59fcef39568c4b46cb4e92d4dcc77
Benny H. Otieno <bennyhinnotieno@gmail.com>
2024-11-12 09:58:53 +0300

    Eng-445-reporting-school-report-nav-tabs (#258)
    * Refactor reporting layout and add dropdown menu for reporting options
    
    * feat: Add new pages for reporting expenses
    
    - Added new pages for reporting expenses, including session, school feedback, fellow reports, and supervisor expenses.
    - Updated routing to redirect to the appropriate pages.
    - Renamed files and updated imports to reflect the new file structure.
    
    Refactor reporting layout and add dropdown menu for reporting options
    
    * feat: Add ComplaintsPage component for reporting complaints
    
    * chore:spelling-fix

---
+++ app/(platform)/hc/reporting/expenses/supervisors/components/supervisor-filter.tsx
@@ -0,0 +1,33 @@
+import AddSupervisorExpensesForm from "#/app/(platform)/hc/reporting/expenses/supervisors/components/add-expense";
+import SupervisorFilterToggle from "#/app/(platform)/hc/reporting/expenses/supervisors/components/supervisor-expense-toggle";
+import { Icons } from "#/components/icons";
+import { SearchCommand } from "#/components/search-command";
+import { Button } from "#/components/ui/button";
+import { Prisma } from "@prisma/client";
+
+export default async function SupervisorsFilterTab({
+  hubCoordinatorId,
+  hubId,
+  supervisorsInHub,
+}: {
+  hubCoordinatorId: string;
+  hubId: string;
+  supervisorsInHub: Prisma.SupervisorGetPayload<{}>[];
+}) {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex w-1/4 gap-3">
+        <SearchCommand data={[]} />
+        <SupervisorFilterToggle supervisors={[]} />
+      </div>
+      <div>
+        <AddSupervisorExpensesForm supervisorsInHub={supervisorsInHub}>
+          <Button className="flex items-center gap-2 bg-shamiri-new-blue text-base font-semibold leading-6 text-white">
+            <Icons.plusCircle />
+            Add expense
+          </Button>
+        </AddSupervisorExpensesForm>
+      </div>
+    </div>
+  );
+}
