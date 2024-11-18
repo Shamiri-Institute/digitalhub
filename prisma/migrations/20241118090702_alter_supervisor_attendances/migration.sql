70296ae84b57586ed7134ac9483c112ec515c4d0
hinn254 <bennyhinnotieno@gmail.com>
2024-11-18 12:27:47 +0300

    remove indexes

--- prisma/migrations/20241118090702_alter_supervisor_attendances/migration.sql
+++ prisma/migrations/20241118090702_alter_supervisor_attendances/migration.sql
@@ -5,12 +5,9 @@
   - Added the required column `marked_by` to the `supervisor_attendances` table without a default value. This is not possible if the table is not empty.
 
 */
--- DropIndex
-DROP INDEX "supervisor_attendances_visible_id_key";
 
 -- AlterTable
-ALTER TABLE "supervisor_attendances" DROP COLUMN "visible_id",
-ADD COLUMN     "marked_by" TEXT NOT NULL;
+ALTER TABLE "supervisor_attendances" ADD COLUMN "marked_by" TEXT NOT NULL;
 
 -- AddForeignKey
 ALTER TABLE "supervisor_attendances" ADD CONSTRAINT "supervisor_attendances_marked_by_fkey" FOREIGN KEY ("marked_by") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
