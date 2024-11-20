9e255d6b205a0abccd4daa7fc03b6c91cb31dda4
Benny H. Otieno <bennyhinnotieno@gmail.com>
2024-11-20 16:57:50 +0300

    Eng-471-add-upload-file-feature-on-schoolgroups (#280)

--- app/(platform)/hc/schools/[visibleId]/files/actions.ts
+++ app/(platform)/hc/schools/[visibleId]/files/actions.ts
@@ -1,9 +1,16 @@
 "use server";
 
+import { getCurrentUser } from "#/app/auth";
 import { db } from "#/lib/db";
 
 export async function removeUploadedSchoolFile(documentId: string) {
   try {
+    const user = await getCurrentUser();
+
+    if (!user) {
+      throw new Error("The session has not been authenticated");
+    }
+
     const result = await db.schoolDocuments.delete({
       where: {
         id: documentId,
@@ -24,6 +31,12 @@ export async function updateUploadedSchoolFile(
   fileName: string,
 ) {
   try {
+    const user = await getCurrentUser();
+
+    if (!user) {
+      throw new Error("The session has not been authenticated");
+    }
+
     const result = await db.schoolDocuments.update({
       where: {
         id: documentId,
@@ -39,5 +52,50 @@ export async function updateUploadedSchoolFile(
   } catch (error) {
     console.error(error);
     return { error: "Something went wrong updating the file" };
+  }
+}
+
+export async function addUploadedSchoolDocs(data: {
+  fileName: string;
+  link: string;
+  visibleId: string;
+  type: string;
+}) {
+  try {
+    const user = await getCurrentUser();
+
+    if (!user) {
+      throw new Error("The session has not been authenticated");
+    }
+
+    const school = await db.school.findFirst({
+      where: {
+        visibleId: data.visibleId,
+      },
+    });
+
+    if (!school) {
+      throw new Error("School not found");
+    }
+    await db.schoolDocuments.create({
+      data: {
+        fileName: data.fileName,
+        link: data.link,
+        schoolId: school.id,
+        uploadedBy: user.user.id,
+        type: data.type,
+      },
+    });
+
+    return {
+      success: true,
+      message: "Successfully uploaded the document.",
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      error: "Something went wrong uploading the document",
+      success: false,
+    };
   }
 }
