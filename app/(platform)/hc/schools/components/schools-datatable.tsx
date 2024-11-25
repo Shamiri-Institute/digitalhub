a1c0199a8ee416f79694b5104b63153a9c297fcc
Benny H. Otieno <bennyhinnotieno@gmail.com>
2024-11-25 08:10:37 +0300

    Refactor: Add correct URL path to metadata (#298), Add download functionality for schools CSV template (#297), Add correct URL path to metadata in BatchUploadDownloadSupervisors component

--- app/(platform)/hc/schools/components/schools-datatable.tsx
+++ app/(platform)/hc/schools/components/schools-datatable.tsx
@@ -4,14 +4,53 @@ import { columns } from "#/app/(platform)/hc/schools/components/columns";
 import { SchoolsDataContext } from "#/app/(platform)/hc/schools/context/schools-data-context";
 import SchoolsDataTable from "#/components/data-table";
 import FileUploader from "#/components/file-uploader";
+import { Icons } from "#/components/icons";
+import { Button } from "#/components/ui/button";
 import { useContext } from "react";
+
+const schoolsCSVHeaders = [
+  "school_name",
+  "numbers_expected",
+  "school_demographics",
+  "boardingorday",
+  "school_type",
+  "school_county",
+  "school_subcounty",
+  "principal_name",
+  "principal_phone",
+  "point_person_name",
+  "point_person_phone",
+  "latitude",
+  "longitude",
+  "presession_date",
+];
+
+export const handleSchoolsCSVTemplateDownload = () => {
+  const csvContent =
+    "data:text/csv;charset=utf-8," + schoolsCSVHeaders.join(",") + "\n";
+  const encodedUri = encodeURI(csvContent);
+
+  const link = document.createElement("a");
+  link.setAttribute("href", encodedUri);
+  link.setAttribute("download", "schools-upload-template.csv");
+  document.body.appendChild(link);
+  link.click();
+};
 
 export default function SchoolsDatatable() {
   const context = useContext(SchoolsDataContext);
 
   const renderTableActions = () => {
     return (
-      <div className="flex">
+      <div className="flex gap-3">
+        <Button
+          variant="outline"
+          className="flex gap-1 bg-white"
+          onClick={handleSchoolsCSVTemplateDownload}
+        >
+          <Icons.fileDown className="h-4 w-4 text-shamiri-text-grey" />
+          <span>Download schools CSV template</span>
+        </Button>
         <FileUploader
           url="/api/csv-uploads/schools"
           type="schools"
