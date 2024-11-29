a50f5ca54bbb85b9930267d1a6796a7802c1be63
Benny H. Otieno <bennyhinnotieno@gmail.com>
2024-11-29 11:55:46 +0300

    refactor: tabs to be reusable across sc/hc and feat:create default pages for reporting tab on sc (#304)

---
+++ app/(platform)/sc/reporting/fellow-reports/fellow-attendance-sheet/page.tsx
@@ -0,0 +1,3 @@
+export default function FellowAttendanceSheetPage() {
+  return <div>FellowAttendanceSheetPage</div>;
+}
---
+++ app/(platform)/sc/reporting/fellow-reports/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function FellowReportsPage() {
+  redirect(`/sc/reporting/fellow-reports/weekly-fellow-evaluation`);
+}
