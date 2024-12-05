8b2139a66680362ab35821910151f635df8de238
hinn254 <bennyhinnotieno@gmail.com>
2024-12-05 06:36:16 +0300

    feat: add components for managing fellow expenses including approval, reversal, and repayment requests in a central place

---
+++ components/common/expenses/fellows/fellow-expense-table-dropdown-me.tsx
@@ -0,0 +1,76 @@
+import { HubFellowsAttendancesType } from "#/app/(platform)/hc/reporting/expenses/fellows/actions";
+import ApproveSpecialSessionFellows from "#/components/common/expenses/fellows/approve-special-session";
+import ConfirmReversalFellows from "#/components/common/expenses/fellows/confirm-reversal";
+import RequestRepaymentFellows from "#/components/common/expenses/fellows/request-repayment";
+import { Icons } from "#/components/icons";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "#/components/ui/dropdown-menu";
+import { cn } from "#/lib/utils";
+
+export type FellowExpenseData =
+  HubFellowsAttendancesType["attendances"][number];
+
+export default function FellowExpenseTableDropdownMe({
+  expense,
+}: {
+  expense: FellowExpenseData;
+}) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <div className="absolute inset-0 border-l bg-white">
+          <div className="flex h-full w-full items-center justify-center">
+            <Icons.moreHorizontal className="h-5 w-5 text-shamiri-text-grey" />
+          </div>
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        align="end"
+        onCloseAutoFocus={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <DropdownMenuLabel>
+          <span className="text-xs font-medium uppercase text-shamiri-text-grey">
+            Actions
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <ConfirmReversalFellows expense={expense}>
+          <div
+            className={cn(
+              "cursor-pointer px-2 py-1.5 text-sm text-shamiri-black",
+            )}
+          >
+            Reverse payment
+          </div>
+        </ConfirmReversalFellows>
+        <RequestRepaymentFellows expense={expense}>
+          <div
+            className={cn(
+              "cursor-pointer px-2 py-1.5 text-sm text-shamiri-black",
+            )}
+          >
+            Request repayment
+          </div>
+        </RequestRepaymentFellows>
+        <DropdownMenuSeparator />
+
+        <ApproveSpecialSessionFellows expense={expense}>
+          <div
+            className={cn(
+              "cursor-pointer px-2 py-1.5 text-sm text-shamiri-black",
+            )}
+          >
+            Approve/reject special session
+          </div>
+        </ApproveSpecialSessionFellows>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
