06229900c01502bceec1cc380615f541bf1208c4
Benny H. Otieno <bennyhinnotieno@gmail.com>
2024-12-05 09:45:57 +0300

    Eng 541 refactor fellow tab (#307)

--- app/(platform)/hc/reporting/expenses/fellows/page.tsx
+++ app/(platform)/hc/reporting/expenses/fellows/page.tsx
@@ -1,8 +1,8 @@
 import { loadHubFellowAttendance } from "#/app/(platform)/hc/reporting/expenses/fellows/actions";
-import HCFellowsDataTable from "#/app/(platform)/hc/reporting/expenses/fellows/components/fellows-table";
+import FellowsReportingDataTable from "#/components/common/expenses/fellows/fellows-table";
 
 export default async function FellowsPage() {
   const expensesData = await loadHubFellowAttendance();
 
-  return <HCFellowsDataTable fellowAttendanceExpenses={expensesData} />;
+  return <FellowsReportingDataTable fellowAttendanceExpenses={expensesData} />;
 }
