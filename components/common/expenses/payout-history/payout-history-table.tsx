63699e39f9bdbc3b8e6f8b4af4ed82ef8324a91b
hinn254 <bennyhinnotieno@gmail.com>
2024-12-05 07:41:53 +0300

    refactor: reuse supervisor payout history page and components for sc and hc

---
+++ components/common/expenses/payout-history/payout-history-table.tsx
@@ -0,0 +1,24 @@
+"use client";
+import DataTable from "#/components/data-table";
+
+import { HubPayoutHistoryType } from "#/app/(platform)/hc/reporting/expenses/payout-history/actions";
+import FellowPayoutFilterTab from "#/components/common/expenses/payout-history/payout-filter";
+import { columns } from "./columns";
+
+export default function FellowPayoutHistoryDataTable({
+  payoutHistory,
+}: {
+  payoutHistory: HubPayoutHistoryType[];
+}) {
+  return (
+    <div className="container w-full grow space-y-3">
+      <FellowPayoutFilterTab />
+      <DataTable
+        data={payoutHistory}
+        columns={columns}
+        className="data-table data-table-action mt-4 bg-white"
+        emptyStateMessage="No payout history found"
+      />
+    </div>
+  );
+}
