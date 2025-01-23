d94c189af2951740b3aba72b5be5ee0701337702
hinn254 <bennyhinnotieno@gmail.com>
2025-01-23 15:07:24 +0300

    Refactor session report table and update tab navigation layout
    - Simplified the structure of the session report data table by removing unnecessary wrapper div.
    - Updated tab navigation component to use a container class for improved layout consistency.

--- components/common/school-reports/session/session-table.tsx
+++ components/common/school-reports/session/session-table.tsx
@@ -10,22 +10,20 @@ export default function SessionReportDataTable({
   sessionReport: SessionReportType[];
 }) {
   return (
-    <div className="container w-full grow space-y-3">
-      <DataTable
-        data={sessionReport}
-        columns={columns}
-        className="data-table data-table-action mt-4 bg-white"
-        emptyStateMessage="No feedback data found"
-        renderSubComponent={({ row }) => (
-          <DataTable
-            data={row.original?.session}
-            editColumns={false}
-            columns={subColumns}
-            className="data-table data-table-action border-0 bg-white"
-            emptyStateMessage="No expenses found for this fellow"
-          />
-        )}
-      />
-    </div>
+    <DataTable
+      data={sessionReport}
+      columns={columns}
+      className="data-table data-table-action mt-4 bg-white"
+      emptyStateMessage="No feedback data found"
+      renderSubComponent={({ row }) => (
+        <DataTable
+          data={row.original?.session}
+          editColumns={false}
+          columns={subColumns}
+          className="data-table data-table-action border-0 bg-white"
+          emptyStateMessage="No expenses found for this fellow"
+        />
+      )}
+    />
   );
 }
