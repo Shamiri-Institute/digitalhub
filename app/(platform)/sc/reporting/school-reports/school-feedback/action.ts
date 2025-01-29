ddc654fe94632deaca618edcd2a66344b1a7e80c
Benny H. Otieno <bennyhinnotieno@gmail.com>
2025-01-29 14:15:14 +0300

    feat: Implement dynamic school feedback loading for HC and SC (#323)

--- app/(platform)/sc/reporting/school-reports/school-feedback/action.ts
+++ app/(platform)/sc/reporting/school-reports/school-feedback/action.ts
@@ -1,28 +1,58 @@
 "use server";
 
+import { currentSupervisor } from "#/app/auth";
+import { db } from "#/lib/db";
+
 export async function loadSchoolFeedback() {
-  return [
-    {
-      schoolName: "School 1",
-      studentTeacherSatisfaction: 3,
-    },
-    {
-      schoolName: "School 2",
-      studentTeacherSatisfaction: 4,
-    },
-    {
-      schoolName: "School 3",
-      studentTeacherSatisfaction: 5,
-    },
-    {
-      schoolName: "School 4",
-      studentTeacherSatisfaction: 2,
-    },
-    {
-      schoolName: "School 5",
-      studentTeacherSatisfaction: 1.4,
-    },
-  ];
+  try {
+    const supervisor = await currentSupervisor();
+
+    if (!supervisor) {
+      throw new Error("The session has not been authenticated");
+    }
+
+    const { hub } = supervisor;
+
+    const schools = await db.school.findMany({
+      where: {
+        hubId: hub?.id,
+      },
+      include: {
+        schoolFeedbacks: {
+          include: {
+            user: true,
+          },
+        },
+      },
+    });
+
+    const formattedData = schools.map((school) => {
+      const studentTeacherSatisfaction =
+        school.schoolFeedbacks.reduce(
+          (acc, curr) => acc + (curr?.studentTeacherSatisfactionRating ?? 0),
+          0,
+        ) / school.schoolFeedbacks.length;
+      return {
+        schoolName: school.schoolName,
+        studentTeacherSatisfaction,
+        supervisorRatings: school.schoolFeedbacks.map((feedback) => ({
+          studentTeacherSatisfaction: feedback.studentTeacherSatisfactionRating,
+          userId: feedback.userId,
+          supervisorName: feedback.user.name,
+          feedbackId: feedback.id,
+          programImpactOnStudents: feedback.programImpactOnStudents,
+          concernsRaisedByTeachers: feedback.concernsRaisedByTeachers,
+          factorsInfluencedStudentParticipation:
+            feedback.factorsInfluencedStudentParticipation,
+        })),
+      };
+    });
+
+    return formattedData || [];
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 }
 
 export type SchoolFeedbackType = Awaited<
