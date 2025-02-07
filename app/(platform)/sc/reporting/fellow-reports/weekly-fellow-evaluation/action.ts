be43631bd3358ae5be7d1d5d041a0c25166f07c4
Benny H. Otieno <bennyhinnotieno@gmail.com>
2025-02-07 09:43:11 +0300

    feat: report-fellow-weekly-evaluation HC and SC (#324)
    * feat: add weekly fellow evaluation actions for HC and SC

---
+++ app/(platform)/sc/reporting/fellow-reports/weekly-fellow-evaluation/action.ts
@@ -0,0 +1,65 @@
+"use server";
+
+import { currentSupervisor } from "#/app/auth";
+import { WeeklyFellowEvaluation } from "#/components/common/fellow-reports/weekly-fellow-evaluation/types";
+import { db } from "#/lib/db";
+
+export async function loadWeeklyFellowEvaluation(): Promise<
+  WeeklyFellowEvaluation[]
+> {
+  try {
+    const supervisor = await currentSupervisor();
+    if (!supervisor) {
+      throw new Error("Supervisor not found");
+    }
+
+    const fellows = await db.fellow.findMany({
+      where: {
+        supervisorId: supervisor?.id,
+      },
+      include: {
+        weeklyFellowRatings: true,
+      },
+    });
+
+    const formattedData = fellows.map((fellow) => {
+      return {
+        id: fellow.id,
+        fellowName: fellow.fellowName,
+        avgBehaviour:
+          fellow.weeklyFellowRatings.reduce(
+            (a, b) => a + (b?.behaviourRating ?? 0),
+            0,
+          ) / fellow.weeklyFellowRatings.length,
+        avgProgramDelivery:
+          fellow.weeklyFellowRatings.reduce(
+            (a, b) => a + (b?.programDeliveryRating ?? 0),
+            0,
+          ) / fellow.weeklyFellowRatings.length,
+        avgDressingGrooming:
+          fellow.weeklyFellowRatings.reduce(
+            (a, b) => a + (b?.dressingAndGroomingRating ?? 0),
+            0,
+          ) / fellow.weeklyFellowRatings.length,
+        week: fellow.weeklyFellowRatings.map((rating) => ({
+          userId: supervisor.user.user.id,
+          evaluationId: rating.id,
+          week: rating.week,
+          behaviour: rating.behaviourRating ?? 0,
+          behaviourNotes: rating.behaviourNotes,
+          programDelivery: rating.programDeliveryRating ?? 0,
+          programDeliveryNotes: rating.programDeliveryNotes,
+          dressingGrooming: rating.dressingAndGroomingRating ?? 0,
+          dressingGroomingNotes: rating.dressingAndGroomingNotes,
+          attendancePunctuality: rating.punctualityRating ?? 0,
+          attendancePunctualityNotes: rating.punctualityNotes,
+        })),
+      };
+    });
+
+    return formattedData || [];
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
