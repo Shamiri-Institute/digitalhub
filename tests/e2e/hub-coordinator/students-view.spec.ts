3dbcf1309e22b339a9b1d247d9233f2dfb98edfb
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2025-02-13 11:25:36 +0300

    add smoke test for students page

--- tests/e2e/hub-coordinator/students-view.spec.ts
+++ tests/e2e/hub-coordinator/students-view.spec.ts
@@ -1,17 +1,46 @@
 import { PersonnelFixtures } from "#/tests/helpers";
 import HubCoordinatorStudentsPage from "#/tests/pages/hub-coordinator/students-page";
-import { test } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
 test.use({ storageState: PersonnelFixtures.hubCoordinator.stateFile });
 test.describe.configure({ mode: "parallel" });
 
 test("Hub coordinator can view the students page", async ({ page }) => {
   // given
-  const hubCoordinatorStudentsPage = new HubCoordinatorStudentsPage(page)
+  const hubCoordinatorStudentsPage = new HubCoordinatorStudentsPage(page);
 
   // when
-  await hubCoordinatorStudentsPage.visit()
+  await hubCoordinatorStudentsPage.visit();
 
   // then
-  await hubCoordinatorStudentsPage.isShown()
+  await hubCoordinatorStudentsPage.isShown();
+  await expect(page.getByRole("main")).toContainText("Total no. of students");
+  await expect(page.getByRole("main")).toContainText("Count of group sessions");
+  await expect(page.getByRole("main")).toContainText("No. of clinical cases");
+  await expect(page.getByRole("main")).toContainText(
+    "No. of clinical sessions",
+  );
+  await expect(page.getByRole("main")).toContainText("Attendance");
+  await expect(page.getByRole("main")).toContainText("Drop-out reasons");
+  await expect(page.getByRole("main")).toContainText(
+    "Student information completion",
+  );
+  await expect(page.getByRole("main")).toContainText("Student group ratings");
+  await expect(page.getByRole("main")).toContainText(
+    "Clinical cases by case status",
+  );
+  await expect(page.getByRole("main")).toContainText("Clinical sessions");
+  await expect(page.getByRole("main")).toContainText(
+    "Clinical cases by supervisor",
+  );
+  await expect(page.getByRole("main")).toContainText(
+    "Clinical cases by initial contact",
+  );
+  await expect(page.getByRole("main")).toContainText(
+    "Students grouped by form",
+  );
+  await expect(page.getByRole("main")).toContainText("Students grouped by age");
+  await expect(page.getByRole("main")).toContainText(
+    "Students grouped by gender",
+  );
 });
--- tests/pages/hub-coordinator/students-page.ts
+++ tests/pages/hub-coordinator/students-page.ts
@@ -3,9 +3,9 @@ import { AppPage } from "../app-page";
 import { getUrl } from "../helpers";
 
 export default class HubCoordinatorStudentsPage extends AppPage {
-  readonly route = getUrl("/hc/students")
+  readonly route = getUrl("/hc/students");
 
   constructor(readonly page: Page) {
-    super(page)
+    super(page);
   }
 }
