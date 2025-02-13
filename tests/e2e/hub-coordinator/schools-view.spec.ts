370e3502a59494b9b42e1fa7423d3060800f41d9
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2025-02-13 14:44:35 +0300

    add database assertion for checking that the hub report works

--- tests/e2e/hub-coordinator/schools-view.spec.ts
+++ tests/e2e/hub-coordinator/schools-view.spec.ts
@@ -1,3 +1,4 @@
+import { db } from "#/lib/db";
 import { PersonnelFixtures } from "#/tests/helpers";
 import HubCoordinatorSchoolsPage from "#/tests/pages/hub-coordinator/school-page";
 import { expect, test } from "@playwright/test";
@@ -12,27 +13,43 @@ test("Hub Coordinator can view the /schools page", async ({ page }) => {
   await hubCoordinatorSchoolsPage.visit();
   // then
   await hubCoordinatorSchoolsPage.isShown();
-  await expect(page.getByRole('button', { name: 'Weekly Hub Report' })).toBeVisible();
+  await expect(
+    page.getByRole("button", { name: "Weekly Hub Report" }),
+  ).toBeVisible();
 
-  const mainTag = page.getByRole('main');
-  await expect(page.locator('h2')).toContainText('Schools');
-  await expect(mainTag).toContainText('Session progress');
-  await expect(mainTag).toContainText('Drop out reasons');
-  await expect(mainTag).toContainText('School information completion');
-  await expect(mainTag).toContainText('Ratings');
-  await expect(mainTag).toContainText('Download schools CSV template');
-  await expect(mainTag).toContainText('Upload schools CSV');
-  await expect(page.locator('main')).toContainText('Edit columns');
+  const mainTag = page.getByRole("main");
+  await expect(page.locator("h2")).toContainText("Schools");
+  await expect(mainTag).toContainText("Session progress");
+  await expect(mainTag).toContainText("Drop out reasons");
+  await expect(mainTag).toContainText("School information completion");
+  await expect(mainTag).toContainText("Ratings");
+  await expect(mainTag).toContainText("Download schools CSV template");
+  await expect(mainTag).toContainText("Upload schools CSV");
+  await expect(page.locator("main")).toContainText("Edit columns");
 });
 
 test("Hub Coordinator can submit a weekly hub report", async ({ page }) => {
+  // given
   const hubCoordinatorSchoolsPage = new HubCoordinatorSchoolsPage(page);
   await hubCoordinatorSchoolsPage.visit();
+  const data = {
+    schoolRelatedIssuesAndObservations: "The school is ok",
+    schoolRelatedIssuesAndObservationRating: 2,
+    supervisorRelatedIssuesAndObservations: "the school is awesome",
+    supervisorRelatedIssuesAndObservationsRating: 3,
+    fellowRelatedIssuesAndObservations: "The fellows have been ok",
+    fellowRelatedIssuesAndObservationsRating: 1,
+    hubRelatedIssuesAndObservations: "There have been no issues",
+    hubRelatedIssuesAndObservationsRating: 4,
+    successes: "It has generally been successful",
+    challenges: "I don't have any challenges to report",
+    recommendations: "I would recommend continued behaviour",
+  };
 
-  await page.getByRole('button', { name: 'Add icon circle outlined' }).click();
+  // when
+  await page.getByRole("button", { name: "Add icon circle outlined" }).click();
   await page.getByRole("combobox", { name: "Select week" }).click();
-  await page.getByRole('option', { name: 'Week 1' }).click();
-
+  await page.getByRole("option", { name: "Week 1" }).click();
   await page
     .locator("label")
     .filter({ hasText: "Hub Related Issues and" })
@@ -41,7 +58,7 @@ test("Hub Coordinator can submit a weekly hub report", async ({ page }) => {
     .click();
   await page
     .getByRole("textbox", { name: "Hub Related Issues and" })
-    .fill("There have been no issues");
+    .fill(data.hubRelatedIssuesAndObservations);
   await page
     .locator("label")
     .filter({ hasText: "School Related Issues and" })
@@ -50,10 +67,7 @@ test("Hub Coordinator can submit a weekly hub report", async ({ page }) => {
     .click();
   await page
     .getByRole("textbox", { name: "School Related Issues and" })
-    .click();
-  await page
-    .getByRole("textbox", { name: "School Related Issues and" })
-    .fill("The school is ok");
+    .fill(data.schoolRelatedIssuesAndObservations);
   await page
     .locator("label")
     .filter({ hasText: "Supervisor Related Issues and" })
@@ -62,10 +76,7 @@ test("Hub Coordinator can submit a weekly hub report", async ({ page }) => {
     .click();
   await page
     .getByRole("textbox", { name: "Supervisor Related Issues and" })
-    .fill("the school is awesome");
-  await page
-    .getByRole("textbox", { name: "Fellow Related Issues and" })
-    .click();
+    .fill(data.supervisorRelatedIssuesAndObservations);
   await page
     .locator("label")
     .filter({ hasText: "Fellow Related Issues and" })
@@ -75,18 +86,22 @@ test("Hub Coordinator can submit a weekly hub report", async ({ page }) => {
   //await page.getByRole('textbox', { name: 'Fellow Related Issues and' }).click();
   await page
     .getByRole("textbox", { name: "Fellow Related Issues and" })
-    .fill("The fellows have been ok");
+    .fill(data.fellowRelatedIssuesAndObservations);
   //await page.getByRole('textbox', { name: 'Successes' }).click();
-  await page
-    .getByRole("textbox", { name: "Successes" })
-    .fill("It has generally been successful");
+  await page.getByRole("textbox", { name: "Successes" }).fill(data.successes);
   //await page.getByRole('textbox', { name: 'Challenges' }).click();
-  await page
-    .getByRole("textbox", { name: "Challenges" })
-    .fill("I don't have any challenges to report");
+  await page.getByRole("textbox", { name: "Challenges" }).fill(data.challenges);
   //await page.getByRole('textbox', { name: 'Recommendations' }).click();
   await page
     .getByRole("textbox", { name: "Recommendations" })
-    .fill("I would recommend continued behaviour");
+    .fill(data.recommendations);
   await page.getByRole("button", { name: "Submit" }).click();
+
+  // then
+  const record = await db.weeklyHubReport.findFirst({
+    where: {
+      supervisorRelatedIssuesAndObservations: "the school is awesome",
+    },
+  });
+  expect(record).toMatchObject(data);
 });
