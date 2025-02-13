281dda1ee5b49591b330acb00fa3d640c6281966
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2025-02-13 11:34:57 +0300

    refactor students-view

--- tests/e2e/hub-coordinator/students-view.spec.ts
+++ tests/e2e/hub-coordinator/students-view.spec.ts
@@ -14,33 +14,34 @@ test("Hub coordinator can view the students page", async ({ page }) => {
 
   // then
   await hubCoordinatorStudentsPage.isShown();
-  await expect(page.getByRole("main")).toContainText("Total no. of students");
-  await expect(page.getByRole("main")).toContainText("Count of group sessions");
-  await expect(page.getByRole("main")).toContainText("No. of clinical cases");
-  await expect(page.getByRole("main")).toContainText(
+  const mainPage = page.getByRole("main");
+  await expect(mainPage).toContainText("Total no. of students");
+  await expect(mainPage).toContainText("Count of group sessions");
+  await expect(mainPage).toContainText("No. of clinical cases");
+  await expect(mainPage).toContainText(
     "No. of clinical sessions",
   );
-  await expect(page.getByRole("main")).toContainText("Attendance");
-  await expect(page.getByRole("main")).toContainText("Drop-out reasons");
-  await expect(page.getByRole("main")).toContainText(
+  await expect(mainPage).toContainText("Attendance");
+  await expect(mainPage).toContainText("Drop-out reasons");
+  await expect(mainPage).toContainText(
     "Student information completion",
   );
-  await expect(page.getByRole("main")).toContainText("Student group ratings");
-  await expect(page.getByRole("main")).toContainText(
+  await expect(mainPage).toContainText("Student group ratings");
+  await expect(mainPage).toContainText(
     "Clinical cases by case status",
   );
-  await expect(page.getByRole("main")).toContainText("Clinical sessions");
-  await expect(page.getByRole("main")).toContainText(
+  await expect(mainPage).toContainText("Clinical sessions");
+  await expect(mainPage).toContainText(
     "Clinical cases by supervisor",
   );
-  await expect(page.getByRole("main")).toContainText(
+  await expect(mainPage).toContainText(
     "Clinical cases by initial contact",
   );
-  await expect(page.getByRole("main")).toContainText(
+  await expect(mainPage).toContainText(
     "Students grouped by form",
   );
-  await expect(page.getByRole("main")).toContainText("Students grouped by age");
-  await expect(page.getByRole("main")).toContainText(
+  await expect(mainPage).toContainText("Students grouped by age");
+  await expect(mainPage).toContainText(
     "Students grouped by gender",
   );
 });
