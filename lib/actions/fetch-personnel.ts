3b6ec686c4ce538536c7f90240f19a2c99ad639e
Davis Wambugu <davvey27@gmail.com>
2025-02-25 17:13:03 +0300

    style: update dev personnel switcher (#355)

--- lib/actions/fetch-personnel.ts
+++ lib/actions/fetch-personnel.ts
@@ -12,21 +12,39 @@ export async function fetchPersonnel() {
       where: {
         assignedSchools: { some: { NOT: { assignedSupervisorId: null } } },
       },
+      include: {
+        hub: {
+          include: {
+            project: true,
+          },
+        },
+      },
     })
   ).map((sup) => ({
     id: sup.id,
     role: ImplementerRole.SUPERVISOR,
     label: `${sup.supervisorName} - ${sup.visibleId}`,
+    hub: sup.hub?.hubName,
+    project: sup.hub?.project?.name,
   }));
 
   const hubCoordinators: Personnel[] = (
     await db.hubCoordinator.findMany({
       orderBy: { coordinatorName: "desc" },
+      include: {
+        assignedHub: {
+          include: {
+            project: true,
+          },
+        },
+      },
     })
   ).map((hc) => ({
     id: hc.id,
     role: ImplementerRole.HUB_COORDINATOR,
     label: `${hc.coordinatorName} - ${hc.visibleId}`,
+    hub: hc.assignedHub?.hubName,
+    project: hc.assignedHub?.project?.name,
   }));
   const personnel = [...supervisors, ...hubCoordinators];
 
