e5c5f233541d1d53fdd1dedf383fecdc12e5751f
hinn254 <bennyhinnotieno@gmail.com>
2025-02-28 14:45:30 +0300

    refactor: update ClinicalFollowUpTreatmentPlan model schema

--- prisma/migrations/20250228114303_add_clinical_follow_up_treatment_plan_table/migration.sql
+++ prisma/migrations/20250228114303_add_clinical_follow_up_treatment_plan_table/migration.sql
@@ -1,14 +1,11 @@
--- CreateEnum
-CREATE TYPE "CLINICAL_FOLLOW_UP_SESSION_FREQUENCY" AS ENUM ('ONCE_A_WEEK', 'TWICE_A_WEEK', 'ONCE_EVERY_TWO_WEEKS');
-
 -- CreateTable
 CREATE TABLE "clinical_follow_up_treatment_plan" (
     "id" TEXT NOT NULL,
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMPTZ NOT NULL,
-    "current_ors_score" TEXT NOT NULL,
+    "current_ors_score" INTEGER NOT NULL,
     "planned_sessions" INTEGER NOT NULL,
-    "session_frequency" "CLINICAL_FOLLOW_UP_SESSION_FREQUENCY" NOT NULL,
+    "session_frequency" TEXT NOT NULL,
     "planned_treatment_intervention" TEXT NOT NULL,
     "planned_treatment_intervention_explanation" TEXT NOT NULL,
     "case_id" TEXT NOT NULL,
