449c20db280a295800062a5e37c3f6980d810bff
stanmorsey <georgestanley14282@gmail.com>
2025-03-07 10:21:09 +0300

    fix: resolve merge conflict and preserve fel redirect

--- next.config.js
+++ next.config.js
@@ -15,14 +15,11 @@ const nextConfig = {
         destination: "/sc/schedule",
         permanent: false,
       },
-<<<<<<< Updated upstream
-=======
       {
         source: "/fel",
         destination: "/fel/schedule",
         permanent: false,
       },
->>>>>>> Stashed changes
     ];
   },
 };
@@ -38,8 +35,6 @@ module.exports = withSentryConfig(
   {
     // For all available options, see:
     // https://github.com/getsentry/sentry-webpack-plugin#options
-
-    // Suppresses source map uploading logs during build
     silent: true,
     org: "shamiri-institute-21336c4e6",
     project: "digitalhub-frontend",
@@ -47,26 +42,11 @@ module.exports = withSentryConfig(
   {
     // For all available options, see:
     // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
     widenClientFileUpload: true,
-
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
     transpileClientSDK: true,
-
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
     tunnelRoute: "/monitoring",
-
-    // Hides source maps from generated client bundles
     hideSourceMaps: true,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
     disableLogger: true,
-
-    // Enables automatic instrumentation of Vercel Cron Monitors.
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
     automaticVercelMonitors: true,
-  },
+  }
 );
