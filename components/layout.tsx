4b59985048066b6ae722b25fd646f6466b128c06
wambugudavis <davvey27@gmail.com>
2025-03-10 20:43:55 +0300

    feat: fellow portal page

--- components/layout.tsx
+++ components/layout.tsx
@@ -215,7 +215,7 @@ function LayoutV2({
             {mainRoute === "fel" && (
               <div className={`tab-link ${cn(studentsActive && "active")}`}>
                 <PeopleIconAlternate />
-                <Link href={`/${mainRoute}/fellow-portal`}>Fellow portal</Link>
+                <Link href={`/${mainRoute}/portal`}>Fellow portal</Link>
               </div>
             )}
             {mainRoute === "sc" && (
