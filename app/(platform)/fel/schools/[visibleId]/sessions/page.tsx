5d12da76997ebd98ecb13e7fb85f0287bdac0654
Davis Wambugu <davvey27@gmail.com>
2025-03-10 20:49:59 +0300

    feat: schools page for fellows + ENG-598 + ENG-597 + ENG-592 + ENG-591 + ENG-590  (#373)

---
+++ app/(platform)/fel/schools/[visibleId]/sessions/page.tsx
@@ -0,0 +1,54 @@
+import { currentFellow } from "#/app/auth";
+import SessionsDatatable from "#/components/common/session/sessions-datatable";
+import { db } from "#/lib/db";
+import { signOut } from "next-auth/react";
+
+export default async function SchoolSessionsPage({
+  params: { visibleId },
+}: {
+  params: { visibleId: string };
+}) {
+  const fellow = await currentFellow();
+  if (fellow === null) {
+    await signOut({ callbackUrl: "/login" });
+  }
+
+  const sessions = await db.interventionSession.findMany({
+    where: {
+      school: {
+        visibleId,
+      },
+    },
+    include: {
+      school: {
+        include: {
+          assignedSupervisor: true,
+          interventionGroups: {
+            include: {
+              students: {
+                include: {
+                  _count: {
+                    select: {
+                      clinicalCases: true,
+                    },
+                  },
+                  studentAttendances: true,
+                },
+              },
+            },
+          },
+        },
+      },
+      sessionRatings: true,
+      session: true,
+    },
+  });
+
+  return (
+    <SessionsDatatable
+      sessions={sessions}
+      role={fellow?.user?.membership.role!}
+      fellowId={fellow?.id}
+    />
+  );
+}
