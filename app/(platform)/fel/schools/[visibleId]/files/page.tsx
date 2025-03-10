8dd69f55dc0f4e44bcfdbe7f7fb6187b0bd6e1d1
wambugudavis <davvey27@gmail.com>
2025-03-10 10:36:22 +0300

    feat: add auth check

--- app/(platform)/fel/schools/[visibleId]/files/page.tsx
+++ app/(platform)/fel/schools/[visibleId]/files/page.tsx
@@ -1,11 +1,19 @@
+import { currentFellow } from "#/app/auth";
 import SchoolFilesDatatable from "#/components/common/files/files-datatable";
 import { db } from "#/lib/db";
+import { signOut } from "next-auth/react";
 
 export default async function SchoolFilesPage({
   params: { visibleId },
 }: {
   params: { visibleId: string };
 }) {
+  const fellow = await currentFellow();
+  if (fellow === null) {
+    await signOut({ callbackUrl: "/login" });
+  }
+
+  //TODO: Filter files for fellow only
   const schoolFiles = db.schoolDocuments.findMany({
     where: {
       school: {
