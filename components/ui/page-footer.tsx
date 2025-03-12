10bdae84da2960f14c104c3a1b04ff0effe32b7f
wambugudavis <davvey27@gmail.com>
2025-03-12 13:51:35 +0300

    feat: mobile responsiveness: footer

--- components/ui/page-footer.tsx
+++ components/ui/page-footer.tsx
@@ -2,13 +2,13 @@ export default function PageFooter() {
   return (
     <footer className="w-full border-t bg-background-secondary">
       <div className="container py-2">
-        <div className="flex items-center justify-between text-sm font-medium text-shamiri-text-grey">
-          <div>
-            <p className="">
+        <div className="flex flex-col-reverse gap-4 text-sm font-medium text-shamiri-text-grey lg:flex-row lg:items-center lg:justify-between lg:gap-2">
+          <div className="col-span-2">
+            <p>
               © Copyright {new Date().getFullYear()} Shamiri Institute Inc.
             </p>
           </div>
-          <div className="flex gap-x-6">
+          <div className="grid grid-cols-2 gap-x-6 lg:flex">
             <span>Terms of Use</span>
             <span>Privacy Policy</span>
           </div>
