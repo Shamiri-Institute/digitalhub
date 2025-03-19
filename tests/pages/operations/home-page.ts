9d13a5cf9d9cc0107c7d86ca48cfb7a1c0df89e7
hinn254 <bennyhinnotieno@gmail.com>
2025-03-19 20:00:44 +0300

    fix: update route for OperationsHomePage to point to expenses section

--- tests/pages/operations/home-page.ts
+++ tests/pages/operations/home-page.ts
@@ -4,7 +4,8 @@ import { AppPage } from "#/tests/pages/app-page";
 import { getUrl } from "#/tests/pages/helpers";
 
 export class OperationsHomePage extends AppPage {
-  readonly route = getUrl("/ops/reporting");
+  // TODO: Update this to the correct route once we have other parts of the ops reporting page
+  readonly route = getUrl("/ops/reporting/expenses/fellows");
 
   static new(page: Page) {
     return new OperationsHomePage(page);
