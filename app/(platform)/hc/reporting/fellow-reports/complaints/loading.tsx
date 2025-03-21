e498040ab4690577d5d9fe0904dd7ac948968630
Benny H. Otieno <bennyhinnotieno@gmail.com>
2025-03-21 11:55:07 +0300

    feat: ops reporting-expenses tab (#383)

--- app/(platform)/hc/reporting/fellow-reports/complaints/loading.tsx
+++ app/(platform)/hc/reporting/fellow-reports/complaints/loading.tsx
@@ -1,5 +1,56 @@
-import TableSkeleton from "#/components/table-skeleton";
+"use client";
 
-export default function FellowComplaintsTableSkeleton() {
-  return <TableSkeleton numRows={10} />;
+import { columns } from "#/components/common/expenses/complaints/columns";
+import { FellowComplaintsType } from "#/components/common/fellow-reports/complaints/actions";
+import DataTable from "#/components/data-table";
+import { Skeleton } from "#/components/ui/skeleton";
+import { ColumnDef } from "@tanstack/react-table";
+
+export default function ComplaintsTableSkeleton() {
+  const loadingColumns: ColumnDef<FellowComplaintsType>[] = columns.map(
+    (column) => {
+      const columnId =
+        typeof column.header === "string"
+          ? column.header
+          : (column.id ?? "unknown");
+      return {
+        accessorFn: () => null,
+        header:
+          columnId !== "checkbox" && columnId !== "button" ? columnId : "",
+        id: columnId,
+        cell: () => {
+          return columnId !== "checkbox" && columnId !== "button" ? (
+            <Skeleton className="h-5 w-full bg-gray-200" />
+          ) : null;
+        },
+      };
+    },
+  );
+
+  const emptyData: FellowComplaintsType[] = Array.from(Array(10).keys()).map(
+    () => ({
+      id: "",
+      fellowName: "",
+      hub: "",
+      supervisorName: "",
+      specialSession: 0,
+      preVsMain: "",
+      trainingSupervision: "",
+      paidAmount: 0,
+      totalAmount: 0,
+      complaints: [],
+    }),
+  );
+
+  return (
+    <div className="space-y-3 px-6 py-10">
+      <DataTable
+        key="skeleton-complaints-table"
+        columns={loadingColumns}
+        data={emptyData}
+        className="data-table data-table-action lg:mt-4"
+        emptyStateMessage=""
+      />
+    </div>
+  );
 }
---
+++ app/(platform)/ops/components/render-reporting-tabs.tsx
@@ -0,0 +1,14 @@
+"use client";
+import TabToggleNavigation, {
+  TabType,
+} from "#/components/common/tabs/tab-navigation";
+
+export default function RenderOpsReportingTabs() {
+  const expensesReportOptions: TabType[] = [
+    { name: "Fellows", href: `/ops/reporting/expenses/fellows` },
+    { name: "Supervisors", href: `/ops/reporting/expenses/supervisors` },
+    { name: "Payout history", href: `/ops/reporting/expenses/payout-history` },
+    { name: "Complaints", href: `/ops/reporting/expenses/complaints` },
+  ];
+  return <TabToggleNavigation options={expensesReportOptions} />;
+}
