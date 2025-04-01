cc90fea3dad1a7b13a100d4a0a01834fcc79867b
stanmorsey <georgestanley14282@gmail.com>
2025-04-01 23:44:02 +0300

    feat: update sentry feedback tool

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -3,7 +3,13 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: "https://07b0e0899b9160a522279ab4a97862e2@o4505804271845376.ingest.us.sentry.io/4505829743788032",
 
+  integrations: [Sentry.replayIntegration()],
+
   tracesSampleRate: 1,
+
+  replaysSessionSampleRate: 0.1,
+
+  replaysOnErrorSampleRate: 1.0,
 
   debug: false,
 });
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,15 +1,9 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://07b0e0899b9160a522279ab4a97862e2@o4505804271845376.ingest.sentry.io/4505829743788032",
+  dsn: "https://07b0e0899b9160a522279ab4a97862e2@o4505804271845376.ingest.us.sentry.io/4505829743788032",
 
-  // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 });
