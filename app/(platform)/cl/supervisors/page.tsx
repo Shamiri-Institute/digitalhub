fc857e1a97c2ab9e31144f433f749f4946526db5
Benny H. Otieno <bennyhinnotieno@gmail.com>
2025-04-14 12:07:04 +0300

    feat: clinical-lead supervisors tab (#414)

---
+++ app/(platform)/cl/supervisors/page.tsx
@@ -0,0 +1,15 @@
+import SupervisorClinicalCasesTable from "#/app/(platform)/cl/supervisors/components/supervisor-clinical-cases-table";
+import PageHeading from "#/components/ui/page-heading";
+import { Separator } from "#/components/ui/separator";
+
+export default function SupervisorsPage() {
+  return (
+    <div className="flex h-full flex-col">
+      <div className="container w-full grow space-y-3">
+        <PageHeading title="Supervisors" />
+        <Separator />
+        <SupervisorClinicalCasesTable />
+      </div>
+    </div>
+  );
+}
