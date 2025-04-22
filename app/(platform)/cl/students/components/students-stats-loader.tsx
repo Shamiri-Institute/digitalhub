b3a918e422b9c03fbb2464df40899837ce12d16f
hinn254 <bennyhinnotieno@gmail.com>
2025-04-22 12:08:59 +0300

    feat: add students stats components with loading and breakdown features

---
+++ app/(platform)/cl/students/components/students-stats-loader.tsx
@@ -0,0 +1,5 @@
+import { Skeleton } from "#/components/ui/skeleton";
+
+export default function StudentsStatsLoader() {
+  return <Skeleton className="h-full w-full" />;
+}
---
+++ app/(platform)/cl/students/components/students-stats.tsx
@@ -0,0 +1,13 @@
+export default function StudentsStatsBreakdown({
+  studentsStats,
+}: {
+  studentsStats: any;
+}) {
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-2">
+        <h1 className="text-2xl font-bold">Students Stats</h1>
+      </div>
+    </div>
+  );
+}
