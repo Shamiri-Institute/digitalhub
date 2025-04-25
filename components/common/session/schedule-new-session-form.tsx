cfa5bb0c8f8c49f70bb153851292777badd77e7f
wambugudavis <davvey27@gmail.com>
2025-04-25 12:23:13 +0300

    style: time picker popover input + style fixes on calendar component

--- components/common/session/schedule-new-session-form.tsx
+++ components/common/session/schedule-new-session-form.tsx
@@ -12,7 +12,11 @@ import {
   FormMessage,
 } from "#/components/ui/form";
 import { Input } from "#/components/ui/input";
-import { Popover, PopoverContent } from "#/components/ui/popover";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "#/components/ui/popover";
 import {
   Select,
   SelectContent,
@@ -22,14 +26,14 @@ import {
 } from "#/components/ui/select";
 import { Separator } from "#/components/ui/separator";
 import { useToast } from "#/components/ui/use-toast";
-import { fetchInterventionSessions } from "#/lib/actions/fetch-sessions";
 import { createNewSession } from "#/lib/actions/session/session";
 import { cn } from "#/lib/utils";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Prisma } from "@prisma/client";
-import { PopoverTrigger } from "@radix-ui/react-popover";
 import { format } from "date-fns";
+import { ChevronsUpDown } from "lucide-react";
 import {
+  ChangeEvent,
   Dispatch,
   SetStateAction,
   useContext,
@@ -51,9 +55,25 @@ export function ScheduleNewSession({
   hubSessionTypes: Prisma.SessionNameGetPayload<{}>[];
 }) {
   const { toast } = useToast();
-  const { setSessions } = useContext(SessionsContext);
+  const { refresh } = useContext(SessionsContext);
 
   const [mode, setMode] = useState<"school" | "venue">("school");
+
+  const [hour, setHour] = useState("9");
+  const [minutes, setMinutes] = useState("00");
+  const [time, setTime] = useState("AM");
+  const [timePicker, setTimePicker] = useState(false);
+
+  const handleMinutesChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const num = parseInt(e.target.value, 10);
+    if (!isNaN(num)) {
+      const clamped = Math.max(0, Math.min(num, 60));
+      const padded = clamped.toString().padStart(2, "0");
+      setMinutes(padded);
+    } else {
+      setMinutes("00");
+    }
+  };
 
   const form = useForm<z.infer<typeof ScheduleNewSessionSchema>>({
     resolver: zodResolver(ScheduleNewSessionSchema),
@@ -64,7 +84,7 @@ export function ScheduleNewSession({
           sessionType.sessionType === "TRAINING"
         );
       })[0]?.id,
-      sessionStartTime: "06:00",
+      sessionStartTime: undefined,
       schoolId: undefined,
       venue: undefined,
     },
@@ -93,27 +113,21 @@ export function ScheduleNewSession({
   }, [hubSessionTypes, sessionIdWatcher]);
 
   const onSubmit = async (data: z.infer<typeof ScheduleNewSessionSchema>) => {
-    const sessionDate =
-      format(new Date(data.sessionDate), "yyyy-MM-dd") +
-      "T" +
-      data.sessionStartTime +
-      ":00";
-    data.sessionDate = new Date(sessionDate);
+    const combinedDateTime = new Date(
+      `${format(new Date(data.sessionDate), "yyyy-MM-dd")}T${new Date(`1970-01-01 ${data.sessionStartTime}`).toTimeString().slice(0, 5)}:00`,
+    );
+    data.sessionDate = new Date(combinedDateTime);
 
     try {
       const response = await createNewSession(data);
       if (response.success) {
-        const fetchedSessions = await fetchInterventionSessions({
-          hubId,
-        });
-        setSessions(fetchedSessions);
-
         toast({
           variant: "default",
           description: `Successfully created new session`,
         });
         form.reset();
         toggleDialog(false);
+        await refresh();
       } else {
         toast({
           variant: "destructive",
@@ -238,7 +252,7 @@ export function ScheduleNewSession({
                     <Button
                       variant={"outline"}
                       className={cn(
-                        "mt-1.5 w-full justify-start px-3 text-left font-normal",
+                        "mt-1.5 w-full justify-start px-3 text-left font-normal active:scale-100",
                         !field.value && "text-muted-foreground",
                       )}
                     >
@@ -250,7 +264,7 @@ export function ScheduleNewSession({
                       )}
                     </Button>
                   </PopoverTrigger>
-                  <PopoverContent className="w-auto p-0">
+                  <PopoverContent className="w-auto p-0" align="start">
                     <Calendar
                       mode="single"
                       selected={field.value}
@@ -268,16 +282,106 @@ export function ScheduleNewSession({
             render={({ field }) => (
               <div>
                 <FormLabel>Start time</FormLabel>
-                <Input
-                  id="startTime"
-                  name="startTime"
-                  type="time"
-                  onChange={field.onChange}
-                  min="06:00"
-                  max="18:00"
-                  defaultValue="06:00"
-                  className="mt-1.5 w-full appearance-none"
-                />
+                <Popover open={timePicker} onOpenChange={setTimePicker}>
+                  <PopoverTrigger asChild>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "mt-1.5 w-full justify-start px-3 text-left font-normal active:scale-100",
+                        !field.value && "text-muted-foreground",
+                      )}
+                    >
+                      <Icons.clock className="mr-2 h-4 w-4 text-muted-foreground" />
+                      {field.value ? field.value : <span>Pick a time</span>}
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto px-4 py-3" align="start">
+                    <div className="flex flex-col gap-2">
+                      <span className="text-sm">Pick a start time</span>
+                      <div className="flex items-center gap-2 py-2">
+                        <div className="relative w-[4rem]">
+                          <Input
+                            type="number"
+                            onChange={(e) => {
+                              setHour(e.target.value);
+                            }}
+                            value={hour}
+                            min={1}
+                            max={12}
+                            className="transparent-number-arrows"
+                          />
+                          <div className="pointer-events-none absolute right-1 top-1/2 flex -translate-y-1/2 flex-col items-center justify-center space-y-0.5 text-muted-foreground">
+                            <ChevronsUpDown className="mr-2 h-4 w-4" />
+                          </div>
+                        </div>
+                        <span>:</span>
+                        <div className="relative w-[4rem]">
+                          <Input
+                            type="number"
+                            onChange={handleMinutesChange}
+                            value={minutes}
+                            min={0}
+                            max={60}
+                            className="transparent-number-arrows"
+                          />
+                          <div className="pointer-events-none absolute right-1 top-1/2 flex -translate-y-1/2 flex-col items-center justify-center space-y-0.5 text-muted-foreground">
+                            <ChevronsUpDown className="mr-2 h-4 w-4" />
+                          </div>
+                        </div>
+                        <Select
+                          onValueChange={(value) => {
+                            setTime(value);
+                          }}
+                          defaultValue={time}
+                        >
+                          <FormControl>
+                            <SelectTrigger className="w-[4rem]">
+                              <SelectValue />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent className="min-w-[4rem]">
+                            {["AM", "PM"].map((time) => {
+                              return (
+                                <SelectItem key={time} value={time}>
+                                  <span>{time}</span>
+                                </SelectItem>
+                              );
+                            })}
+                          </SelectContent>
+                        </Select>
+                      </div>
+                      <Separator />
+                      <div className="flex justify-end gap-2">
+                        <Button
+                          variant="ghost"
+                          type="button"
+                          size="sm"
+                          className="text-sm font-semibold leading-6 text-shamiri-new-blue hover:text-shamiri-new-blue"
+                          onClick={() => {
+                            setTimePicker(false);
+                          }}
+                        >
+                          Cancel
+                        </Button>
+                        <Button
+                          className="flex items-center gap-2 bg-shamiri-new-blue text-sm font-semibold leading-6 text-white"
+                          type="submit"
+                          size="sm"
+                          disabled={form.formState.isSubmitting}
+                          loading={form.formState.isSubmitting}
+                          onClick={() => {
+                            const timeString =
+                              hour + ":" + minutes + " " + time;
+                            field.onChange(timeString);
+                            setTimePicker(false);
+                          }}
+                        >
+                          Save
+                        </Button>
+                      </div>
+                    </div>
+                  </PopoverContent>
+                </Popover>
               </div>
             )}
           />
