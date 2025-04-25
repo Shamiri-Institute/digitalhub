d6fc2969b9e85b20af64ee0e7023ee989a7f07cf
wambugudavis <davvey27@gmail.com>
2025-04-25 12:30:22 +0300

    fix: remove unnecessary migrations

--- prisma/migrations/20250424133648_alter_session_date_timestamptz/migration.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE intervention_sessions
-ALTER COLUMN session_date TYPE timestamptz
-USING session_date AT TIME ZONE 'Africa/Nairobi';
--- prisma/migrations/20250424133835_move_values_back_to_session_date/migration.sql
+++
@@ -1,2 +0,0 @@
-UPDATE intervention_sessions
-SET session_date = session_date_tz;
