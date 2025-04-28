758d0962f29a21ce388548faf6597709cdddb198
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2025-04-28 21:43:20 +0300

    feat: add sentry feedback tool to the SDH (#427)

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,9 +5,9 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://07b0e0899b9160a522279ab4a97862e2@o4505804271845376.ingest.sentry.io/4505829743788032",
+  dsn: "https://07b0e0899b9160a522279ab4a97862e2@o4505804271845376.ingest.us.sentry.io/4505829743788032",
 
-  // Adjust this value in production, or use tracesSampler for greater control
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
