f136149194e9fc3051ffb5a0fc9e984603b6b35a
wambugudavis <davvey27@gmail.com>
2025-04-28 13:37:17 +0300

    fix: adjust migrations

--- prisma/migrations/20250424133446_convert_session_date_to_utc/migration.sql
+++ prisma/migrations/20250424133446_convert_session_date_to_utc/migration.sql
@@ -1,2 +1,4 @@
 UPDATE intervention_sessions
 SET session_date_tz = session_date AT TIME ZONE 'Africa/Nairobi';
+
+ALTER TABLE "intervention_sessions" ALTER COLUMN "session_date_tz" SET NOT NULL;
--- prisma/migrations/20250425092654_rename_session_date_tz_to_session_date/migration.sql
+++ prisma/migrations/20250425092654_rename_session_date_tz_to_session_date/migration.sql
@@ -1,9 +1,5 @@
-/*
-  Warnings:
+ALTER TABLE "intervention_sessions"
+DROP COLUMN "session_date";
 
-  - You are about to drop the column `session_date_tz` on the `intervention_sessions` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "intervention_sessions" DROP COLUMN "session_date_tz",
-ALTER COLUMN "session_date" SET DATA TYPE TIMESTAMPTZ(6);
+ALTER TABLE "intervention_sessions"
+RENAME COLUMN "session_date_tz" TO "session_date";
