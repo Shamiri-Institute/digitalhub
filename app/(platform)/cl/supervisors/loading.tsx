7481514e8c680b13b5bc3f2264e450fc06978b04
Benny H. Otieno <bennyhinnotieno@gmail.com>
2025-04-28 14:54:19 +0300

    feat: clinical lead students tab (#419)

--- app/(platform)/cl/supervisors/loading.tsx
+++ app/(platform)/cl/supervisors/loading.tsx
@@ -4,15 +4,13 @@ import DataTable from "#/components/data-table";
 import { Separator } from "#/components/ui/separator";
 import { Skeleton } from "#/components/ui/skeleton";
 
-export default function ClinicalTableSkeleton() {
+export default function SupervisorTableSkeleton() {
   const loadingColumns = [
-    { header: "Name", id: "fellowName" },
-    { header: "Average Rating", id: "averageRating" },
+    { header: "Supervisor Name", id: "supervisorName" },
     { header: "Active Status", id: "activeStatus" },
-    { header: "Cases Referred", id: "casesReferred" },
-    { header: "No. of Groups", id: "noOfGroups" },
-    { header: "Phone Number", id: "phoneNumber" },
-    { header: "Supervisor", id: "supervisorName" },
+    { header: "No. of Clinical Cases", id: "noOfClinicalCases" },
+    { header: "No. of Treatment Plans", id: "noOfTreatmentPlans" },
+    { header: "No. of Case Notes", id: "noOfCaseNotes" },
   ].map((column) => ({
     header: column.header,
     id: column.id,
@@ -21,10 +19,9 @@ export default function ClinicalTableSkeleton() {
 
   return (
     <div className="space-y-3 px-6 py-10">
-      <h1 className="text-2xl font-bold">Fellows</h1>
+      <h1 className="text-2xl font-bold">Supervisors</h1>
       <Separator />
       <DataTable
-        key="skeleton-clinical-cases-table"
         columns={loadingColumns}
         data={Array.from(Array(10).keys()).map(() => ({}))}
         className="data-table data-table-action lg:mt-4"
