3eb18b8117abffafbb6c76f9feeb30a08b5dee51
Davis Wambugu <davvey27@gmail.com>
2025-05-08 10:29:00 +0300

    feat: add school functionality (#436)

--- app/(platform)/hc/schools/[visibleId]/layout.tsx
+++ app/(platform)/hc/schools/[visibleId]/layout.tsx
@@ -3,7 +3,7 @@ import { currentHubCoordinator, getCurrentUser } from "#/app/auth";
 import AssignPointSupervisor from "#/components/common/schools/assign-point-supervisor";
 import { SchoolsTableData } from "#/components/common/schools/columns";
 import { DropoutSchool } from "#/components/common/schools/dropout-school-form";
-import EditSchoolDetailsForm from "#/components/common/schools/edit-school-details-form";
+import SchoolDetailsForm from "#/components/common/schools/school-details-form";
 import SchoolInfoProvider from "#/components/common/schools/school-info-provider";
 import SchoolLeftPanel from "#/components/common/schools/school-left-panel";
 import SchoolsBreadcrumb from "#/components/common/schools/schools-breadcrumb";
@@ -100,7 +100,7 @@ export default async function SchoolViewLayout({
           <PageFooter />
         </div>
       </div>
-      <EditSchoolDetailsForm />
+      <SchoolDetailsForm />
       <AssignPointSupervisor supervisors={supervisors} />
       <DropoutSchool />
       <UndoDropoutSchool />
