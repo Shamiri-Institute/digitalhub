c75ee6769582906fb760013da4d16eda9054b923
wambugudavis <davvey27@gmail.com>
2025-05-12 11:52:29 +0300

    fix: set supervisor visible id to nullable

---
+++ prisma/migrations/20250512081358_set_supervisor_visible_id_nullable/migration.sql
@@ -0,0 +1,8 @@
+-- DropIndex
+DROP INDEX "intervention_sessions_school_id_session_id_key";
+
+-- DropIndex
+DROP INDEX "intervention_sessions_school_id_session_type_key";
+
+-- AlterTable
+ALTER TABLE "supervisors" ALTER COLUMN "visible_id" DROP NOT NULL;
