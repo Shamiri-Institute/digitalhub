9cb9b7ba31d99d5f5ce8b7109fb93ec0224f3445
Davis Wambugu <davvey27@gmail.com>
2025-05-30 11:30:38 +0300

    refactor: edit profile dialog (#464)

--- app/(platform)/hc/layout.tsx
+++ app/(platform)/hc/layout.tsx
@@ -1,16 +1,9 @@
 import React from "react";
 
-export default async function HubCoordinatorLayout({
-  profileModal,
+export default function HubCoordinatorLayout({
   children,
 }: {
   children: React.ReactNode;
-  profileModal: React.ReactNode;
 }) {
-  return (
-    <div className="w-full self-stretch">
-      {profileModal}
-      {children}
-    </div>
-  );
+  return <div className="w-full self-stretch">{children}</div>;
 }
