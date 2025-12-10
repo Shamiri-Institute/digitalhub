19c8a1b2a23ed0a44162d9ea3816a17c568a63fb
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2025-12-10 16:13:04 +0300

    update the import for node stream

--- app/api/csv-uploads/fellows/route.ts
+++ app/api/csv-uploads/fellows/route.ts
@@ -1,7 +1,7 @@
 import type { Prisma } from "@prisma/client";
 import * as fastCsv from "fast-csv";
 import { type NextRequest, NextResponse } from "next/server";
-import { Readable } from "stream";
+import { Readable } from "node:stream";
 import { currentHubCoordinator } from "#/app/auth";
 import { objectId } from "#/lib/crypto";
 import { db } from "#/lib/db";
