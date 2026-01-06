baaaa43c2780b3d853bb493c7368251a69aabe4e
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2026-01-06 19:44:37 +0300

    style: add css directives to the biome configuration

--- tests/e2e/supervisors-clinical-screening.spec.ts
+++ tests/e2e/supervisors-clinical-screening.spec.ts
@@ -3,35 +3,36 @@ import { expect, test } from "@playwright/test";
 import { PersonnelFixtures } from "#/tests/helpers";
 import { ClinicalScreeningPage } from "#/tests/pages/supervisors/clinical-screening-page";
 
-test.describe.skip("clinical screenings", () => {
-  test.use({ storageState: PersonnelFixtures.supervisor.stateFile });
+test.describe
+  .skip("clinical screenings", () => {
+    test.use({ storageState: PersonnelFixtures.supervisor.stateFile });
 
-  test("supervisor can submit a clinical screening", async ({ page }) => {
-    const clinicalScreeningPage = new ClinicalScreeningPage(page);
-    await clinicalScreeningPage.visit();
-    await clinicalScreeningPage.isShown();
+    test("supervisor can submit a clinical screening", async ({ page }) => {
+      const clinicalScreeningPage = new ClinicalScreeningPage(page);
+      await clinicalScreeningPage.visit();
+      await clinicalScreeningPage.isShown();
 
-    await clinicalScreeningPage.openClinicalCaseCreationDialogue();
-    await expect(page.getByText("Create Clinical Case")).toBeVisible();
+      await clinicalScreeningPage.openClinicalCaseCreationDialogue();
+      await expect(page.getByText("Create Clinical Case")).toBeVisible();
 
-    await clinicalScreeningPage.chooseSchoolInSelectDropdown("ANS23_School_22");
-    await expect(page.getByText("Dandora High").nth(0)).toBeVisible();
-    await page.waitForLoadState("networkidle");
+      await clinicalScreeningPage.chooseSchoolInSelectDropdown("ANS23_School_22");
+      await expect(page.getByText("Dandora High").nth(0)).toBeVisible();
+      await page.waitForLoadState("networkidle");
 
-    await clinicalScreeningPage.chooseSupervisorInSelectDropdown("SPV23_S_20");
-    await expect(page.getByText("Zakia Arbubaqar").nth(0)).toBeVisible();
-    await page.waitForLoadState("networkidle");
+      await clinicalScreeningPage.chooseSupervisorInSelectDropdown("SPV23_S_20");
+      await expect(page.getByText("Zakia Arbubaqar").nth(0)).toBeVisible();
+      await page.waitForLoadState("networkidle");
 
-    await clinicalScreeningPage.chooseFellowInSelectDropdown("TFW23_S_115");
-    await expect(page.getByText("Georgina Wamaitha").nth(0)).toBeVisible();
-    await page.waitForLoadState("networkidle");
+      await clinicalScreeningPage.chooseFellowInSelectDropdown("TFW23_S_115");
+      await expect(page.getByText("Georgina Wamaitha").nth(0)).toBeVisible();
+      await page.waitForLoadState("networkidle");
 
-    // Make sure to pick an option visible on the screen
-    await clinicalScreeningPage.chooseStudentInSelectDropdown("Stu_Adm_ANS23_School_17_13");
-    await expect(page.getByText("Student 12").nth(0)).toBeVisible();
+      // Make sure to pick an option visible on the screen
+      await clinicalScreeningPage.chooseStudentInSelectDropdown("Stu_Adm_ANS23_School_17_13");
+      await expect(page.getByText("Student 12").nth(0)).toBeVisible();
 
-    await clinicalScreeningPage.submitClinicalCaseDialogue();
+      await clinicalScreeningPage.submitClinicalCaseDialogue();
 
-    await clinicalScreeningPage.assertClinicalCaseIsVisible("Student 12");
+      await clinicalScreeningPage.assertClinicalCaseIsVisible("Student 12");
+    });
   });
-});
