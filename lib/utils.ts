364ee468f0ac7deed34c0d3a7e74221dc4b9db5b
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2026-01-06 23:24:00 +0300

    chore: Remove unused dependencies, functions, and obsolete configurations (#566)

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-import type { InterventionSession, riskStatusOptions } from "@prisma/client";
+import type { riskStatusOptions } from "@prisma/client";
 import { type ClassValue, clsx } from "clsx";
 import type { Metadata } from "next";
 import { twMerge } from "tailwind-merge";
@@ -39,40 +39,6 @@ export function constructMetadata({
   };
 }
 
-export function parseEuropeanDate(dateString: string | null): Date | null {
-  if (!dateString) return null;
-
-  // European date format is "day/month/year"
-  const parts = dateString.split("/");
-
-  if (parts.length < 3) return null;
-
-  const year = Number(parts[2]);
-  const month = Number(parts[1]);
-  const day = Number(parts[0]);
-
-  if (Number.isNaN(day) || Number.isNaN(month) || Number.isNaN(year)) return null;
-
-  const date = new Date(year, month - 1, day);
-
-  return date;
-}
-
-export function ordinalSuffixOf(i: number): string {
-  const j = i % 10;
-  const k = i % 100;
-  if (j === 1 && k !== 11) {
-    return `${i}st`;
-  }
-  if (j === 2 && k !== 12) {
-    return `${i}nd`;
-  }
-  if (j === 3 && k !== 13) {
-    return `${i}rd`;
-  }
-  return `${i}th`;
-}
-
 export function getHighestValue(data: { [k: string]: string }): riskStatusOptions {
   const values = Object.values(data);
 
@@ -88,29 +54,8 @@ export function getHighestValue(data: { [k: string]: string }): riskStatusOption
   return "No";
 }
 
-export function isSessionScheduled(
-  sessions: InterventionSession[],
-  sessionType: string, // s0, s1, s2, ...
-) {
-  return sessions.some(
-    (session) => session.sessionType === sessionType && session.occurred === true,
-  );
-}
-
 export function stringValidation(message: string | undefined = "Required*") {
   return z.string({ error: message }).trim().min(1, { error: message });
-}
-
-export function mapSessionTypeToSessionNumber(sessionType: string): number {
-  if (sessionType[0] === "s" && sessionType.length === 2) {
-    return Number.parseInt(sessionType[1] ?? "0");
-  }
-
-  throw new Error(`Invalid session type: ${sessionType}`);
-}
-
-export function notEmpty<T>(value: T | null | undefined): value is T {
-  return value !== null && value !== undefined;
 }
 
 export function sessionDisplayName(sessionType?: string) {
