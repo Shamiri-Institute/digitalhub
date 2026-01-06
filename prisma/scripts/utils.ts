394dd7d89be190eb929a57239db457ce621add37
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2026-01-06 16:25:03 +0300

    chore: use builtin node modules during imports

--- prisma/scripts/utils.ts
+++ prisma/scripts/utils.ts
@@ -1,6 +1,6 @@
 import * as csv from "csv-parse";
-import * as fs from "fs";
-import * as path from "path";
+import * as fs from "node:fs";
+import * as path from "node:path";
 
 /**
  * Loads and parse CSV file in ./data/ directory
