c5c039f930e7456e9ec99acbcaa62c4a59ea15aa
Shadrack Lilan <30429938+tindNan@users.noreply.github.com>
2026-01-19 12:32:37 +0300

    perf(fel): remove blocking DB queries from loading.tsx states [ENG-1239] (#591)

--- app/(platform)/fel/schools/[visibleId]/files/loading.tsx
+++ app/(platform)/fel/schools/[visibleId]/files/loading.tsx
@@ -1,16 +1,6 @@
 import { ImplementerRole } from "@prisma/client";
-import { signOut } from "next-auth/react";
-import { getCurrentUserSession } from "#/app/auth";
 import FilesDatatableSkeleton from "#/components/common/files/files-datatable-skeleton";
 
-export default async function Loading() {
-  const userSession = await getCurrentUserSession();
-  if (!userSession) {
-    await signOut({ callbackUrl: "/login" });
-  }
-  return (
-    <FilesDatatableSkeleton
-      role={userSession?.user.activeMembership?.role ?? ImplementerRole.FELLOW}
-    />
-  );
+export default function Loading() {
+  return <FilesDatatableSkeleton role={ImplementerRole.FELLOW} />;
 }
