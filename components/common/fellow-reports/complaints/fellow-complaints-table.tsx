b3e4eae18f0686575b47e37b9f5e86037a7b60f6
hinn254 <bennyhinnotieno@gmail.com>
2026-02-25 16:11:35 +0300

    fix: update empty state messages in fellow complaints table for clarity

--- components/common/fellow-reports/complaints/fellow-complaints-table.tsx
+++ components/common/fellow-reports/complaints/fellow-complaints-table.tsx
@@ -15,14 +15,14 @@ export default function FellowComplaintsTable({
         data={fellowComplaints}
         columns={columns}
         className="data-table data-table-action bg-white lg:mt-4"
-        emptyStateMessage="No feedback data found"
+        emptyStateMessage="No complaints found"
         renderSubComponent={({ row }) => (
           <DataTable
             data={row.original?.complaints}
             editColumns={false}
             columns={subColumns}
             className="data-table data-table-action border-0 bg-white"
-            emptyStateMessage="No expenses found for this fellow"
+            emptyStateMessage="No complaints found for this fellow"
             disablePagination={true}
             disableSearch={true}
           />
