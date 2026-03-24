ceac31100123f963b9a8cc58d1a28140ede22e91
hinn254 <bennyhinnotieno@gmail.com>
2026-03-24 08:43:28 +0300

    wrap column headers in student datatable for improved styling

--- components/common/student/columns.tsx
+++ components/common/student/columns.tsx
@@ -9,6 +9,7 @@ import StudentsDataTableMenu from "#/components/common/student/students-datatabl
 import SessionHistoryWidget from "#/components/common/supervisor/sessions-history-widget";
 import { Badge } from "#/components/ui/badge";
 import { Checkbox } from "#/components/ui/checkbox";
+import { wrapColumnHeader } from "#/lib/utils";
 
 export type SchoolStudentTableData = Prisma.StudentGetPayload<{
   include: {
@@ -98,17 +99,17 @@ export const columns = (state: {
   },
   {
     accessorKey: "studentName",
-    header: "Student Name",
+    header: () => wrapColumnHeader("Student Name"),
     id: "Student Name",
   },
   {
     // TODO: this computation should be done during the fetch and possible user an accessor Function
     accessorKey: "assignedGroup.groupName",
-    header: "Group Name",
+    header: () => wrapColumnHeader("Group Name"),
     id: "Group Name",
   },
   {
-    header: "Attendance history",
+    header: () => wrapColumnHeader("Attendance history"),
     id: "Attendance history",
     cell: ({ row }) => {
       const attendances = state.sessions.map((session) => {
@@ -128,7 +129,7 @@ export const columns = (state: {
     },
   },
   {
-    header: "Shamiri ID",
+    header: () => wrapColumnHeader("Shamiri ID"),
     id: "Shamiri ID",
     accessorKey: "visibleId",
   },
@@ -138,7 +139,7 @@ export const columns = (state: {
     id: "Age",
   },
   {
-    header: "Clinical Sessions",
+    header: () => wrapColumnHeader("Clinical Sessions"),
     id: "Clinical Sessions",
     accessorFn: (row) => row.clinicalCases?.reduce((acc, val) => acc + val.sessions.length, 0),
   },
@@ -148,7 +149,7 @@ export const columns = (state: {
     accessorKey: "gender",
   },
   {
-    header: "Contact no.",
+    header: () => wrapColumnHeader("Contact no."),
     id: "Contact no.",
     accessorFn: (row) => {
       try {
@@ -159,7 +160,7 @@ export const columns = (state: {
     },
   },
   {
-    header: "Admission number",
+    header: () => wrapColumnHeader("Admission number"),
     id: "Admission number",
     accessorKey: "admissionNumber",
   },
@@ -189,7 +190,7 @@ export const columns = (state: {
       ),
   },
   {
-    header: "Date added",
+    header: () => wrapColumnHeader("Date added"),
     id: "Date added",
     accessorFn: (row) => format(row.createdAt, "dd/MM/yyyy"),
   },
