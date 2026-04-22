70690bae47be578ab3bac44540d3729648451006
hinn254 <bennyhinnotieno@gmail.com>
2026-04-22 13:02:47 +0300

    show accurate student stats and charts

--- app/(platform)/admin/schools/[visibleId]/students/page.tsx
+++ app/(platform)/admin/schools/[visibleId]/students/page.tsx
@@ -2,7 +2,6 @@ import { ImplementerRole } from "@prisma/client";
 import { signOut } from "next-auth/react";
 import { currentAdminUser } from "#/app/auth";
 import StudentsDatatable from "#/components/common/student/students-datatable";
-import { getActiveProjectId } from "#/lib/active-project-id";
 import { db } from "#/lib/db";
 
 export default async function StudentsPage({ params }: { params: Promise<{ visibleId: string }> }) {
@@ -12,18 +11,11 @@ export default async function StudentsPage({ params }: { params: Promise<{ visib
     await signOut({ callbackUrl: "/login" });
   }
 
-  const implementerId = admin?.session?.user.activeMembership?.implementerId;
-  const projectId = await getActiveProjectId();
-
   const students = await db.student.findMany({
     where: {
       archivedAt: null,
       school: {
         visibleId,
-        implementerId,
-        hub: {
-          projectId,
-        },
       },
     },
     include: {
