a54edcc07121886f84af3c27ff9c98095c27fc41
Benny H. Otieno <bennyhinnotieno@gmail.com>
2026-04-28 10:58:16 +0300

    feat(clinical): allow clinical leads to un-terminate cases (#675)

--- components/common/clinical/clinical-case-actions-dropdown.tsx
+++ components/common/clinical/clinical-case-actions-dropdown.tsx
@@ -1,3 +1,4 @@
+import CaseUnterminateDialog from "#/app/(platform)/cl/clinical/components/case-unterminate-dialog";
 import type { ClinicalCases } from "#/app/(platform)/sc/clinical/action";
 import CaseNotesForm from "#/app/(platform)/sc/clinical/components/case-notes-form";
 import CaseTerminationForm from "#/app/(platform)/sc/clinical/components/case-termination-form";
@@ -110,6 +111,13 @@ export default function ClinicalCaseActionsDropdownMenu({
             Terminate case
           </div>
         </CaseTerminationForm>
+        {role === "CLINICAL_LEAD" && clinicalCase.caseStatus === "Terminated" && (
+          <CaseUnterminateDialog caseId={clinicalCase.id} pseudonym={clinicalCase.pseudonym}>
+            <div className="cursor-pointer px-2 py-1.5 text-sm text-shamiri-black">
+              Un-terminate case
+            </div>
+          </CaseUnterminateDialog>
+        )}
       </DropdownMenuContent>
     </DropdownMenu>
   );
