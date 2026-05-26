ec601f335fba26b82764d8fae2d8d3d249290e5b
“Pberu <pberu8531@gmail.com>
2026-05-26 12:42:48 +0300

    linting error fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const { withSentryConfig } = require("@sentry/nextjs");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ['9327-41-215-134-1.ngrok-free.app'],
   images: {
     remotePatterns: [
       {
