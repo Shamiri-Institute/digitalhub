04baca3e98dd6cf6a5446664953447bdd0c2d35d
“Pberu <pberu8531@gmail.com>
2026-06-15 20:14:54 +0300

    added build skeleton for loader

--- components/common/ticket/loading.tsx
+++ components/common/ticket/loading.tsx
@@ -1,38 +1,40 @@
 "use client";
 
-import { Skeleton } from "#/components/ui/skeleton";
+import { ImplementerRole } from "@prisma/client";
+import {
+  buildSkeletonColumns,
+  buildSkeletonRows,
+} from "#/components/common/build-skeleton-columns";
+import DataTable from "#/components/data-table";
+import { columns, type TicketData } from "./columns";
+import CreateTicketDialog from "./create-ticket-dialog";
 
-const HEADER_KEYS = ["h1", "h2", "h3", "h4", "h5", "h6"];
-const ROW_KEYS = ["r1", "r2", "r3", "r4", "r5"];
-const CELL_KEYS = ["c1", "c2", "c3", "c4", "c5", "c6"];
+export default function TicketsLoading({
+  role = ImplementerRole.FELLOW,
+  rows = 5,
+}: {
+  role?: ImplementerRole;
+  rows?: number;
+}) {
+  const loadingColumns = buildSkeletonColumns(
+    columns({
+      setTicket: () => {},
+      setViewDialog: () => {},
+      role,
+    }),
+  );
 
-export default function TicketsLoading() {
+  const renderTableActions = () => {
+    return <CreateTicketDialog disabled />;
+  };
+
   return (
-    <div className="space-y-4">
-      <div className="flex items-center justify-between">
-        <Skeleton className="h-10 w-48" />
-        <Skeleton className="h-10 w-32" />
-      </div>
-      <div className="rounded-lg border bg-white">
-        <div className="border-b">
-          <div className="flex gap-4 p-4">
-            {HEADER_KEYS.map((key) => (
-              <Skeleton key={key} className="h-6 w-24" />
-            ))}
-            <Skeleton className="h-6 w-12 ml-auto" />
-          </div>
-        </div>
-        <div className="p-4 space-y-3">
-          {ROW_KEYS.map((rowKey) => (
-            <div key={rowKey} className="flex gap-4 items-center">
-              {CELL_KEYS.map((cellKey) => (
-                <Skeleton key={`${rowKey}-${cellKey}`} className="h-5 w-24" />
-              ))}
-              <Skeleton className="h-5 w-12 ml-auto" />
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
+    <DataTable
+      columns={loadingColumns}
+      data={buildSkeletonRows<TicketData>(rows)}
+      className="data-table data-table-action lg:mt-4"
+      emptyStateMessage=""
+      renderTableActions={renderTableActions()}
+    />
   );
 }
