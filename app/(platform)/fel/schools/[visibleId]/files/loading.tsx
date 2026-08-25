a7dc359a2bfaa97ca0940e831b0bcca16b7c1a42
hinn254 <bennyhinnotieno@gmail.com>
2026-08-25 15:04:37 +0300

    chore: remove deprecated file management components and functionality

--- app/(platform)/fel/schools/[visibleId]/files/loading.tsx
+++
@@ -1,6 +0,0 @@
-import { ImplementerRole } from "@prisma/client";
-import FilesDatatableSkeleton from "#/components/common/files/files-datatable-skeleton";
-
-export default function Loading() {
-  return <FilesDatatableSkeleton role={ImplementerRole.FELLOW} />;
-}
